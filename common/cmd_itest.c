khadas/u-boot

commit 7861204c9af7fec1ea9b41541c272516235a6c93

itest: make memory access work under sandbox
itest accesses memory, and hence must map/unmap it. Without doing so, it
accesses invalid addresses and crashes.

Signed-off-by: Stephen Warren <swarren@wwwdotorg.org>
Reviewed-by: Simon Glass <sjg@chromium.org>

common/cmd_itest.c
@@ -15,6 +15,9 @@
 #include <common.h>
 #include <config.h>
 #include <command.h>
+#include <mapmem.h>
+
+#include <asm/io.h>
 
 #define EQ	0
 #define NE	1
@@ -49,16 +52,24 @@ static const op_tbl_t op_table [] = {
 static long evalexp(char *s, int w)
 {
 	long l = 0;
-	long *p;
+	unsigned long addr;
+	void *buf;
 
 	/* if the parameter starts with a * then assume is a pointer to the value we want */
 	if (s[0] == '*') {
-		p = (long *)simple_strtoul(&s[1], NULL, 16);
-		switch (w) {
-		case 1: return((long)(*(unsigned char *)p));
-		case 2: return((long)(*(unsigned short *)p));
-		case 4: return(*p);
+		addr = simple_strtoul(&s[1], NULL, 16);
+		buf = map_physmem(addr, w, MAP_WRBACK);
+		if (!buf) {
+			puts("Failed to map physical memory\n");
+			return 0;
 		}
+		switch (w) {
+		case 1: l = (long)(*(unsigned char *)buf);
+		case 2: l = (long)(*(unsigned short *)buf);
+		case 4: l = (long)(*(unsigned long *)buf);
+		}
+		unmap_physmem(buf, w);
+		return l;
 	} else {
 		l = simple_strtoul(s, NULL, 16);
 	}

commit 322dc0df6d23573cd81e6ffa1d95d8baed9267c1

itest: add missing break statements to evalexp()
The commit mentioned below replaced return statements inside a switch so
that other code could be called after the switch. However, it didn't add
any break statements, causing the cases to run together. Fix this.

Reported-by: Coverity (CID 132282, 132283)
Fixes: 7861204c9af7 ("itest: make memory access work under sandbox")
Signed-off-by: Stephen Warren <swarren@nvidia.com>

common/cmd_itest.c
@@ -64,9 +64,15 @@ static long evalexp(char *s, int w)
 			return 0;
 		}
 		switch (w) {
-		case 1: l = (long)(*(unsigned char *)buf);
-		case 2: l = (long)(*(unsigned short *)buf);
-		case 4: l = (long)(*(unsigned long *)buf);
+		case 1:
+			l = (long)(*(unsigned char *)buf);
+			break;
+		case 2:
+			l = (long)(*(unsigned short *)buf);
+			break;
+		case 4:
+			l = (long)(*(unsigned long *)buf);
+			break;
 		}
 		unmap_physmem(buf, w);
 		return l;