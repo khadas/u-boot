fb0d4fb6f06efd4e21b46c9ebc4eead9d5697347
Gouwa <gouwa@khadas.com>
2017-01-14 20:02:14 +0800

    DDR: adjust the ddr frequency to match both Samsung/Skhynix DDR

--- board/khadas/configs/kvim.h
+++ board/khadas/configs/kvim.h
@@ -119,7 +119,7 @@
 
 /* ddr */
 #define CONFIG_DDR_SIZE					0 //MB //0 means ddr size auto-detect
-#define CONFIG_DDR_CLK					792  //MHz, Range: 384-1200, should be multiple of 24
+#define CONFIG_DDR_CLK					768  //MHz, Range: 384-1200, should be multiple of 24
 #define CONFIG_DDR4_CLK					1008  //MHz, for boards which use different ddr chip
 /* DDR type setting
  *    CONFIG_DDR_TYPE_LPDDR3   : LPDDR3
