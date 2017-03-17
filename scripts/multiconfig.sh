9e16f012b26cc24cb2b5033a303737c550e73286
terry <terry@szwesion.com>
2017-03-17 15:30:41 +0800

    KVim: add the Khadas vim SBC board support Fork from 'gxl_p212_v1' board in below commit: commit 1b2b2ca8710150b256d618082c38d159d9534a9d Author: Yonghui Yu <yonghui.yu@amlogic.com> Date:   Tue Feb 28 18:31:09 2017 +0800      PD#140026: dtb: compile multi dtb by default 	 Change-Id: I09f8095366ca702dd257308bf07602a17f2c7546

--- scripts/multiconfig.sh
+++ scripts/multiconfig.sh
@@ -122,6 +122,12 @@ do_board_defconfig () {
 	if [ ! -r $defconfig_path ]; then
 		defconfig_path=$srctree/board/amlogic/defconfigs/$1
 	fi
+
+	# khadas configs folder support
+	if [ ! -r $defconfig_path ]; then
+		defconfig_path=$srctree/board/khadas/defconfigs/$1
+	fi
+
 	# amlogic customer configs support
 	if [ ! -r $defconfig_path ]; then
 		defconfig_path=$srctree/customer/board/defconfigs/$1
