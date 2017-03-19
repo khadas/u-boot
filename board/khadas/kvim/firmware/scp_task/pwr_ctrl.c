76c3adde833e47282df143742f623ca186d7b034
terry <terry@szwesion.com>
2017-03-19 17:18:43 +0800

    Power: adjust the gpio for vcck control

--- board/khadas/kvim/firmware/scp_task/pwr_ctrl.c
+++ board/khadas/kvim/firmware/scp_task/pwr_ctrl.c
@@ -79,14 +79,14 @@ static void hdmi_5v_ctrl(unsigned int ctrl)
 static void vcck_ctrl(unsigned int ctrl)
 {
 	if (ctrl == ON) {
-		aml_update_bits(PREG_PAD_GPIO0_EN_N, 1 << 25, 0);
-		aml_update_bits(PREG_PAD_GPIO0_O, 1 << 25, 1 << 25);
+		aml_update_bits(PREG_PAD_GPIO3_EN_N, 1 << 15, 0);
+		aml_update_bits(PREG_PAD_GPIO3_O, 1 << 15, 1 << 15);
 		/* after power on vcck, should init vcck*/
 		_udelay(5000);
 		pwm_set_voltage(pwm_d, CONFIG_VCCK_INIT_VOLTAGE);
 	} else {
-		aml_update_bits(PREG_PAD_GPIO0_EN_N, 1 << 25, 0);
-		aml_update_bits(PREG_PAD_GPIO0_O, 1 << 25, 0);
+		aml_update_bits(PREG_PAD_GPIO3_EN_N, 1 << 15, 0);
+		aml_update_bits(PREG_PAD_GPIO3_O, 1 << 15, 0);
 	}
 }
 
