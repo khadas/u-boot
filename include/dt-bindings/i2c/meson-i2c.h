f5acfa71306485b34ce9f74bb6ce92923e75e02b
Jian Hu <jian.hu@amlogic.com>
2019-01-02 15:51:45 +0800

    i2c: g12: remove i2c deadcode [1/1]
    PD#SWPL-3460
    
    Problem:
    there is deadcode in g12a/g12b
    
    Solution:
    1.remove i2c deadcode
    2.add i2c master macro definition for i2c users
    
    Verify:
    verified on u200
    
    Change-Id: I42275d460c1bd818495d0fa914f47f4e6f588c38
    Signed-off-by: Jian Hu <jian.hu@amlogic.com>

---
+++ include/dt-bindings/i2c/meson-i2c.h
@@ -0,0 +1,18 @@
+/*
+ * This header provides constants for MESON SoCs I2c bindings.
+ *
+ * This file is licensed under the terms of the GNU General Public
+ * License version 2.  This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
+
+#ifndef _DT_BINDINGS_MESON_I2C_H
+#define _DT_BINDINGS_MESON_I2C_H
+
+#define MESON_I2C_M0         0
+#define MESON_I2C_M1         1
+#define MESON_I2C_M2         2
+#define MESON_I2C_M3         3
+#define MESON_I2C_M_AO       4
+
+#endif /* _DT_MESON_I2C_H */
