9142d024de4f13f7323d9637083eca3b8d73e478
xiaobo gu <xiaobo.gu@amlogic.com>
2019-05-25 22:49:23 +0800

    a1: add new config ad401 [1/1]
    PD#SWPL-9010
    
    Problem:
    a1 reference board ad401
    
    Solution:
    add config ad401
    
    Verify:
    test pass on ad401
    
    Change-Id: I93d1f27cb3c9b6d4cc29c9872351b2ab0c46e105
    Signed-off-by: xiaobo gu <xiaobo.gu@amlogic.com>

---
+++ board/amlogic/a1_ad401/a1_ad401.c
@@ -0,0 +1,160 @@
+
+/*
+ * board/amlogic/txl_skt_v1/txl_skt_v1.c
+ *
+ * Copyright (C) 2015 Amlogic, Inc. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include <common.h>
+#include <asm/io.h>
+#include <malloc.h>
+#include <errno.h>
+#include <environment.h>
+#include <fdt_support.h>
+#include <linux/libfdt.h>
+#include <asm/arch/cpu_id.h>
+#include <asm/arch/secure_apb.h>
+#include <asm/arch/pinctrl_init.h>
+#include <linux/sizes.h>
+#include <asm-generic/gpio.h>
+#include <dm.h>
+#include <asm/armv8/mmu.h>
+#include <amlogic/aml_v3_burning.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+void sys_led_init(void)
+{
+}
+
+int serial_set_pin_port(unsigned long port_base)
+{
+    return 0;
+}
+
+int dram_init(void)
+{
+	gd->ram_size = PHYS_SDRAM_1_SIZE;
+	return 0;
+}
+
+/* secondary_boot_func
+ * this function should be write with asm, here, is is only for compiling pass
+ * */
+void secondary_boot_func(void)
+{
+}
+
+int board_eth_init(bd_t *bis)
+{
+	return 0;
+}
+
+int active_a1_clk(void)
+{
+	struct udevice *a1_clk = NULL;
+	int err;
+
+	err = uclass_get_device_by_name(UCLASS_CLK,
+			"xtal-clk", &a1_clk);
+	if (err) {
+		pr_err("Can't find xtal-clk clock (%d)\n", err);
+		return err;
+	}
+	err = uclass_get_device_by_name(UCLASS_CLK,
+			"clock-controller@0", &a1_clk);
+	if (err) {
+		pr_err("Can't find clock-controller@0 clock (%d)\n", err);
+		return err;
+	}
+
+	return 0;
+}
+
+int board_init(void)
+{
+	printf("board init\n");
+#if defined(CONFIG_AML_V3_FACTORY_BURN) && defined(CONFIG_AML_V3_USB_TOOl)
+	if ((0x1b8ec003 != readl(SYSCTRL_STICKY_REG2)) && (0x1b8ec004 != readl(SYSCTRL_STICKY_REG2)))
+	{ aml_v3_factory_usb_burning(0, gd->bd); }
+#endif//#if defined(CONFIG_AML_V3_FACTORY_BURN) && defined(CONFIG_AML_V3_USB_TOOl)
+	pinctrl_devices_active(PIN_CONTROLLER_NUM);
+	active_a1_clk();
+
+	return 0;
+}
+
+int board_late_init(void)
+{
+	printf("board late init\n");
+
+	return 0;
+}
+
+
+phys_size_t get_effective_memsize(void)
+{
+	// >>16 -> MB, <<20 -> real size, so >>16<<20 = <<4
+#if defined(CONFIG_SYS_MEM_TOP_HIDE)
+	return (((readl(SYSCTRL_SEC_STATUS_REG4)) & 0xFFFF0000) << 4) - CONFIG_SYS_MEM_TOP_HIDE;
+#else
+	return (((readl(SYSCTRL_SEC_STATUS_REG4)) & 0xFFFF0000) << 4);
+#endif
+}
+
+static struct mm_region bd_mem_map[] = {
+	{
+		.virt = 0x00000000UL,
+		.phys = 0x00000000UL,
+		.size = 0x80000000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
+			 PTE_BLOCK_INNER_SHARE
+	}, {
+		.virt = 0x80000000UL,
+		.phys = 0x80000000UL,
+		.size = 0x80000000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
+			 PTE_BLOCK_NON_SHARE |
+			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
+	}, {
+		/* List terminator */
+		0,
+	}
+};
+
+struct mm_region *mem_map = bd_mem_map;
+
+int mach_cpu_init(void) {
+	printf("\nmach_cpu_init\n");
+	return 0;
+}
+
+int ft_board_setup(void *blob, bd_t *bd)
+{
+	/* eg: bl31/32 rsv */
+	return 0;
+}
+
+int __attribute__((weak)) mmc_initialize(bd_t *bis){ return 0;}
+
+int __attribute__((weak)) do_bootm(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]){ return 0;}
+
+void __attribute__((weak)) set_working_fdt_addr(ulong addr) {}
+
+int __attribute__((weak)) ofnode_read_u32_default(ofnode node, const char *propname, u32 def) {return 0;}
+
+void __attribute__((weak)) md5_wd (unsigned char *input, int len, unsigned char output[16],	unsigned int chunk_sz){}
