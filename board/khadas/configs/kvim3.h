6aa8c647a70217d86d68be68fe7515251f250078
Nick Xie <nick@khadas.com>
2019-06-18 16:12:43 +0800

    KVIM3: update initargs
    Signed-off-by: Nick Xie <nick@khadas.com>

--- board/khadas/configs/kvim3.h
+++ board/khadas/configs/kvim3.h
@@ -127,7 +127,7 @@
         "reboot_mode_android=""normal""\0"\
         "fs_type=""rootfstype=ramfs""\0"\
         "initargs="\
-            "init=/init console=ttyS0,115200 no_console_suspend earlycon=aml-uart,0xff803000 ramoops.pstore_en=1 ramoops.record_size=0x8000 ramoops.console_size=0x4000 "\
+            "root=LABEL=ROOTFS rootflags=data=writeback rw console=ttyS0,115200n8 console=tty0 no_console_suspend consoleblank=0 fsck.repair=yes net.ifnames=0 "\
             "\0"\
         "upgrade_check="\
             "echo upgrade_step=${upgrade_step}; "\
