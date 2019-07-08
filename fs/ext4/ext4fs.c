f38ef35e0f7f02b6bf9d4c19ecc39a8de8f678ff
Paul Emge <paulemge@forallsecure.com>
2019-07-08 16:37:07 -0700

    BL33: CVE: CVE-2019-13106 patch merge [1/1]
    PD#SWPL-77526
    
    Problem:
    BL33 CVE patch merge
    
    Solution:
    CVE-2019-13106: ext4: fix out-of-bounds memset
    
    In ext4fs_read_file in ext4fs.c, a memset can overwrite the bounds of
    the destination memory region. This patch adds a check to disallow
    this.
    
    Signed-off-by: Paul Emge <paulemge@forallsecure.com>
    
    Verify:
    build pass
    
    Change-Id: I77c584f080461c4282038109f6aba98f9805c681
    Signed-off-by: Zhongfu Luo <zhongfu.luo@amlogic.com>

--- fs/ext4/ext4fs.c
+++ fs/ext4/ext4fs.c
@@ -61,6 +61,7 @@ int ext4fs_read_file(struct ext2fs_node *node, loff_t pos,
 	lbaint_t delayed_skipfirst = 0;
 	lbaint_t delayed_next = 0;
 	char *delayed_buf = NULL;
+	char *start_buf = buf;
 	short status;
 
 	if (blocksize <= 0)
@@ -130,6 +131,7 @@ int ext4fs_read_file(struct ext2fs_node *node, loff_t pos,
 			}
 		} else {
 			int n;
+			int n_left;
 			if (previous_block_number != -1) {
 				/* spill */
 				status = ext4fs_devread(delayed_start,
@@ -142,8 +144,9 @@ int ext4fs_read_file(struct ext2fs_node *node, loff_t pos,
 			}
 			/* Zero no more than `len' bytes. */
 			n = blocksize - skipfirst;
-			if (n > len)
-				n = len;
+			n_left = len - (buf - start_buf);
+			if (n > n_left)
+				n = n_left;
 			memset(buf, 0, n);
 		}
 		buf += blocksize - skipfirst;
