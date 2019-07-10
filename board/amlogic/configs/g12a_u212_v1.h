3872860575168cc2d609b1a8cef4b34f9945a00e
Zhigang Yu <zhigang.yu@amlogic.com>
2019-07-10 11:06:58 +0800

    franklin: uboot env lost when recovery upgrade [1/1]
    PD#SWPL-11086
    
    Problem:
    recovery dtb check can not find aml_dt
    
    Solution:
    fixed by modify bootargs
    
    Verify:
    verify by u212
    
    Change-Id: I2d0e6cb58828629f1cdf792f8ef48d42003ccbb3
    Signed-off-by: Zhigang Yu <zhigang.yu@amlogic.com>

--- board/amlogic/configs/g12a_u212_v1.h
+++ board/amlogic/configs/g12a_u212_v1.h
@@ -244,10 +244,10 @@
             "get_valid_slot;"\
             "echo active_slot: ${active_slot};"\
             "if test ${active_slot} = normal; then "\
-                "setenv bootargs ${bootargs} ${bootargs} aml_dt=${aml_dt} recovery_part={recovery_part} recovery_offset={recovery_offset};"\
+                "setenv bootargs ${bootargs} ${fs_type} aml_dt=${aml_dt} recovery_part={recovery_part} recovery_offset={recovery_offset};"\
                 "if imgread kernel ${recovery_part} ${loadaddr} ${recovery_offset}; then wipeisb; bootm ${loadaddr}; fi;"\
             "else "\
-                "setenv bootargs ${bootargs} ${bootargs} aml_dt=${aml_dt} recovery_part=${boot_part} recovery_offset=${recovery_offset};"\
+                "setenv bootargs ${bootargs} ${fs_type} aml_dt=${aml_dt} recovery_part=${boot_part} recovery_offset=${recovery_offset};"\
                 "if imgread kernel ${boot_part} ${loadaddr}; then bootm ${loadaddr}; fi;"\
             "fi;"\
             "\0"\
