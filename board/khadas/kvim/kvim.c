d5b5ad8b0fd679467604091e6b810e94a8a2c6f7
Waylon <waylon@khadas.com>
2019-07-25 11:34:32 +0800

    KVIM: add I2C B-bus support [VIM]
    Signed-off-by: Waylon <waylon@khadas.com>

--- board/khadas/kvim/kvim.c
+++ board/khadas/kvim/kvim.c
@@ -279,43 +279,30 @@ int board_mmc_init(bd_t	*bis)
 }
 
 #ifdef CONFIG_SYS_I2C_AML
-#if 0
 static void board_i2c_set_pinmux(void){
-	/*********************************************/
-	/*                | I2C_Master_AO        |I2C_Slave            |       */
-	/*********************************************/
-	/*                | I2C_SCK                | I2C_SCK_SLAVE  |      */
-	/* GPIOAO_4  | [AO_PIN_MUX: 6]     | [AO_PIN_MUX: 2]   |     */
-	/*********************************************/
-	/*                | I2C_SDA                 | I2C_SDA_SLAVE  |     */
-	/* GPIOAO_5  | [AO_PIN_MUX: 5]     | [AO_PIN_MUX: 1]   |     */
-	/*********************************************/
-
-	//disable all other pins which share with I2C_SDA_AO & I2C_SCK_AO
-	clrbits_le32(P_AO_RTI_PIN_MUX_REG, ((1<<2)|(1<<24)|(1<<1)|(1<<23)));
-	//enable I2C MASTER AO pins
-	setbits_le32(P_AO_RTI_PIN_MUX_REG,
-	(MESON_I2C_MASTER_AO_GPIOAO_4_BIT | MESON_I2C_MASTER_AO_GPIOAO_5_BIT));
+	// Disable UART_CTS_B / UART_RTS_B
+	clrbits_le32(P_PERIPHS_PIN_MUX_2, ((1<<14)|(1<<13)));
+	// Enable I2C_SCK_B / I2C_SDA_B
+	setbits_le32(P_PERIPHS_PIN_MUX_1,
+	(MESON_I2C_MASTER_B_GPIODV_27_BIT | MESON_I2C_MASTER_B_GPIODV_26_BIT));
 
 	udelay(10);
 };
-#endif
 struct aml_i2c_platform g_aml_i2c_plat = {
 	.wait_count         = 1000000,
 	.wait_ack_interval  = 5,
 	.wait_read_interval = 5,
 	.wait_xfer_interval = 5,
-	.master_no          = AML_I2C_MASTER_AO,
+	.master_no          = AML_I2C_MASTER_B,
 	.use_pio            = 0,
-	.master_i2c_speed   = AML_I2C_SPPED_400K,
-	.master_ao_pinmux = {
-		.scl_reg    = (unsigned long)MESON_I2C_MASTER_AO_GPIOAO_4_REG,
-		.scl_bit    = MESON_I2C_MASTER_AO_GPIOAO_4_BIT,
-		.sda_reg    = (unsigned long)MESON_I2C_MASTER_AO_GPIOAO_5_REG,
-		.sda_bit    = MESON_I2C_MASTER_AO_GPIOAO_5_BIT,
+	.master_i2c_speed   = AML_I2C_SPPED_100K,
+	.master_b_pinmux = {
+		.scl_reg    = (unsigned long)MESON_I2C_MASTER_B_GPIODV_27_REG,
+		.scl_bit    = MESON_I2C_MASTER_B_GPIODV_27_BIT,
+		.sda_reg    = (unsigned long)MESON_I2C_MASTER_B_GPIODV_26_REG,
+		.sda_bit    = MESON_I2C_MASTER_B_GPIODV_26_BIT,
 	}
 };
-#if 0
 static void board_i2c_init(void)
 {
 	//set I2C pinmux with PCB board layout
@@ -323,11 +310,11 @@ static void board_i2c_init(void)
 
 	//Amlogic I2C controller initialized
 	//note: it must be call before any I2C operation
+	i2c_plat_init();
 	aml_i2c_init();
 
 	udelay(10);
 }
-#endif
 #endif
 #endif
 
@@ -397,6 +384,9 @@ int board_init(void)
 #ifdef CONFIG_AML_NAND
 	extern int amlnf_init(unsigned char flag);
 	amlnf_init(0);
+#endif
+#ifdef CONFIG_SYS_I2C_AML
+	board_i2c_init();
 #endif
 	return 0;
 }
