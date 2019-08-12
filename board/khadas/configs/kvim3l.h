0686a25ddcf2ecb7435b1c218927d9a2196f94d9
terry <terry@szwesion.com>
2019-08-12 16:10:36 +0800

    PortMode: init port mode for USB3.0 and PCIE [VIM3L]

--- board/khadas/configs/kvim3l.h
+++ board/khadas/configs/kvim3l.h
@@ -123,6 +123,7 @@
         "lock=10001000\0"\
         "active_slot=normal\0"\
         "boot_part=boot\0"\
+        "port_mode=0\0"\
         "reboot_mode_android=""normal""\0"\
         "Irq_check_en=0\0"\
         "fs_type=""rootfstype=ramfs""\0"\
@@ -282,6 +283,17 @@
              "kbi poweroff;"\
              "fi;"\
              "\0"\
+        "port_mode_change="\
+             "fdt addr ${dtb_mem_addr}; "\
+             "kbi portmode r;"\
+             "if test ${port_mode} = 0; then "\
+                "fdt set /usb3phy@ffe09080 portnum <1>;"\
+                "fdt set /pcieA@fc000000 status disable;"\
+             "else "\
+                "fdt set /usb3phy@ffe09080 portnum <0>;"\
+                "fdt set /pcieA@fc000000 status okay;"\
+             "fi;"\
+             "\0"\
         "cmdline_keys="\
             "if keyman init 0x1234; then "\
                 "if keyman read usid ${loadaddr} str; then "\
@@ -331,6 +343,7 @@
             "run wol_init;"\
             "run storeargs;"\
             "run upgrade_key;" \
+            "run port_mode_change;"\
             "forceupdate;" \
             "bcb uboot-command;"\
             "run switch_bootmode;"
