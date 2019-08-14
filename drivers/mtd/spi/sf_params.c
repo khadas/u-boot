1ef1911493646e0c759797cabd3bbb57a0ee8cd2
Sunny Luo <sunny.luo@amlogic.com>
2019-08-14 20:00:30 +0800

    spi-nor: add GD25Q80C support [1/1]
    PD#TV-6677
    
    Problem:
    Customer requirement
    
    Solution:
    add GD25Q80C in NOR support list
    
    Verify:
    tl1 x301
    
    Change-Id: Iacc9fc7a97d1b392fcb5b7dcacc7b7a593b2f824
    Signed-off-by: Sunny Luo <sunny.luo@amlogic.com>

--- drivers/mtd/spi/sf_params.c
+++ drivers/mtd/spi/sf_params.c
@@ -31,6 +31,7 @@ const struct spi_flash_params spi_flash_params_table[] = {
 	{"EN25S64",	   0x1c3817, 0x0,	64 * 1024,   128, RD_NORM,			  0},
 #endif
 #ifdef CONFIG_SPI_FLASH_GIGADEVICE	/* GIGADEVICE */
+	{"GD25Q80C",	   0xc84014, 0x0,	64 * 1024,    16, RD_NORM,		    SECT_4K},
 	{"GD25Q64B",	   0xc84017, 0x0,	64 * 1024,   128, RD_NORM,		    SECT_4K},
 	{"GD25LQ32",	   0xc86016, 0x0,	64 * 1024,    64, RD_NORM,		    SECT_4K},
 #endif
