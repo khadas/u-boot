d1fdbb9f214917c858e1cf91780ea1bba38fc002
Nick Xie <nick@khadas.com>
2019-08-15 11:34:24 +0800

    KVIM1/KVIM2: disable CONFIG_OF_LIBFDT_OVERLAY
    Signed-off-by: Nick Xie <nick@khadas.com>

--- board/khadas/configs/kvim2.h
+++ board/khadas/configs/kvim2.h
@@ -52,6 +52,10 @@
 #define CONFIG_KHADAS_KBI 1
 #define CONFIG_KHADAS_CFGLOAD 1
 
+#ifdef CONFIG_OF_LIBFDT_OVERLAY
+#undef CONFIG_OF_LIBFDT_OVERLAY
+#endif
+
 /* support ext4*/
 #define CONFIG_CMD_EXT4 1
 
