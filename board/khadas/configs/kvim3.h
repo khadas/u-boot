8fdc9042e02ed7eaf5555c85295b008693c42aa3
Nick Xie <nick@khadas.com>
2019-08-15 17:32:22 +0800

    cfgload: use boot.ini to bootup the system
    Signed-off-by: Nick Xie <nick@khadas.com>

--- board/khadas/configs/kvim3.h
+++ board/khadas/configs/kvim3.h
@@ -174,6 +174,7 @@
             "\0" \
         "storeboot="\
             "boot_cooling;"\
+            "cfgload;" \
             "if load mmc 0:1 1020000 s905_autoscript || load mmc 1:1 1020000 s905_autoscript || load mmc 1:5 1020000 /boot/s905_autoscript; then autoscr 1020000; fi;"\
             "ext4load mmc 1:5 1080000 zImage;ext4load mmc 1:5 10000000 uInitrd;ext4load mmc 1:5 20000000 dtb.img;booti 1080000 10000000 20000000;"\
             "run update;"\
