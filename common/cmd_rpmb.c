a153e24658d72079ad788bd031a032356c5ea69b
Zhongfu Luo <zhongfu.luo@amlogic.com>
2019-09-20 14:13:37 +0800

    rpmb: bootloader coverity fix - uboot v2015 - cmd rpmb [1/1]
    PD#SWPL-14243
    
    Problem:
    bootloader coverity fix - uboot v2015 - cmd rpmb
    
    Solution:
    bootloader coverity fix - uboot v2015 - cmd rpmb
    
    Verify:
    G12A skt board verify pass
    
    Change-Id: I7c1b3992f8c43cfcbb6f9a81ed8e6d3823a20d9b
    Signed-off-by: Zhongfu Luo <zhongfu.luo@amlogic.com>

--- common/cmd_rpmb.c
+++ common/cmd_rpmb.c
@@ -33,37 +33,25 @@ static int do_rpmb_state(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[
 #define AML_RPMB_STATE (((readl(AO_SEC_GP_CFG7))>>22) & 0x01)
 
 	char *pARG = getenv("bootargs");
+	char env_cmd[128] = {0};
 
 	if (pARG)
 	{
-		//printf("1 bootargs=%s\n",pARG);
-
-		char *szBuffer=malloc(strlen(pARG)+64);
-		nReturn = __LINE__;
-
-		if (szBuffer)
+		char *pFind = strstr(pARG,"androidboot.rpmb_state");
+		if (!pFind)
 		{
-			memset(szBuffer+strlen(pARG),0,64);
-			strcpy(szBuffer,pARG);
-			char *pFind = strstr(szBuffer,"androidboot.rpmb_state");
-			if (!pFind)
-				sprintf(szBuffer,"%s androidboot.rpmb_state=%d",pARG,AML_RPMB_STATE);
-			else
-				pFind[23] = AML_RPMB_STATE ? '1':'0';
-
-			//printf("2 bootargs=%s\n",szBuffer);
-
-			setenv("rpmb_state",AML_RPMB_STATE?"1":"0"); //need this?
-			setenv("bootargs",szBuffer);
-			free(szBuffer);
-			szBuffer = 0;
-
-			nReturn = 0;
+			sprintf(env_cmd,"setenv bootargs ${bootargs} androidboot.rpmb_state=%d",AML_RPMB_STATE);
+			run_command(env_cmd,0);
 		}
 		else
-			printf("aml log : internal sys error!\n");
+			pFind[23] = AML_RPMB_STATE ? '1':'0';
 
+		setenv("rpmb_state",AML_RPMB_STATE?"1":"0");
+
+		nReturn = 0;
 	}
+	else
+		printf("aml log : internal sys error!\n");
 
 	return nReturn;
 }
