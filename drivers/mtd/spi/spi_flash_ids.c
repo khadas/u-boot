c254f8bd649a2ca0fc2f3f1487d1046112d149e7
Sunny Luo <sunny.luo@amlogic.com>
2019-09-23 19:58:16 +0800

    SPI-NOR: add EN25QH128 support [1/1]
    PD#SWPL-14618
    
    Problem:
    Need support EN25QH128 spi nor flash in uboot
    
    Solution:
    add EN25QH128 in support list
    
    Verify:
    Verifying on a1
    
    Change-Id: Ibb19c030d1fb9ce4bee0600869ea434dc7fabd77
    Signed-off-by: Sunny Luo <sunny.luo@amlogic.com>

--- drivers/mtd/spi/spi_flash_ids.c
+++ drivers/mtd/spi/spi_flash_ids.c
@@ -60,6 +60,7 @@ const struct spi_flash_info spi_flash_ids[] = {
 	{"en25q64",	   INFO(0x1c3017, 0x0, 64 * 1024,   128, SECT_4K) },
 	{"en25q128b",	   INFO(0x1c3018, 0x0, 64 * 1024,   256, 0) },
 	{"en25s64",	   INFO(0x1c3817, 0x0, 64 * 1024,   128, 0) },
+	{"en25qh128",      INFO(0x1c7018, 0x0, 64 * 1024,   256, SECT_4K) },
 #endif
 #ifdef CONFIG_SPI_FLASH_GIGADEVICE	/* GIGADEVICE */
 	{"gd25q16c",	   INFO(0xc84015, 0x0, 64 * 1024,    32, RD_FULL | WR_QPP | SECT_4K) },
