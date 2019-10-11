36b8d13f457e441c7e1650c3c0f766c8e435fce8
Haixiang Bao <haixiang.bao@amlogic.com>
2019-10-11 17:12:14 +0800

    BL33: A1: secure boot solution work [4/4]
    PD#SWPL-8274
    
    Problem:
    A1 secure boot solution not ready.
    same issue for SWPL-8526 and SWPL-8532
    
    Solution:
    all aml-user-key.sig for A1 should be created by aml_encrypt_a1 again
    for AES key parts must be generated from raw DVGK.
    EFUSE pattern generate will depend on flag CONFIG_AML_EFUSE_GEN_AES_ONLY
    which set by BL33. Currently no AES key used for secure boot and no AES
    key related MRK generated for secure boot. Once the AES key is ready after
    RSA key burned then just enable CONFIG_AML_EFUSE_GEN_AES_ONLY to
    generate the pattern and burn it to SoC for AES encrypt for bootloader.
    
    Verify:
    secure boot with a1_ad409 skt board verify pass.
    
    Change-Id: Ibff244533260fccacbc4ca8c220009f586170967
    Signed-off-by: Haixiang Bao <haixiang.bao@amlogic.com>

--- arch/arm/include/asm/arch-a1/cpu.h
+++ arch/arm/include/asm/arch-a1/cpu.h
@@ -86,4 +86,12 @@
 /*A1 SoC has problem to support AES for secure boot, here just disable it*/
 //#define CONFIG_AML_CRYPTO_AES 1
 
+
+/*Flag for enable AES key generate to EFUSE pattern, with this flag only
+AES key will be generated to EFUSE pattern, it can be used for use case
+like the AES key not ready when secure boot enabled for the first time.
+*/
+
+//#define CONFIG_AML_EFUSE_GEN_AES_ONLY 1
+
 #endif /* _CPU_H */
