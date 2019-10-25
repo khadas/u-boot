b571f4dbf55e70e81ff59f562db18fc5829c3e2e
Sandy Luo <sandy.luo@amlogic.com>
2019-10-25 16:32:12 +0800

    Irq: add env Irq_check_en for tv chip [1/1]
    PD#SWPL-4196
    
    Problem:
    auto suspend rcu issue.
    
    Solution:
    add Irq_check_en, but default 0
    
    Verify:
    x301
    
    Change-Id: I19f4da41bb02a0a51af554d62e79ebd7d12108f0
    Signed-off-by: Sandy Luo <sandy.luo@amlogic.com>

--- board/amlogic/configs/tl1_x301_v1.h
+++ board/amlogic/configs/tl1_x301_v1.h
@@ -144,7 +144,7 @@
             "else fi;"\
             "\0"\
         "storeargs="\
-            "setenv bootargs ${initargs} otg_device=${otg_device} logo=${display_layer},loaded,${fb_addr} powermode=${powermode} vout=${outputmode},enable panel_type=${panel_type} lcd_ctrl=${lcd_ctrl} hdmimode=${hdmimode} cvbsmode=${cvbsmode} osd_reverse=${osd_reverse} video_reverse=${video_reverse} androidboot.selinux=${EnableSelinux} androidboot.firstboot=${firstboot} jtag=${jtag} mem_size=${mem_size} ; irq_check_en=${Irq_check_en} "\
+            "setenv bootargs ${initargs} otg_device=${otg_device} logo=${display_layer},loaded,${fb_addr} powermode=${powermode} vout=${outputmode},enable panel_type=${panel_type} lcd_ctrl=${lcd_ctrl} hdmimode=${hdmimode} cvbsmode=${cvbsmode} osd_reverse=${osd_reverse} video_reverse=${video_reverse} androidboot.selinux=${EnableSelinux} androidboot.firstboot=${firstboot} jtag=${jtag} mem_size=${mem_size} irq_check_en=${Irq_check_en} ; "\
             "setenv bootargs ${bootargs} androidboot.hardware=amlogic;"\
             "run cmdline_keys;"\
             "\0"\
