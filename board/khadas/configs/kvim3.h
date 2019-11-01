72a25bf0dd736ddc2b663a5c00590823c2e9007d
adamg <adamg88@users.noreply.github.com>
2019-11-01 04:49:54 +0000

    kvim3/kvim3l: try to read aml_autoscript from all eMMC partitions if all other boot methods fail

--- board/khadas/configs/kvim3.h
+++ board/khadas/configs/kvim3.h
@@ -177,6 +177,7 @@
             "cfgload;" \
             "if load mmc 0:1 1020000 s905_autoscript || load mmc 1:1 1020000 s905_autoscript || load mmc 1:5 1020000 /boot/s905_autoscript; then autoscr 1020000; fi;"\
             "ext4load mmc 1:5 1080000 zImage;ext4load mmc 1:5 10000000 uInitrd;ext4load mmc 1:5 20000000 dtb.img;booti 1080000 10000000 20000000;"\
+            "for p in 1 2 3 4 5 6 7 8 9 A B C D E F 10 11 12 13 14 15 16 17 18; do if fatload mmc 1:${p} ${loadaddr} aml_autoscript; then autoscr ${loadaddr}; fi; done;"\
             "run update;"\
             "\0"\
         "factory_reset_poweroff_protect="\
