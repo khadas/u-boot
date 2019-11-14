d2ea89236292c38f264b5fada14deb44d95d6617
Xindong Xu <xindong.xu@amlogic.com>
2019-11-14 11:21:10 +0800

    lcd: change partition name when ab update enabled [1/1]
    PD#TV-12072
    
    Problem:
    can not find tvconfig files when enable ab update
    
    Solution:
    change partition name when ab update enabled
    
    Verify:
    x301,marconi
    
    Change-Id: I027e7fba4b4ab3627cceb579e6a8de99c81e9aba
    Signed-off-by: Xindong Xu <xindong.xu@amlogic.com>

--- common/ini/ini_platform.c
+++ common/ini/ini_platform.c
@@ -28,6 +28,7 @@ Description:
 
 #include "ini_handler.h"
 #include "ini_platform.h"
+#include <partition_table.h>
 
 //c basic lib
 char* plat_strtok_r(char *str, const char *delim, char **saveptr) {
@@ -44,6 +45,7 @@ static int splitFilePath(const char *file_path, char part_name[], char file_name
     int i = 0;
     char *tmp_start_ptr = NULL;
     char *tmp_end_ptr = NULL;
+    char *slot_name;
 
     if (file_path == NULL) {
         ALOGE("%s, file_path is NULL!!!\n", __FUNCTION__);
@@ -65,6 +67,18 @@ static int splitFilePath(const char *file_path, char part_name[], char file_name
     strncpy(part_name, tmp_start_ptr + 1, tmp_end_ptr - tmp_start_ptr - 1);
     part_name[tmp_end_ptr - tmp_start_ptr - 1] = '\0';
     //ALOGD("%s, partition name is %s\n", __FUNCTION__, part_name);
+
+    if (has_boot_slot == 1) {
+        run_command("get_valid_slot", 0);
+        slot_name = getenv("slot-suffixes");
+        printf("slot-suffixes: %s\n", slot_name);
+        if (strcmp(slot_name, "0") == 0) {
+            strcat(part_name, "_a");
+        } else if (strcmp(slot_name, "1") == 0) {
+            strcat(part_name, "_b");
+        }
+        ALOGD("%s, partition name is %s\n", __FUNCTION__, part_name);
+    }
 
     tmp_start_ptr = tmp_end_ptr;
 
