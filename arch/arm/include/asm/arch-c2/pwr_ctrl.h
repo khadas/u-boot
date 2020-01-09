41de81731528dcd328baaba1f3afb4877b4f43ff
Haixiang Bao <haixiang.bao@amlogic.com>
2020-01-09 16:21:26 +0800

    BL33: C2: chip init bring up [5/6]
    PD#SWPL-19701
    
    Problem:
    C2 need bring up
    
    Solution:
    create C2_skt based on c1_ae409
    now C2 used C1's setting
    need update with C2's spec
    
    Verify:
    compile pass, Z1 can run to BL2, need fine tune
    
    Change-Id: I9929ef253fee1fd0fd7717f835fd1d1b24a291ab
    Signed-off-by: Haixiang Bao <haixiang.bao@amlogic.com>
    (cherry picked from commit 9b2a99e435aaa0717f6805229a3c1903d342e016)

---
+++ arch/arm/include/asm/arch-c2/pwr_ctrl.h
@@ -0,0 +1,47 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+/*
+ * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+ */
+
+#define PWR_ON    1
+#define PWR_OFF   0
+
+enum pm_e {
+	PM_CPU_PWR0,
+	PM_CPU_CORE0,
+	PM_CPU_CORE1,
+	PM_DSP_A = 8,
+	PM_DSP_B,
+	PM_UART,
+	PM_DMC,
+	PM_I2C,
+	PM_SDEMMC_B,
+	PM_ACODEC,
+	PM_AUDIO,
+	PM_MKL_OTP,
+	PM_DMA,
+	PM_SDEMMC_A,
+	PM_SRAM_A,
+	PM_SRAM_B,
+	PM_IR,
+	PM_SPICC,
+	PM_SPIFC,
+	PM_USB,
+	PM_NIC,
+	PM_PDM,
+	PM_RSA,
+	PM_MIPI_ISP,
+	PM_HCODEC,
+	PM_WAVE,
+	PM_SDEMMC_C,
+	PM_SRAM_C,
+	PM_GDC,
+	PM_GE2D,
+	PM_NNA,
+	PM_ETH,
+	PM_GIC,
+	PM_DDR,
+	PM_SPICC_B
+};
+
+unsigned long pwr_ctrl_psci_smc(enum pm_e power_domain, bool power_control);
