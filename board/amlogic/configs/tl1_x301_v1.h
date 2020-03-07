cf40e7a07e84ff48df938eb66b41198b0c0b9ef6
zhiguang.ouyang <zhiguang.ouyang@amlogic.com>
2020-03-07 16:38:50 +0800

    ddr: timing:ddr fastboot enable when power off bl33_v2015 [1/1]
    PD#SWPL-22113
    
    Problem:
    add ddr fastboot on the shutdown mode
    
    Solution:
    add default parameter at x301
    
    Verify:
    test pass at x301
    
    Change-Id: I6a192d6cb5ffeca4c1bb377d12e203a2e6460f02
    Signed-off-by: zhiguang.ouyang <zhiguang.ouyang@amlogic.com>

--- board/amlogic/configs/tl1_x301_v1.h
+++ board/amlogic/configs/tl1_x301_v1.h
@@ -395,6 +395,8 @@
 			"run update;\n" \
 			"fi;fi;" \
 		"fi;\0" \
+	"ddr_fast_boot_enable_flag=1\0" \
+	"enable_ddr_check_boot_reason=1\0" \
 
 #define CONFIG_PREBOOT  \
 	"mw ff638630 0 2;"\
@@ -409,7 +411,12 @@
 	"run reset_suspend;"
 
 
-#define CONFIG_BOOTCOMMAND "run storeboot"
+#define CONFIG_BOOTCOMMAND \
+	"echo ddr_fast_boot_enable_flag = ${ddr_fast_boot_enable_flag};"\
+	"if itest ${ddr_fast_boot_enable_flag} == 1; then "\
+		"ddr_auto_fast_boot_check 6;"\
+	"fi;"\
+	"run storeboot"
 
 //#define CONFIG_ENV_IS_NOWHERE  1
 #define CONFIG_ENV_SIZE   (64*1024)
