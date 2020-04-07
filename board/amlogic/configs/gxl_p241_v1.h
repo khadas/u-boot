ecc64e180ef70fbf518ad9a64240ffd34b7b576a
Gongwei Chen <gongwei.chen@amlogic.com>
2020-04-07 16:10:55 +0800

    hdr_policy: hdr_policy params is passed by bootargs. [1/2]
    PD#SWPL-22912
    
    Problem:
    when set hdr_policy to 1, but this value is 0 in boot init
    
    Solution:
    hdr_policy params is passed by bootargs.
    
    Verify:
    verify by newton.
    
    Change-Id: I53308002883c24c873882aa85be6cb3cb6d6eeb7
    Signed-off-by: Luan Yuan <luan.yuan@amlogic.com>

--- board/amlogic/configs/gxl_p241_v1.h
+++ board/amlogic/configs/gxl_p241_v1.h
@@ -136,7 +136,7 @@
             "\0"\
     "storeargs="\
             "get_bootloaderversion;" \
-            "setenv bootargs ${initargs} hdr_priority=${hdr_priority} otg_device=${otg_device} reboot_mode_android=${reboot_mode_android} androidboot.selinux=${EnableSelinux} logo=${display_layer},loaded,${fb_addr},${outputmode} maxcpus=${maxcpus} vout=${outputmode},enable hdmitx=${cecconfig},${colorattribute} hdmimode=${hdmimode} frac_rate_policy=${frac_rate_policy} cvbsmode=${cvbsmode} hdmitx=${cecconfig} cvbsdrv=${cvbs_drv} irq_check_en=${Irq_check_en}  androidboot.firstboot=${firstboot} jtag=${jtag}; "\
+            "setenv bootargs ${initargs} hdr_policy=${hdr_policy} hdr_priority=${hdr_priority} otg_device=${otg_device} reboot_mode_android=${reboot_mode_android} androidboot.selinux=${EnableSelinux} logo=${display_layer},loaded,${fb_addr},${outputmode} maxcpus=${maxcpus} vout=${outputmode},enable hdmitx=${cecconfig},${colorattribute} hdmimode=${hdmimode} frac_rate_policy=${frac_rate_policy} cvbsmode=${cvbsmode} hdmitx=${cecconfig} cvbsdrv=${cvbs_drv} irq_check_en=${Irq_check_en}  androidboot.firstboot=${firstboot} jtag=${jtag}; "\
 	"setenv bootargs ${bootargs} androidboot.hardware=amlogic androidboot.bootloader=${bootloader_version} androidboot.build.expect.baseband=N/A;"\
         "setenv bootargs ${bootargs} page_trace=${page_trace};" \
             "run cmdline_keys;"\
