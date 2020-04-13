464ca381ca75954a7d8ca2dae4a15a4f593a05e1
Zongdong Jiao <zongdong.jiao@amlogic.com>
2020-04-13 15:01:58 +0800

    hdmitx: fine tune 7-2 VL value under 480p [1/2]
    PD#SWPL-24086
    
    Problem:
    the 7-2 VL value is cirtical
    
    Solution:
    fine tune 7-2 VL value under 480p
    This fine tune is suitable for both ver A and B, and only related with
    the external Rext 1.5k ohm.
    
    Verify:
    tm2b
    
    Change-Id: Ifb63b5418762a084f918fdf04ed8952224b67583
    Signed-off-by: Zongdong Jiao <zongdong.jiao@amlogic.com>
    (cherry picked from commit 0bd511d7f3afe60d93f560eefaacbbcc52c24337)

--- arch/arm/cpu/armv8/tm2/hdmitx20/hdmitx_set.c
+++ arch/arm/cpu/armv8/tm2/hdmitx20/hdmitx_set.c
@@ -1183,14 +1183,14 @@ static void set_phy_by_mode(struct hdmitx_dev *hdev, unsigned int mode)
 		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL5, 0x0000080b);
 		break;
 	case HDMI_PHYPARA_3G: /* 2.97Gbps */
-		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL0, 0x31cb6262);
+		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL0, 0x33eb62a3);
 		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL3, 0x2ab0ff3b);
-		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL5, 0x00000002);
+		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL5, 0x00000003);
 		break;
-	case HDMI_PHYPARA_270M: /* 1.485Gbps, and below */
-	case HDMI_PHYPARA_DEF:
+	case HDMI_PHYPARA_270M: /* 270Mbps */
+	case HDMI_PHYPARA_DEF: /* 1.485Gbps, and others */
 	default:
-		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL0, 0x33eb6262);
+		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL0, 0x33eb5252);
 		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL3, 0x2ab0ff3b);
 		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL5, 0x00000003);
 		break;
@@ -1236,6 +1236,17 @@ static void hdmitx_set_phy(struct hdmitx_dev *hdev)
 			set_phy_by_mode(hdev, HDMI_PHYPARA_3G);
 		else
 			set_phy_by_mode(hdev, HDMI_PHYPARA_4p5G);
+		break;
+	case HDMI_640x480p60_4x3:
+	case HDMI_720x480p60_4x3:
+	case HDMI_720x480p60_16x9:
+	case HDMI_720x480i60_4x3:
+	case HDMI_720x480i60_16x9:
+	case HDMI_720x576p50_4x3:
+	case HDMI_720x576p50_16x9:
+	case HDMI_720x576i50_4x3:
+	case HDMI_720x576i50_16x9:
+		set_phy_by_mode(hdev, HDMI_PHYPARA_270M);
 		break;
 	case HDMI_1920x1080p60_16x9:
 	case HDMI_1920x1080p50_16x9:
