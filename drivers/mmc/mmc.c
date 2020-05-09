783f3779de07f7defbdbebf5a0cbe5bb6929b6f3
Nan Li <nan.li@amlogic.com>
2020-05-09 09:58:22 +0800

    emmc: driver: optimize emmc tuning num per point [1/1]
    PD#SWPL-25641
    
    Problem:
    uboot emmc tuning times is 10.
    
    Solution:
    modify emmc tuning times to 40 as the same as kernel.
    
    Verify:
    C328x
    
    Change-Id: I084166f1d29dc6984f27f99a6744322eb69327ac
    Signed-off-by: Nan Li <nan.li@amlogic.com>

--- drivers/mmc/mmc.c
+++ drivers/mmc/mmc.c
@@ -1679,7 +1679,9 @@ static const struct mode_width_tuning sd_modes_by_pref[] = {
 	{
 		.mode = SD_HS,
 		.widths = MMC_MODE_4BIT | MMC_MODE_1BIT,
+#ifdef MMC_SUPPORTS_TUNING
 		.tuning = MMC_SD_HS_TUNING
+#endif
 	},
 #if CONFIG_IS_ENABLED(MMC_UHS_SUPPORT)
 	{
@@ -1885,7 +1887,9 @@ static const struct mode_width_tuning mmc_modes_by_pref[] = {
 	{
 		.mode = MMC_HS_52,
 		.widths = MMC_MODE_8BIT | MMC_MODE_4BIT | MMC_MODE_1BIT,
+#ifdef MMC_SUPPORTS_TUNING
 		.tuning = MMC_SD_HS_TUNING
+#endif
 	},
 	{
 		.mode = MMC_HS,
