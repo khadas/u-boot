cf3d8f5ade83c804acdee59f26a2c04098a31d6d
Qianggui Song <qianggui.song@amlogic.com>
2020-05-22 11:12:33 +0800

    pinctrl: support c2 pinctrl & gpio function [1/1]
    PD#SWPL-27066
    
    Problem:
    c2 need support gpio & pinctrl
    
    Solution:
    add c2 pinctrl driver code
    
    Verify:
    on c2_af409
    
    Change-Id: I947082bf328c4d816b6e592b64bfec631b01b067
    Signed-off-by: Qianggui Song <qianggui.song@amlogic.com>

--- include/dt-bindings/gpio/meson-c2-gpio.h
+++ include/dt-bindings/gpio/meson-c2-gpio.h
@@ -2,9 +2,9 @@
 /*
  * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
  */
+
 #ifndef _DT_BINDINGS_MESON_C2_GPIO_H
 #define _DT_BINDINGS_MESON_C2_GPIO_H
-
 #define	GPIOD_0		0
 #define	GPIOD_1		1
 #define	GPIOD_2		2
@@ -18,71 +18,71 @@
 #define	GPIOD_10	10
 #define	GPIOD_11	11
 #define	GPIOD_12	12
+#define	GPIOD_13	13
 
-#define	GPIOE_0		13
-#define	GPIOE_1		14
+#define	GPIOE_0		14
+#define	GPIOE_1		15
+#define	GPIOE_2		16
 
-#define	GPIOB_0		15
-#define	GPIOB_1		16
-#define	GPIOB_2		17
-#define	GPIOB_3		18
-#define	GPIOB_4		19
-#define	GPIOB_5		20
-#define	GPIOB_6		21
-#define	GPIOB_7		22
-#define	GPIOB_8		23
-#define	GPIOB_9		24
-#define	GPIOB_10	25
-#define	GPIOB_11	26
-#define	GPIOB_12	27
-#define	GPIOB_13	28
+#define	GPIOB_0		17
+#define	GPIOB_1		18
+#define	GPIOB_2		19
+#define	GPIOB_3		20
+#define	GPIOB_4		21
+#define	GPIOB_5		22
+#define	GPIOB_6		23
+#define	GPIOB_7		24
+#define	GPIOB_8		25
+#define	GPIOB_9		26
+#define	GPIOB_10	27
+#define	GPIOB_11	28
+#define	GPIOB_12	29
+#define	GPIOB_13	30
 
-#define	GPIOC_0		29
-#define	GPIOC_1		30
-#define	GPIOC_2		31
-#define	GPIOC_3		32
-#define	GPIOC_4		33
-#define	GPIOC_5		34
-#define	GPIOC_6		35
-#define	GPIOC_7		36
+#define	GPIOC_0		31
+#define	GPIOC_1		32
+#define	GPIOC_2		33
+#define	GPIOC_3		34
+#define	GPIOC_4		35
+#define	GPIOC_5		36
+#define	GPIOC_6		37
 
-#define	GPIOX_0		37
-#define	GPIOX_1		38
-#define	GPIOX_2		39
-#define	GPIOX_3		40
-#define	GPIOX_4		41
-#define	GPIOX_5		42
-#define	GPIOX_6		43
-#define	GPIOX_7		44
-#define	GPIOX_8		45
-#define	GPIOX_9		46
-#define	GPIOX_10	47
-#define	GPIOX_11	48
-#define	GPIOX_12	49
-#define	GPIOX_13	50
-#define	GPIOX_14	51
-#define	GPIOX_15	52
-#define	GPIOX_16	53
-#define	GPIOX_17	54
-#define	GPIOX_18	55
-#define	GPIOX_19	56
+#define	GPIOX_0		38
+#define	GPIOX_1		39
+#define	GPIOX_2		40
+#define	GPIOX_3		41
+#define	GPIOX_4		42
+#define	GPIOX_5		43
+#define	GPIOX_6		44
+#define	GPIOX_7		45
+#define	GPIOX_8		46
+#define	GPIOX_9		47
+#define	GPIOX_10	48
+#define	GPIOX_11	49
+#define	GPIOX_12	50
+#define	GPIOX_13	51
+#define	GPIOX_14	52
+#define	GPIOX_15	53
+#define	GPIOX_16	54
+#define	GPIOX_17	55
+#define	GPIOX_18	56
+#define	GPIOX_19	57
+#define	GPIOX_20	58
 
-#define	GPIOM_0		57
-#define	GPIOM_1		58
-#define	GPIOM_2		59
-#define	GPIOM_3		60
-#define	GPIOM_4		61
-#define	GPIOM_5		62
-#define	GPIOM_6		63
-#define	GPIOM_7		64
-#define	GPIOM_8		65
-#define	GPIOM_9		66
-#define	GPIOM_10	67
-#define	GPIOM_11	68
-#define	GPIOM_12	69
-#define	GPIOM_13	70
-#define	GPIOM_14	71
-#define	GPIOM_15	72
+#define	GPIOM_0		59
+#define	GPIOM_1		60
+#define	GPIOM_2		61
+#define	GPIOM_3		62
+#define	GPIOM_4		63
+#define	GPIOM_5		64
+#define	GPIOM_6		65
+#define	GPIOM_7		66
+#define	GPIOM_8		67
+#define	GPIOM_9		68
+#define	GPIOM_10	69
+#define	GPIOM_11	70
+#define	GPIOM_12	71
+#define	GPIOM_13	72
 
 #define	GPIOA_0		73
 #define	GPIOA_1		74
