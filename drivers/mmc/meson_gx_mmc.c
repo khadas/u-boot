21b4914b5a66dde1df735701d6e95b62616ae1e7
Nan Li <nan.li@amlogic.com>
2020-05-27 14:58:04 +0800

    emmc: add emmc support for sc2 [1/1]
    PD#SWPL-26885
    
    Problem:
    emmc/sd is not supported for sc2.
    
    Solution:
    add emmc/sd supported for sc2.
    rebase from below two patch:
    https://scgit.amlogic.com/#/c/110985/
    https://scgit.amlogic.com/#/c/113292/
    
    Verify:
    sc2_z1/sc2_ptm
    
    Change-Id: I5ba70ae10aef7b9574f51ceadbb6e24d8a44123c
    Signed-off-by: Nan Li <nan.li@amlogic.com>
    Signed-off-by: Honglin Zhang <honglin.zhang@amlogic.com>

--- drivers/mmc/meson_gx_mmc.c
+++ drivers/mmc/meson_gx_mmc.c
@@ -16,6 +16,8 @@
 #include <linux/log2.h>
 #include <dm/pinctrl.h>
 #include "mmc_private.h"
+#include <asm/arch/register.h>
+#include <amlogic/cpu_id.h>
 
 static inline void *get_regbase(const struct mmc *mmc)
 {
@@ -107,6 +109,7 @@ static void meson_mmc_config_clock(struct meson_host *host)
 	uint32_t clk = 0, clk_src = 0, clk_div = 0;
 	uint32_t co_phase = 0, tx_phase = 0;
 	uint32_t meson_mmc_clk = 0;
+	cpu_id_t cpu_id = get_cpu_id();
 
 	if (!mmc->clock)
 		return;
@@ -114,14 +117,19 @@ static void meson_mmc_config_clock(struct meson_host *host)
 	if (mmc->clock > 12000000) {
 		clk = 1000000000;
 		clk_src = 1;
+	if (cpu_id.family_id != 0x32) {
 		clk_disable(&host->xtal);
 		clk_set_parent(&host->mux, &host->div2);
 		clk_set_rate(&host->div, clk);
+		}
 	} else {
 		clk = 24000000;
 		clk_src = 0;
+	if (cpu_id.family_id != 0x32)
 		clk_enable(&host->xtal);
 	}
+	if (cpu_id.family_id == 0x32)
+	clk_div = (clk / mmc->clock) + (!!(clk % mmc->clock));
 
 	//printf("sd_emmc_clk_ctrl:0x%x\n", readl(((0x0038<<2) + 0xfe000800)));
 	//printf("sd_emmc_clk_ctrl1:0x%x\n", readl(((0x0048<<2) + 0xfe000800)));
@@ -147,6 +155,8 @@ static void meson_mmc_config_clock(struct meson_host *host)
 			dev_read_u32(mmc->dev, "hs_tx_phase", &tx_phase);
 			break;
 		case SD_HS:
+			dev_read_u32(mmc->dev, "sd_hs_co_phase", &co_phase);
+			dev_read_u32(mmc->dev, "sd_hs_tx_phase", &tx_phase);
 			break;
 		case MMC_HS_200:
 			dev_read_u32(mmc->dev, "hs2_co_phase", &co_phase);
@@ -206,18 +216,6 @@ static int meson_dm_mmc_set_ios(struct udevice *dev)
 		meson_mmc_cfg |= CFG_BUS_WIDTH_8;
 	else
 		return -EINVAL;
-
-	/* 512 bytes block length */
-	meson_mmc_cfg &= ~CFG_BL_LEN_MASK;
-	meson_mmc_cfg |= CFG_BL_LEN_512;
-
-	/* Response timeout 256 clk */
-	meson_mmc_cfg &= ~CFG_RESP_TIMEOUT_MASK;
-	meson_mmc_cfg |= CFG_RESP_TIMEOUT_256;
-
-	/* Command-command gap 16 clk */
-	meson_mmc_cfg &= ~CFG_RC_CC_MASK;
-	meson_mmc_cfg |= CFG_RC_CC_16;
 
 	meson_write(mmc, meson_mmc_cfg, MESON_SD_EMMC_CFG);
 
@@ -942,6 +940,8 @@ static int meson_mmc_ofdata_to_platdata(struct udevice *dev)
 	struct udevice *clk_udevice;
 	fdt_addr_t addr;
 	int ret = 0;
+	int val = 0;
+	cpu_id_t cpu_id = get_cpu_id();
 
 	addr = devfdt_get_addr(dev);
 	if (addr == FDT_ADDR_T_NONE)
@@ -972,16 +972,36 @@ static int meson_mmc_ofdata_to_platdata(struct udevice *dev)
 			return ret;
 	}
 
-	uclass_get_device_by_name(UCLASS_CLK, "amlogic,g12a-clkc", &clk_udevice);
+	if (cpu_id.family_id == 0x32) {
+		#ifndef CLKCTRL_SD_EMMC_CLK_CTRL
+		#define CLKCTRL_SD_EMMC_CLK_CTRL ((0x005b  << 2) + 0xfe000000)
+		#endif
+		if (aml_card_type_non_sdio(host)) {
+			//	clksrc
+			val = readl(CLKCTRL_SD_EMMC_CLK_CTRL);
+			val &= ~(0xfff000);
+			val |= 0x800000;
+			writel(val, CLKCTRL_SD_EMMC_CLK_CTRL);
+		}
+		if (aml_card_type_mmc(host)) {
+			//	clksrc
+			val = readl(CLKCTRL_SD_EMMC_CLK_CTRL);
+			val &= ~(0xfff);
+			val |= 0x80;
+			writel(val, CLKCTRL_SD_EMMC_CLK_CTRL);
+		}
+	} else {
+		uclass_get_device_by_name(UCLASS_CLK, "amlogic,g12a-clkc", &clk_udevice);
 
-	clk_get_by_name(dev, "clkin", &host->div2);
-	clk_get_by_name(dev, "xtal", &host->xtal);
-	clk_get_by_name(dev, "mux", &host->mux);
-	clk_get_by_name(dev, "div", &host->div);
-	clk_get_by_name(dev, "gate", &host->gate);
+		clk_get_by_name(dev, "clkin", &host->div2);
+		clk_get_by_name(dev, "xtal", &host->xtal);
+		clk_get_by_name(dev, "mux", &host->mux);
+		clk_get_by_name(dev, "div", &host->div);
+		clk_get_by_name(dev, "gate", &host->gate);
 
-	//clk_enable(&host->core);
-	clk_enable(&host->gate);
+		//clk_enable(&host->core);
+		clk_enable(&host->gate);
+	}
 
 	return 0;
 }
@@ -1046,6 +1066,18 @@ static int meson_mmc_probe(struct udevice *dev)
 	val = meson_read(mmc, MESON_SD_EMMC_CFG);
 	val &= ~CFG_SDCLK_ALWAYS_ON;
 	val |= CFG_AUTO_CLK;
+
+	/* 512 bytes block length */
+	val &= ~CFG_BL_LEN_MASK;
+	val |= CFG_BL_LEN_512;
+
+	/* Response timeout 256 clk */
+	val &= ~CFG_RESP_TIMEOUT_MASK;
+	val |= CFG_RESP_TIMEOUT_256;
+
+	/* Command-command gap 16 clk */
+	val &= ~CFG_RC_CC_MASK;
+	val |= CFG_RC_CC_16;
 	meson_write(mmc, val, MESON_SD_EMMC_CFG);
 	printf("[%s]%s: probe success!\n", __func__, mmc->cfg->name);
 
