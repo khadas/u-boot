481667f3a098aba11bf98dc2dd2da4f5ddb3d9f5
Jian Hu <jian.hu@amlogic.com>
2020-07-06 15:42:49 +0800

    i2c: T5: add i2c support [1/1]
    PD#SWPL-29117
    
    Problem:
    it does not support i2c
    
    Solution:
    add i2c support
    
    Verify:
    verified on t5 ak329-skt
    
    Change-Id: Ibedfd3621ec3e30e67c8b4872b3e842280bf65c5
    Signed-off-by: Jian Hu <jian.hu@amlogic.com>

--- board/amlogic/t5_skt_v1/t5_skt_v1.c
+++ board/amlogic/t5_skt_v1/t5_skt_v1.c
@@ -520,28 +520,33 @@ static const struct meson_i2c_platdata i2c_data[] = {
 	{ 0, 0xffd1f000, 166666666, 3, 15, 100000 },
 	{ 1, 0xffd1e000, 166666666, 3, 15, 100000 },
 	{ 2, 0xffd1d000, 166666666, 3, 15, 100000 },
-	{ 3, 0xffd1c000, 166666666, 3, 15, 100000 },
-	{ 4, 0xff805000, 166666666, 3, 15, 100000 },
 };
 
 U_BOOT_DEVICES(meson_i2cs) = {
 	{ "i2c_meson", &i2c_data[0] },
 	{ "i2c_meson", &i2c_data[1] },
 	{ "i2c_meson", &i2c_data[2] },
-	{ "i2c_meson", &i2c_data[3] },
-	{ "i2c_meson", &i2c_data[4] },
 };
 
 /*
- *GPIOAO_10//I2C_SDA_AO
- *GPIOAO_11//I2C_SCK_AO
+ *GPIOH_20//I2C_SCL
+ *GPIOH_21//I2C_SDA
  *pinmux configuration seperated with i2c controller configuration
  * config it when you use
  */
-void set_i2c_ao_pinmux(void)
+#if 0 /* i2c pinmux demo */
+void set_i2c_b_pinmux(void)
 {
+	/*ds =3 */
+	setbits_le32(PAD_DS_REG2B, 0xf << 8);
+	/*pull up disable*/
+	clrbits_le32(PAD_PULL_UP_EN_REG2, 0x3 << 20);
+	/*pin mux to i2cm1*/
+	clrbits_le32(PERIPHS_PIN_MUX_7, 0xff << 16);
+	setbits_le32(PERIPHS_PIN_MUX_7, 0x1 << 16 | 0x1 << 20);
 	return;
 }
+#endf
 #endif /*end CONFIG_SYS_I2C_MESON*/
 
 #ifdef CONFIG_PWM_MESON
@@ -591,6 +596,7 @@ void reset_mt7668(void)
 int board_late_init(void)
 {
 	TE(__func__);
+
 	char outputModePre[30];
 	char outputModeCur[30];
 	strcpy(outputModePre,getenv("outputmode"));
