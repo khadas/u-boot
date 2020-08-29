fd6c13b1970fcb54bfc19ebe733e1e4c916293dc
zhiguang.ouyang <zhiguang.ouyang@amlogic.com>
2020-08-30 00:05:00 +0800

    ddr: bl33_v2015: delete ddr_test_watchdog_reset_system [1/1]
    PD#SWPL-32507
    
    Problem:
    window test cause reboot_mode watchdog_reboot
    
    Solution:
    //ddr_test_watchdog_reset_system();
    
    Verify:
    test pass at u200/x301
    
    Change-Id: Id6af55d823fa91b898d91c2c8c8923219b988414
    Signed-off-by: zhiguang.ouyang <zhiguang.ouyang@amlogic.com>

--- common/cmd_ddr_test_g12.c
+++ common/cmd_ddr_test_g12.c
@@ -8558,7 +8558,7 @@ int do_ddr2pll_g12_cmd(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[])
 	} else { //G12A/G12B/SM1/TL1/TM2
 		printf("reboot...\n"); //reset will enter bl2 panic path,so change to "reboot"
 		run_command("reboot", 0);
-		ddr_test_watchdog_reset_system();
+		//ddr_test_watchdog_reset_system();
 	}
 
 	return 0;
@@ -9826,4 +9826,3 @@ U_BOOT_CMD(
 	"ddr_fastboot_config cmd arg1 arg2 arg3...",
 	"ddr_fastboot_config cmd arg1 arg2 arg3... \n dcache off ? \n"
 	);
-
