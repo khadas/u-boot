4bf9991ed7103bda2018e808bdd3e5df8886488e
Terry <terry@khadas.com>
2020-09-23 15:17:02 +0800

    fixup! storeargs: remove run storeargs [1/1]

--- board/khadas/configs/kvim3l.h
+++ board/khadas/configs/kvim3l.h
@@ -302,7 +302,6 @@
                     "osd open;osd clear;"\
             "else "\
                 "setenv reboot_mode_android ""normal"";"\
-                "run storeargs;"\
                 "if test ${lcd_exist} = 0; then "\
                     "hdmitx hpd;hdmitx get_preferred_mode;hdmitx get_parse_edid;dovi process;osd open;osd clear;imgread pic logo bootup $loadaddr;bmp display $bootup_offset;bmp scale;vout output ${outputmode};vpp hdrpkt;"\
                 "else "\
