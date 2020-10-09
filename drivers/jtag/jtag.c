868c6ddddd6579f8a817987cf696aac324498e13
Zelong Dong <zelong.dong@amlogic.com>
2020-10-09 14:38:41 +0800

    bl33: jtag: optimize jtag driver [1/1]
    PD#SWPL-34318
    
    Problem:
    1. CONFIG_AML_MESON_* does not cover new platform
    2. writing pinmux register without mask field
    
    Solution:
    1. use macro CONFIG_TARGET_MESON_*
    2. add mask field
    
    Verify:
    t5_ak329, txhd_skt
    
    Change-Id: Id860c36869b2c33ff380e337091c3cb69fcd6067
    Signed-off-by: Zelong Dong <zelong.dong@amlogic.com>

--- drivers/jtag/jtag.c
+++ drivers/jtag/jtag.c
@@ -14,29 +14,51 @@
 struct jtag_pinctrl_reg_desc {
 	unsigned char mode;
 	volatile uint32_t *reg;
-	uint32_t	val;
-	uint32_t   valbak;
+	uint32_t val;
+	uint32_t mask;
+	uint32_t valbak;
 };
 
 /* axg */
-#if defined(CONFIG_AML_MESON_AXG)
+#if defined(CONFIG_TARGET_MESON_AXG)
 	struct jtag_pinctrl_reg_desc regs_data[] = {
-		{JTAG_AP_AO, P_AO_RTI_PINMUX_REG0, (0x4<<12) | (0x4<<16) | (0x4<<20) | (0x4<<28)},
-		{JTAG_AP_EE, P_PERIPHS_PIN_MUX_4,  (0x2<<0) | (0x2<<4) | (0x2<<16) | (0x2<<20)},
+		{JTAG_AP_AO, P_AO_RTI_PINMUX_REG0,
+			(0x4 << 12) | (0x4 << 16) | (0x4 << 20) | (0x4 << 28),
+			(0xf << 12) | (0xf << 16) | (0xf << 20) | (0xf << 28)},
+		{JTAG_AP_EE, P_PERIPHS_PIN_MUX_4,
+			(0x2 << 0) | (0x2 << 4) | (0x2 << 16) | (0x2 << 20),
+			(0xf << 0) | (0xf << 4) | (0xf << 16) | (0xf << 20)},
 	};
 /* g12a/g12b/tl1 */
-#elif defined(CONFIG_AML_MESON_G12A) || defined(CONFIG_AML_MESON_G12B) || defined(CONFIG_AML_MESON_TL1) || defined(CONFIG_AML_MESON_SM1) ||defined(CONFIG_AML_MESON_TM2)
+#elif defined(CONFIG_TARGET_MESON_G12A) || \
+	defined(CONFIG_TARGET_MESON_G12B) || \
+	defined(CONFIG_TARGET_MESON_TL1) || \
+	defined(CONFIG_TARGET_MESON_SM1) || \
+	defined(CONFIG_TARGET_MESON_TM2) || \
+	defined(CONFIG_TARGET_MESON_T5)
 	struct jtag_pinctrl_reg_desc regs_data[] = {
-		{JTAG_AP_AO, P_AO_RTI_PINMUX_REG0, (1 << 24) | (1 << 28)},
-		{JTAG_AP_AO, P_AO_RTI_PINMUX_REG1, (1 << 4) | (1 << 0)},
-		{JTAG_AP_EE, P_PERIPHS_PIN_MUX_9, (2 << 0) | (2 << 4) | (2 << 16) | (2 << 20)},
-		{SWD_AP_AO, P_AO_RTI_PINMUX_REG0, (4 << 24) | (4 << 28)},
+		{JTAG_AP_AO, P_AO_RTI_PINMUX_REG0,
+			(0x1 << 24) | (0x1 << 28),
+			(0xf << 24) | (0xf << 28)},
+		{JTAG_AP_AO, P_AO_RTI_PINMUX_REG1,
+			(0x1 << 4) | (0x1 << 0),
+			(0xf << 4) | (0xf << 0)},
+		{JTAG_AP_EE, P_PERIPHS_PIN_MUX_9,
+			(0x2 << 0) | (0x2 << 4) | (0x2 << 16) | (0x2 << 20),
+			(0xf << 0) | (0xf << 4) | (0xf << 16) | (0xf << 20)},
+		{SWD_AP_AO, P_AO_RTI_PINMUX_REG0,
+			(0x4 << 24) | (0x4 << 28),
+			(0xf << 24) | (0xf << 28)},
 	};
 /* txhd */
-#elif defined(CONFIG_AML_MESON_TXHD)
+#elif defined(CONFIG_TARGET_MESON_TXHD)
 	struct jtag_pinctrl_reg_desc regs_data[] = {
-		{JTAG_AP_AO, P_AO_RTI_PIN_MUX_REG, (3 << 12) | (4 << 16) | (4 << 20) | (3 << 28)},
-		{JTAG_AP_EE, P_PERIPHS_PIN_MUX_9,  (2 << 0) | (2 << 4) | (2 << 16) | (2 << 20)},
+		{JTAG_AP_AO, P_AO_RTI_PIN_MUX_REG,
+			(0x3 << 12) | (0x4 << 16) | (0x4 << 20) | (0x3 << 28),
+			(0xf << 12) | (0xf << 16) | (0xf << 20) | (0xf << 28)},
+		{JTAG_AP_EE, P_PERIPHS_PIN_MUX_9,
+			(0x2 << 0) | (0x2 << 4) | (0x2 << 16) | (0x2 << 20),
+			(0xf << 0) | (0xf << 4) | (0xf << 16) | (0xf << 20)},
 	};
 /* axg before */
 #else
@@ -50,6 +72,7 @@ static int enabled_flag = 0;
 void jtag_set_pinmux(unsigned int sel, int enable)
 {
 	int i;
+	uint32_t value;
 
 	if (regs_data[0].mode == 0xff)
 		return;
@@ -57,8 +80,12 @@ void jtag_set_pinmux(unsigned int sel, int enable)
 		for (i = 0; i < sizeof(regs_data)/sizeof(struct jtag_pinctrl_reg_desc); i++) {
 			if (sel == regs_data[i].mode) {
 				enabled_flag = 1;
-				regs_data[i].valbak = readl(regs_data[i].reg);
-				writel(readl(regs_data[i].reg) | regs_data[i].val, regs_data[i].reg);
+				value = readl(regs_data[i].reg);
+				regs_data[i].valbak = value;
+
+				value = (value & ~regs_data[i].mask) |
+							regs_data[i].val;
+				writel(value, regs_data[i].reg);
 			}
 		}
 	} else {
