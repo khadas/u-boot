1e74873eb12fe3c00b2fd1f761fa430e18373dab
Cheng Wang <cheng.wang@amlogic.com>
2020-10-10 14:21:46 +0800

    config: system UI certification bug [1/1]
    PD#SWPL-29469
    
    Problem:
    UI certification bug missing a space
    
    Solution:
    add a space in sc2 config
    
    Verify:
    on sc2
    
    Change-Id: I358eba369789425680653e251b6cbff11c3b2aaf
    Signed-off-by: Cheng Wang <cheng.wang@amlogic.com>

--- board/amlogic/configs/sc2_ah212_lpddr4.h
+++ board/amlogic/configs/sc2_ah212_lpddr4.h
@@ -132,7 +132,7 @@
                 "logo=${display_layer},loaded,${fb_addr} vout=${outputmode},${vout_init} panel_type=${panel_type} "\
                 "hdmitx=${cecconfig},${colorattribute} hdmimode=${hdmimode} "\
                 "hdmichecksum=${hdmichecksum} dolby_vision_on=${dolby_vision_on} " \
-                "hdr_policy=${hdr_policy} hdr_priority=${hdr_priority}"\
+                "hdr_policy=${hdr_policy} hdr_priority=${hdr_priority} "\
                 "frac_rate_policy=${frac_rate_policy} hdmi_read_edid=${hdmi_read_edid} cvbsmode=${cvbsmode} "\
                 "osd_reverse=${osd_reverse} video_reverse=${video_reverse} irq_check_en=${Irq_check_en}  "\
                 "androidboot.selinux=${EnableSelinux} androidboot.firstboot=${firstboot} jtag=${jtag}; "\
