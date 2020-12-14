4b8f4bbfec00cb2328f97da7ec2ebdefdfea91d0
Bing Jiang <bing.jiang@amlogic.com>
2020-12-14 18:02:30 +0800

    uboot: modify system partition size [1/1]
    PD#SWPL-38916
    
    Problem:
      increase system partition size to 256M
    
    Solution:
      increase system partition size to 256M
    
    Verify:
      T5D_AM301
    
    Change-Id: I8d17c30eedc28048dbf7cd9bd44b7093813d28e4
    Signed-off-by: Bing Jiang <bing.jiang@amlogic.com>

--- board/amlogic/t5d_am301_v1/t5d_am301_v1.c
+++ board/amlogic/t5d_am301_v1/t5d_am301_v1.c
@@ -456,7 +456,7 @@ static struct mtd_partition normal_partition_info[] = {
     {
         .name = "system",
         .offset = 0,
-        .size = 64*SZ_1M,
+        .size = 256*SZ_1M,
     },
 	/* last partition get the rest capacity */
     {
