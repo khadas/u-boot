f49c452b1c85666f270f44743614f2acda3f4bb8
Bo Lv <bo.lv@amlogic.com>
2021-01-07 02:57:53 -0500

    T5D: Fix package id problem of T5D [1/1]
    PD#SWPL-40631
    
    Problem:
    t5d need acquire package id.
    
    Solution:
    1. t5d pakage id different from formerly soc.
    2. add weak function for t5d to acquire ID.
    
    Verify:
    test pass on T950X4.
    
    Change-Id: I9f96ce13e73c805e99abaf9520dd72805bdcf618
    Signed-off-by: Bo Lv <bo.lv@amlogic.com>

--- arch/arm/cpu/armv8/t5d/cpu_id.c
+++ arch/arm/cpu/armv8/t5d/cpu_id.c
@@ -17,36 +17,15 @@
  *
  * Chip ID copy to avoid unnecessary message exchange.
  */
-chip_id_t aml_chip_id = { 0, { 0 } };
 
-cpu_id_t __attribute__((weak)) get_cpu_id(void)
+cpu_id_t get_cpu_id(void)
 {
 	cpu_id_t cpu_id;
 	unsigned int cpu_id_reg = readl(P_AO_SEC_SD_CFG8);
 	cpu_id.reserve = 0;
 	cpu_id.family_id = (cpu_id_reg >> 24) & (0XFF);
-	cpu_id.package_id = (cpu_id_reg >> 16) & (0XF0);
+	cpu_id.package_id = (cpu_id_reg) & (0X07);
 	cpu_id.chip_rev = (cpu_id_reg >> 8) & (0XFF);
-	cpu_id.layout_ver = (cpu_id_reg) & (0XF);
 	return cpu_id;
 }
 
-int get_chip_id(unsigned char *buff, unsigned int size)
-{
-	int rc = 0;
-
-	if (buff == NULL || size < 16)
-		return -1;
-
-	if (aml_chip_id.version == 0) {
-		/* Chip ID has not been fetched yet */
-		rc = __get_chip_id(&aml_chip_id.chipid[0], 16);
-
-		/* Mark version if chip ID is fetched successfully */
-		if (rc == 0)
-			aml_chip_id.version = 2;
-	}
-
-	memcpy(buff, &aml_chip_id.chipid[0], 16);
-	return rc;
-}
