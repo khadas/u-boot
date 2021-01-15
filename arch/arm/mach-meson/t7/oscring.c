500eeaa98dcd28c4003484b7bd41777126d4d479
Zhenxin Pu <zhenxin.pu@amlogic.com>
2021-01-15 16:40:58 +0800

    T7: ringmsr enable [2/2]
    PD#SWPL-41301
    
    Problem:
    t7 ringmsr enable.
    
    Solution:
    t7 ringmsr enable.
    
    Verify:
    T7 skt
    
    Change-Id: Ib92268f1323333dbf16245ac2f5719aca9d025d4
    Signed-off-by: Zhenxin Pu <zhenxin.pu@amlogic.com>

--- arch/arm/mach-meson/t7/oscring.c
+++ arch/arm/mach-meson/t7/oscring.c
@@ -7,8 +7,9 @@
 
 void ring_powerinit(void)
 {
-	writel(0x18000a, RING_PWM_VCCK);/*set vcck 0.8v*/
-	writel(0x90009, RING_PWM_EE);/*set ee 0.8v*/
+	writel(0x18000a, RING_PWM_VCCK_A);/*set vcck 0.8v*/
+	writel(0x18000a, RING_PWM_VCCK_B);/*set vcck 0.8v*/
+	writel(0x8000a, RING_PWM_EE);/*set ee 0.8v*/
 }
 
 unsigned long clk_util_ring_msr(unsigned long clk_mux)
@@ -41,36 +42,53 @@ unsigned long clk_util_ring_msr(unsigned long clk_mux)
 int ring_msr(int index)
 {
 	const char* clk_table[] = {
-			[17] = "dspa_osc_ring(SVT16) " ,
-			[16] = "axi_sram_osc_ring(SVT16) " ,
-			[15] = "sys_cpu_osc_ring3(LVT16) " ,
-			[14] = "sys_cpu_osc_ring2(ULVT16) " ,
-			[13] = "sys_cpu_osc_ring1(ULVT20) " ,
-			[12] = "sys_cpu_osc_ring0(ULVT16) " ,
-			[11] = "ddr_osc_ring(LVT16) " ,
-			[10] = "dos_osc_ring3(ULVT20) " ,
-			[9] = "dos_osc_ring2(LVT16) " ,
-			[8] = "dos_osc_ring1(SVT16) " ,
-			[7] = "dos_osc_ring0(SVT24) " ,
-			[6] = "vpu_osc_ring2(LVT16) " ,
-			[5] = "vpu_osc_ring1(LVT20) " ,
-			[4] = "vpu_osc_ring0(SVT24) " ,
-			[3] = "gpu_osc_ring2(SVT16) " ,
-			[2] = "gpu_osc_ring1(ULVT16) " ,
-			[1] = "gpu_osc_ring0(LVT16) " ,
-			[0] = "dmc_osc_ring(LVT16) " ,
+			[32] = "am_ring_osc_clk_out[32] " ,
+			[31] = "am_ring_osc_clk_out[31] " ,
+			[30] = "am_ring_osc_clk_out[30] " ,
+			[29] = "am_ring_osc_clk_out[29] " ,
+			[28] = "am_ring_osc_clk_out[28] " ,
+			[27] = "am_ring_osc_clk_out[27] " ,
+			[26] = "am_ring_osc_clk_out[26] " ,
+			[25] = "am_ring_osc_clk_out[25] " ,
+			[24] = "am_ring_osc_clk_out[24] " ,
+			[23] = "am_ring_osc_clk_out[23] " ,
+			[22] = "am_ring_osc_clk_out[22] " ,
+			[21] = "am_ring_osc_clk_out[21] " ,
+			[20] = "am_ring_osc_clk_out[20] " ,
+			[19] = "am_ring_osc_clk_out[19] " ,
+			[18] = "am_ring_osc_clk_out[18] " ,
+			[17] = "am_ring_osc_clk_out[17] " ,
+			[16] = "am_ring_osc_clk_out[16] " ,
+			[15] = "am_ring_osc_clk_out[15] " ,
+			[14] = "am_ring_osc_clk_out[14] " ,
+			[13] = "am_ring_osc_clk_out[13] " ,
+			[12] = "am_ring_osc_clk_out[12] " ,
+			[11] = "am_ring_osc_clk_out[11] " ,
+			[10] = "am_ring_osc_clk_out[10] " ,
+			[9] = "am_ring_osc_clk_out[9] " ,
+			[8] = "am_ring_osc_clk_out[8] " ,
+			[7] = "am_ring_osc_clk_out[7] " ,
+			[6] = "am_ring_osc_clk_out[6] " ,
+			[5] = "am_ring_osc_clk_out[5] " ,
+			[4] = "am_ring_osc_clk_out[4] " ,
+			[3] = "am_ring_osc_clk_out[3] " ,
+			[2] = "am_ring_osc_clk_out[2] " ,
+			[1] = "am_ring_osc_clk_out[1] " ,
+			[0] = "am_ring_osc_clk_out[0] " ,
 		};
 	const int tb[] = {180, 181, 182, 183, 184, 185, 186, 187, 188, 189,
-			  190, 191, 192, 193, 194, 195, 196, 197};
+			  190, 191, 192, 193, 194, 195, 196, 197, 198, 199,
+			  206, 207, 208, 209, 210, 211, 212, 213, 214, 215,
+			  216, 217, 218};
 	unsigned long i;
-	uint8_t efuseinfo[4] = {0, 0, 0, 0};
+	uint8_t efuseinfo[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 	if ((index != 0xff) && (index != 0)) {
 		if (bl31_get_cornerinfo(efuseinfo, sizeof(efuseinfo) / sizeof(uint8_t)) != 0) {
 			printf("fail get efuse info\n");
 			return 0;
 		}
-		if ((index >= 1) && (index <= 3))
+		if ((index >= 1) && (index <= 11))
 			printf("%d\n", efuseinfo[index]);
 		else
 			printf("input data not support!\n");
@@ -84,8 +102,9 @@ int ring_msr(int index)
 	/*RING_OSCILLATOR       0x7f: set slow ring*/
 	writel(OSCRING_CTL_DATA0, OSCRING_CTL_REG0);
 	writel(OSCRING_CTL_DATA1, OSCRING_CTL_REG1);
+	writel(OSCRING_CTL_DATA2, OSCRING_CTL_REG2);
 
-	for (i = 0; i < 18; i++) {
+	for (i = 0; i < 33; i++) {
 		printf("%s      :",clk_table[i]);
 		printf("%ld     KHz",clk_util_ring_msr(tb[i]));
 		printf("\n");
@@ -97,18 +116,25 @@ int ring_msr(int index)
 	}
 
 	printf("osc efuse info:\n");
-	for (i = 0; i < 4; i++)
+	for (i = 0; i < 10; i++)
 		printf("0x%x, ", efuseinfo[i]);
+	printf("0x%x, ", efuseinfo[11]);
 	printf("\n");
 
 	/*efuse to test value*/
-	printf("osc_ring_core0(20lvt), idd_ee, idd_cpu\n");
+	printf("idd_ee, idd_cpu_a53, a53_osc_ring, idd_cpu_a73, a73_osc_ring, idd_nn, nna_osc_ring, idd_gpu, gpu_osc_ring, vpu_osc_ring\n");
 
-	printf("%d KHz ", (efuseinfo[1] * 20));
+	printf("%d uA ", (efuseinfo[1] * 400));
+	printf("%d uA ", (efuseinfo[2] * 400));
+	printf("%d KHz ", (efuseinfo[3] * 20));
+	printf("%d uA ", (efuseinfo[4] * 400));
+	printf("%d KHz ", (efuseinfo[5] * 20));
+	printf("%d uA ", (efuseinfo[6] * 400));
+	printf("%d KHz ", (efuseinfo[7] * 20));
+	printf("%d uA ", (efuseinfo[8] * 400));
+	printf("%d KHz ", (efuseinfo[9] * 20));
+	printf("%d KHz ", (efuseinfo[11] * 20));
 
-	for (i = 2; i <=3; i++) {
-		printf("%d uA ",  (efuseinfo[i] * 400));
-	}
 	printf("\n");
 
 	return 0;
