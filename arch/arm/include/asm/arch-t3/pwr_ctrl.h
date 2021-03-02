92d7eb6ac5883706cdc87c8ec2a3e03b4a2c1bf7
zhiqiang liang <zhiqiang.liang@amlogic.com>
2021-03-02 15:42:19 +0800

    powerdomain: add powerdomain driver for t3 [2/3]
    PD#SWPL-44156
    
    Problem:
    add powerdomain driver for t3
    
    Solution:
    add code
    
    Verify:
    Z1
    
    Change-Id: Ib22c52f1418f2d9a96e22e43d791d2a809f7f7e6
    Signed-off-by: zhiqiang liang <zhiqiang.liang@amlogic.com>

--- arch/arm/include/asm/arch-t3/pwr_ctrl.h
+++ arch/arm/include/asm/arch-t3/pwr_ctrl.h
@@ -6,58 +6,64 @@
 #define PWR_ON    1
 #define PWR_OFF   0
 
-#define PM_DSPA          0
-#define PM_DSPB          1
-#define PM_DOS_HCODEC    2
-#define PM_DOS_HEVC      3
-#define PM_DOS_VDEC      4
-#define PM_DOS_WAVE      5
-#define PM_VPU_HDMI      6
-#define PM_USB_COMB      7
-#define PM_PCIE          8
-#define PM_GE2D          9
-#define PM_SRAMA         10
-#define PM_SRAMB         11
-#define PM_HDMIRX        12
-#define PM_VI_CLK1       13
-#define PM_VI_CLK2       14
-#define PM_ETH           15
-#define PM_ISP           16
-#define PM_MIPI_ISP      17
-#define PM_GDC           18
-#define PM_DEWARP        19
-#define PM_SDIO_A        20
-#define PM_SDIO_B        21
-#define PM_EMMC          22
-#define PM_MALI_SC0      23
-#define PM_MALI_SC1      24
-#define PM_MALI_SC2      25
-#define PM_MALI_SC3      26
-#define PM_MALI_TOP      27
-#define PM_NNA_CORE0     28
-#define PM_NNA_CORE1     29
-#define PM_NNA_CORE2     30
-#define PM_NNA_CORE3     31
-#define PM_NNA_TOP       32
-#define PM_DDR0          33
-#define PM_DDR1          34
-#define PM_DMC0          35
-#define PM_DMC1          36
-#define PM_NOC           37
-#define PM_NIC2          38
-#define PM_NIC3          39
-#define PM_CCI           40
-#define PM_MIPI_DSI0     41
-#define PM_SPICC0        42
-#define PM_SPICC1        43
-#define PM_SPICC2        44
-#define PM_SPICC3        45
-#define PM_SPICC4        46
-#define PM_SPICC5        47
-#define PM_EDP0          48
-#define PM_EDP1          49
-#define PM_MIPI_DSI1     50
-#define PM_AUDIO         51
+#define PM_DSPA		0
+#define PM_DOS_HCODEC	1
+#define PM_DOS_HEVC	2
+#define PM_DOS_VDEC	3
+#define PM_VPU_HDMI	4
+#define PM_USB_COMB	5
+#define PM_PCIE		6
+#define PM_GE2D		7
+#define PM_SRAMA		8
+#define PM_HDMIRX		9
+#define PM_VI_CLK1		10
+#define PM_VI_CLK2		11
+#define PM_ETH		12
+#define PM_NNA		13
+#define PM_DEMOD		14
+#define PM_FRCTOP		15
+#define PM_FRCME		16
+#define PM_FRCMC		17
+#define PM_SDEMMC_B	18
+#define PM_SDEMMC_C	19
+#define PM_NOC_DEV		20
+#define PM_NOC_VPU	21
+#define PM_SPICC0		22
+#define PM_SPICC1		23
+#define PM_SPICC2		24
+#define PM_AUDIO		25
+
+static int pm_max = 26;
+
+static char* domain_name[] =
+{
+	"PM_DSPA",
+	"PM_DOS_HCODEC",
+	"PM_DOS_HEVC",
+	"PM_DOS_VDEC",
+	"PM_VPU_HDMI",
+	"PM_USB_COMB",
+	"PM_PCIE",
+	"PM_GE2D",
+	"PM_SRAMA",
+	"PM_HDMIRX",
+	"PM_VI_CLK1",
+	"PM_VI_CLK2",
+	"PM_ETH",
+	"PM_NNA",
+	"PM_DEMOD",
+	"PM_FRCTOP",
+	"PM_FRCME",
+	"PM_FRCMC",
+	"PM_SDEMMC_B",
+	"PM_SDEMMC_C",
+	"PM_NOC_DEV",
+	"PM_NOC_VPU",
+	"PM_SPICC0",
+	"PM_SPICC1",
+	"PM_SPICC2",
+	"PM_AUDIO",
+};
 
 unsigned long pwr_ctrl_psci_smc(unsigned int power_domain, bool power_control);
 unsigned long pwr_ctrl_status_psci_smc(unsigned int power_domain);
