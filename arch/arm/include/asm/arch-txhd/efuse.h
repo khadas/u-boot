26bc5d88021e31081dee73bd917e3edc28f25a81
Zhongfu Luo <zhongfu.luo@amlogic.com>
2021-03-11 15:23:28 +0800

    BL33: TXL/TXHD/G12A/TL1: support efuse object read/write [2/2]
    PD#SWPL-43459
    
    Problem:
    TXL/TXHD/G12A/TL1: need to support efuse object read/write
    
    Solution:
    efuse_obj command:
    efuse_obj get usb-boot-disable
    efuse_obj set usb-boot-disable
    efuse_obj get usb-password-protect
    efuse_obj set usb-password-protect
    efuse_obj get jtag-disable
    efuse_obj set jtag-disable
    
    Verify:
    TXL/TXHD/G12A/TL1 verify
    
    Change-Id: I4312a639af3179dac3b0b44890370675cf518a65
    Signed-off-by: Zhongfu Luo <zhongfu.luo@amlogic.com>

--- arch/arm/include/asm/arch-txhd/efuse.h
+++ arch/arm/include/asm/arch-txhd/efuse.h
@@ -28,6 +28,7 @@ struct efuse_hal_api_arg {
 #define EFUSE_HAL_API_WRITE 1
 #define EFUSE_HAL_API_WRITE_PATTERN 2
 #define EFUSE_HAL_API_USER_MAX 3
+#define CONFIG_EFUSE_OBJ_API 1
 
 #define EFUSE_USER_MASK            (0x1 << 16)
 #define EFUSE_THERMAL_MASK         (0x1 << 17)
@@ -35,6 +36,29 @@ struct efuse_hal_api_arg {
 #define EFUSE_ENCRYPT_MASK         (0x1 << 19)
 
 //#define ASSIST_HW_REV                              0x1f53
+
+typedef enum efuse_obj_status_s {
+	EFUSE_OBJ_SUCCESS		= 0,
+
+	EFUSE_OBJ_ERR_INVALID_DATA	= 100,
+	EFUSE_OBJ_ERR_NOT_FOUND,
+	EFUSE_OBJ_ERR_DEPENDENCY,
+	EFUSE_OBJ_ERR_SIZE,
+	EFUSE_OBJ_ERR_NOT_SUPPORT,
+
+	EFUSE_OBJ_ERR_ACCESS		= 200,
+	EFUSE_OBJ_ERR_WRITE_PROTECTED = 201,
+
+	EFUSE_OBJ_ERR_UNKNOWN		= 300,
+	EFUSE_OBJ_ERR_INTERNAL,
+} efuse_obj_status_e;
+
+typedef enum efuse_obj_type_s {
+	/* Built-in object type */
+	EFUSE_OBJ_LICENSE_DISABLE_JTAG_ALL	=0,
+	EFUSE_OBJ_LICENSE_ENABLE_USB_BOOT_PASSWORD,
+	EFUSE_OBJ_LICENSE_DISABLE_USB_BOOT,
+} efuse_obj_type_e;
 
 int efuse_read_usr(char *buf, size_t count, loff_t *ppos);
 int efuse_write_usr(char *buf, size_t count, loff_t *ppos);
@@ -45,5 +69,19 @@ ssize_t efuse_write(const char *buf, size_t count, loff_t *ppos);
 int32_t meson_trustzone_efuse(struct efuse_hal_api_arg *arg);
 int32_t meson_trustzone_efuse_get_max(struct efuse_hal_api_arg *arg);
 
+#ifdef CONFIG_EFUSE_OBJ_API
+uint32_t meson_efuse_obj_write(
+	uint32_t obj_id,
+	uint8_t *buff,
+	uint32_t size);
+
+uint32_t meson_efuse_obj_read(
+	uint32_t obj_id,
+	uint8_t *buff,
+	uint32_t *size);
+
+uint32_t efuse_obj_write(uint32_t obj_id, uint8_t *buff, uint32_t size);
+uint32_t efuse_obj_read(uint32_t obj_id, uint8_t *buff, uint32_t *size);
+#endif /* CONFIG_EFUSE_OBJ_API */
 #endif
 
