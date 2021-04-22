fc58644b12b1ceecf7f5965d09c08f657f92e1ae
Bo Lv <bo.lv@amlogic.com>
2021-04-22 16:08:30 +0800

    BL33: T3: clean up warning [1/1]
    PD#SWPL-43527
    
    Problem:
    t3 need to clean up warning
    
    Solution:
    fix it
    
    Verify:
    ./check_compile all chip
    
    Change-Id: Ia647973bd2a1b0f9f441ac70959c8bbb3228a46d
    Signed-off-by: Bo Lv <bo.lv@amlogic.com>

--- arch/arm/include/asm/arch-t3/pwr_ctrl.h
+++ arch/arm/include/asm/arch-t3/pwr_ctrl.h
@@ -33,37 +33,7 @@
 #define PM_SPICC2		24
 #define PM_AUDIO		25
 
-static int pm_max = 26;
-
-static char* domain_name[] =
-{
-	"PM_DSPA",
-	"PM_DOS_HCODEC",
-	"PM_DOS_HEVC",
-	"PM_DOS_VDEC",
-	"PM_VPU_HDMI",
-	"PM_USB_COMB",
-	"PM_PCIE",
-	"PM_GE2D",
-	"PM_SRAMA",
-	"PM_HDMIRX",
-	"PM_VI_CLK1",
-	"PM_VI_CLK2",
-	"PM_ETH",
-	"PM_NNA",
-	"PM_DEMOD",
-	"PM_FRCTOP",
-	"PM_FRCME",
-	"PM_FRCMC",
-	"PM_SDEMMC_B",
-	"PM_SDEMMC_C",
-	"PM_NOC_DEV",
-	"PM_NOC_VPU",
-	"PM_SPICC0",
-	"PM_SPICC1",
-	"PM_SPICC2",
-	"PM_AUDIO",
-};
+#define PM_MAX		26
 
 unsigned long pwr_ctrl_psci_smc(unsigned int power_domain, bool power_control);
 unsigned long pwr_ctrl_status_psci_smc(unsigned int power_domain);
