d2f5d355f7599d97c693964ee15ed81779d43820
Xindong Xu <xindong.xu@amlogic.com>
2021-04-28 18:05:39 +0800

    fastboot: pass androidboot.bootloader to android [2/2]
    PD#SWPL-48811
    
    Problem:
    show error bootloader_version in fastbootd mode
    
    Solution:
    pass right androidboot.bootloader to android
    
    Verify:
    newton
    
    Change-Id: I144bfa3821ef8255f5f1eda7d2e12de16766016f
    Signed-off-by: Xindong Xu <xindong.xu@amlogic.com>

--- common/cmd_version.c
+++ common/cmd_version.c
@@ -33,7 +33,8 @@ static int do_version(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 static int do_bootloadr_version(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
 	char s_version[64];
-	strcpy(s_version, U_BOOT_VERSION);
+	strcpy(s_version, "01.01.");
+	strcat(s_version, U_BOOT_DATE_TIME);
 	printf("s_version: %s\n", s_version);
 	setenv("bootloader_version", s_version);
 	return 0;
