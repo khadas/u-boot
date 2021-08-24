2ead6c154aef6b016c859121e4450cd3a66e0fdb
Zhigang Yu <zhigang.yu@amlogic.com>
2021-08-24 14:03:09 +0800

    avb2: fixed fault address when remount and default env [1/1]
    PD#SWPL-58358
    
    Problem:
    double free for data buffer
    
    Solution:
    add data buffer null check
    
    Verify:
    verify on 905x2
    
    Change-Id: I683295357a9358be29684e6eab2005b8a6c9b9bd
    Signed-off-by: Zhigang Yu <zhigang.yu@amlogic.com>

--- lib/libavb/avb_slot_verify.c
+++ lib/libavb/avb_slot_verify.c
@@ -1630,6 +1630,9 @@ fail:
 }
 
 void avb_slot_verify_data_free(AvbSlotVerifyData* data) {
+  if (!data) {
+    return;
+  }
   if (data->ab_suffix != NULL) {
     avb_free(data->ab_suffix);
   }
@@ -1663,6 +1666,7 @@ void avb_slot_verify_data_free(AvbSlotVerifyData* data) {
     avb_free(data->loaded_partitions);
   }
   avb_free(data);
+  data = NULL;
 }
 
 const char* avb_slot_verify_result_to_string(AvbSlotVerifyResult result) {
