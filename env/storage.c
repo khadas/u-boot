259cc367be29db127f39a8e328bb480b77e48e2b
Nick Xie <nick@khadas.com>
2021-08-30 14:14:18 +0800

    env: VIM4: load env from SD Card/SPI Flash when boot from them
    Signed-off-by: Nick Xie <nick@khadas.com>
    Change-Id: I4526b0cecb71fec1d12b0078fc381afe482016d1

--- env/storage.c
+++ env/storage.c
@@ -7,25 +7,36 @@
 #include <memalign.h>
 #include <errno.h>
 #include <amlogic/storage.h>
+#include <asm/arch-meson/boot.h>
 
 DECLARE_GLOBAL_DATA_PTR;
+
+extern unsigned int get_romcode_boot_id(void);
+extern const char *get_boot_source_str(unsigned int boot_id);
 
 #ifdef CONFIG_CMD_SAVEENV
 static int env_storage_save(void)
 {
-	if (store_get_type() == BOOT_NONE) {
-		printf("env_storage: must init before save\n");
-		return -ENOENT;
-	}
-	ALLOC_CACHE_ALIGN_BUFFER(char, buf, CONFIG_ENV_SIZE);
+	int boot_id = get_romcode_boot_id();
 
-	if (env_export((env_t *)buf)) {
-		printf("env_storage: export failed\n");
-		return -EINVAL;
-	}
+	if (BOOT_DEVICE_SPI != boot_id && BOOT_DEVICE_SD != boot_id) {
+		if (store_get_type() == BOOT_NONE) {
+			printf("env_storage: must init before save\n");
+			return -ENOENT;
+		}
+		ALLOC_CACHE_ALIGN_BUFFER(char, buf, CONFIG_ENV_SIZE);
 
-	if (store_rsv_write(RSV_ENV, CONFIG_ENV_SIZE, (void *)buf)) {
-		printf("env_storage: write failed\n");
+		if (env_export((env_t *)buf)) {
+			printf("env_storage: export failed\n");
+			return -EINVAL;
+		}
+
+		if (store_rsv_write(RSV_ENV, CONFIG_ENV_SIZE, (void *)buf)) {
+			printf("env_storage: write failed\n");
+			return -EIO;
+		}
+	} else {
+		printf("Current boot device: %s, do not save env to eMMC.\n", get_boot_source_str(get_romcode_boot_id()));
 		return -EIO;
 	}
 
@@ -35,19 +46,29 @@ static int env_storage_save(void)
 
 static int env_storage_load(void)
 {
-	if (store_get_type() == BOOT_NONE) {
-		printf("env_storage: must init before load\n");
-		return -ENOENT;
-	}
-	ALLOC_CACHE_ALIGN_BUFFER(char, buf, CONFIG_ENV_SIZE);
+	int boot_id = get_romcode_boot_id();
 
-	if (store_rsv_read(RSV_ENV, CONFIG_ENV_SIZE, (void *)buf)) {
-		set_default_env("!env_storage: read failed", 0);
-		return -EIO;
-	}
-	else if (env_import(buf, 1)) {
-		set_default_env("!env_storage: import failed", 0);
-		return -EINVAL;
+	if (BOOT_DEVICE_SPI != boot_id && BOOT_DEVICE_SD != boot_id) {
+		if (store_get_type() == BOOT_NONE) {
+			printf("env_storage: must init before load\n");
+			return -ENOENT;
+		}
+		ALLOC_CACHE_ALIGN_BUFFER(char, buf, CONFIG_ENV_SIZE);
+
+		if (store_rsv_read(RSV_ENV, CONFIG_ENV_SIZE, (void *)buf)) {
+			set_default_env("!env_storage: read failed", 0);
+			return -EIO;
+		}
+		else if (env_import(buf, 1)) {
+			set_default_env("!env_storage: import failed", 0);
+			return -EINVAL;
+		}
+	} else {
+		char str[64];
+
+		sprintf(str, "load env from %s", get_boot_source_str(get_romcode_boot_id()));
+
+		set_default_env(str, 0);
 	}
 
 	return 0;
