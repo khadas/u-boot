5a83470619a2f1daecfe6880e1e04397e216e301
Sam Wu <yihui.wu@amlogic.com>
2021-09-22 17:53:42 +0800

    factoryBurn: security register usage clear [1/1]
    PD#SWPL-59713
    
    Problem:
    sys ctrl reg for detect dnl or wcp not clear
    
    Solution:
    use cpuid to clear the usage in detecting dnl
    
    Verify:
    sc2 / a1 / p1
    
    Change-Id: I820586c03c9923f92642080b9e3efa1f7e3efa6c
    Signed-off-by: Sam Wu <yihui.wu@amlogic.com>

--- drivers/usb/gadget/v3_burning/aml_v3_burning.c
+++ drivers/usb/gadget/v3_burning/aml_v3_burning.c
@@ -47,15 +47,53 @@ static int is_boot_device_usb(void)
 
 static int is_bl1_usb_protocol_DNL(void)
 {
+	const cpu_id_t cpuid = get_cpu_id();
+	const int familyId	 = cpuid.family_id;
+
+	if (familyId < MESON_CPU_MAJOR_ID_G12A)
+		return 0;//old cpu, only wcp
+	if (familyId > MESON_CPU_MAJOR_ID_C2 || familyId == MESON_CPU_MAJOR_ID_SC2)
+		return 1;//new cpu, only dnl
+
+	switch (familyId) {
+	case MESON_CPU_MAJOR_ID_A1:
+	case MESON_CPU_MAJOR_ID_C1:
+	case MESON_CPU_MAJOR_ID_C2:
+		{
 #ifdef SYSCTRL_SEC_STATUS_REG1
-    const unsigned cfg9 = readl(SYSCTRL_SEC_STATUS_REG1);
-    FB_DBG("SYSCTRL_SEC_STATUS_REG1 0x%x\n", cfg9);
-    return !(cfg9 & (1U<<12));
+			const unsigned cfg9 = readl(SYSCTRL_SEC_STATUS_REG1);
+
+			FB_DBG("SYSCTRL_SEC_STATUS_REG1 0x%x\n", cfg9);
+			return !(cfg9 & (1U << 12));
 #else
-    const unsigned cfg9 = readl(P_AO_SEC_SD_CFG9);
-    FB_DBG("cfg9 0x%x\n", cfg9);
-    return cfg9 & (1U<<15);
-#endif// #ifdef SYSCTRL_SEC_STATUS_REG1
+			FB_MSG("cpu 0x%x reg1 dnl\n", familyId);
+			return 1;
+#endif
+		}
+	case MESON_CPU_MAJOR_ID_G12A:
+	case MESON_CPU_MAJOR_ID_G12B:
+	case MESON_CPU_MAJOR_ID_SM1:
+	case MESON_CPU_MAJOR_ID_TL1:
+	case MESON_CPU_MAJOR_ID_TM2:
+		{
+#ifdef P_AO_SEC_SD_CFG9
+			const unsigned cfg9 = readl(P_AO_SEC_SD_CFG9);
+
+			FB_DBG("cfg9 0x%x\n", cfg9);
+			return cfg9 & (1U << 15);
+#else
+			FB_MSG("cpu 0x%x cfg9 dnl\n", familyId);
+			return 1;
+#endif// #ifdef P_AO_SEC_SD_CFG9
+		}
+
+	default:
+		FB_MSG("cpu 0x%x dnl\n", familyId);
+		return 1;
+	}
+
+	FB_MSG("id 0x%x dnl\n", familyId);
+	return 1;
 }
 
 
