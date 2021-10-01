ff383ed92bcbea5f6ea74bee4e05c433d6de0fd1
shaochan.liu <shaochan.liu@amlogic.com>
2021-10-01 17:24:54 +0800

    vout: add vout2 hdmitx support [1/1]
    PD#SWPL-59576
    
    Problem:
    need use vout2 hdmitx
    
    Solution:
    1, add vout2 hdmitx support
    
    Verify:
    t7_an400
    
    Change-Id: I8a7b177b1696ed904382260a5388ba1e356e6674
    Signed-off-by: shaochan.liu <shaochan.liu@amlogic.com>

--- include/amlogic/media/vout/hdmitx21/hdmitx.h
+++ include/amlogic/media/vout/hdmitx21/hdmitx.h
@@ -12,5 +12,6 @@
 
 int hdmitx_likely_frac_rate_mode(char *m);
 unsigned int hdmi_outputmode_check(char *mode, unsigned int frac);
-
+int do_hpd_detect(cmd_tbl_t *cmdtp, int flag, int argc,
+		  char *const argv[]);
 #endif
