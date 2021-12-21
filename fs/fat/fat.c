59e29c3543e1c95630e74436c73a760354ad7fa6
Bo Lv <bo.lv@amlogic.com>
2021-12-21 11:23:22 +0800

    uboot: fatload image size too large caused memory corruption [1/1]
    PD#SWPL-66887
    
    Problem:
    fatload image size too large caused memory corruption
    
    Solution:
    when fatload image size exceed available space will return
    
    Verify:
    test pass
    
    Change-Id: Ib3107e9bddb5638eee79f008a8e445788098fbe1
    Signed-off-by: Bo Lv <bo.lv@amlogic.com>

--- fs/fat/fat.c
+++ fs/fat/fat.c
@@ -21,6 +21,8 @@
 #include <linux/compiler.h>
 #include <linux/ctype.h>
 
+DECLARE_GLOBAL_DATA_PTR;
+
 /*
  * Convert a string to lowercase.  Converts at most 'len' characters,
  * 'len' may be larger than the length of 'str' if 'str' is NULL
@@ -319,6 +321,12 @@ static int get_contents(fsdata *mydata, dir_entry *dentptr, loff_t pos,
 
 	*gotsize = 0;
 	debug("Filesize: %llu bytes\n", filesize);
+
+	if ((filesize - (loff_t)buffer) >= gd->start_addr_sp) {
+		printf("img size subtract load address %llx exceed maximum available space %lx\n",
+				filesize - (loff_t)buffer, gd->start_addr_sp);
+		return -1;
+	}
 
 	if (pos >= filesize) {
 		debug("Read position past EOF: %llu\n", pos);
