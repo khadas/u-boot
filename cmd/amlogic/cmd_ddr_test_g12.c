a7b10a04acc73757bcaa73ab140c33c349d8261b
xiang.fu <xiang.fu@amlogic.com>
2021-12-31 12:30:54 +0800

    ddr-bl33: A5 enable ddr related cmd under uboot [1/1]
    PD#SWPL-69006
    
    Problem:
    enable ddr related cmd under uboot
    
    Solution:
    enable ddr related cmd under uboot
    
    Verify:
    enable ddr related cmd under uboot
    
    Change-Id: I22e4c8de54f1b43d66cf5d234f4e0a71a642f11c
    Signed-off-by: xiang.fu <xiang.fu@amlogic.com>

--- cmd/amlogic/cmd_ddr_test_g12.c
+++ cmd/amlogic/cmd_ddr_test_g12.c
@@ -83,6 +83,7 @@ typedef struct  ddr_base_address_table ddr_base_address_table_t;
 #define MESON_CPU_MAJOR_ID_P1                   0x39
 #undef  MESON_CPU_MAJOR_ID_S4D
 #define MESON_CPU_MAJOR_ID_S4D                  0x3a
+#define MESON_CPU_MAJOR_ID_A5                   0x3c
 #define MESON_CPU_VERSION_LVL_MAJOR     0
 #define MESON_CPU_VERSION_LVL_MINOR     1
 #define MESON_CPU_VERSION_LVL_PACK      2
@@ -1398,9 +1399,9 @@ int check_base_address(void)
 	if (chip_id == 0)
 		chip_id = CHIP_ID_MASK;
 	if (chip_id) {
-		if (chip_id == MESON_CPU_MAJOR_ID_S4D)
+		if (chip_id == MESON_CPU_MAJOR_ID_S4D || chip_id == MESON_CPU_MAJOR_ID_A5)
 			chip_id = MESON_CPU_MAJOR_ID_S4;
-		for (table_index = 0; table_index < table_max; table_index++) { //p_ddr_base=(p_ddr_base+1);
+		for (table_index = 0; table_index < table_max; table_index++) {
 			printf("\ntable_index=%08x,p_ddr_base_add=%08x,(p_ddr_base->chip_id==%08x",
 			       table_index, (unsigned int)(unsigned long)p_ddr_base, (p_ddr_base->chip_id));
 			if ((p_ddr_base->chip_id == chip_id) && (chip_id < CHIP_ID_MASK)) {
