bf91bf430ff07f45254a28307543968ae969e348
hao.qi <hao.qi@amlogic.com>
2022-01-14 14:29:03 +0800

    bootup: support non-AB for 5.4 GKI [1/2]
    PD#SWPL-69599
    
    Problem:
    to support non-AB for 5.4 R
    
    Solution:
    read vendor_boot before enter recovery
    
    Verify:
    t982_ar301
    
    Change-Id: Iddccba374b1f29e884ffa8394c6bfb625f4d2ff9
    Signed-off-by: hao.qi <hao.qi@amlogic.com>

--- board/amlogic/configs/t3_t982.h
+++ board/amlogic/configs/t3_t982.h
@@ -303,11 +303,20 @@
             "echo active_slot: ${active_slot};"\
             "setenv loadaddr ${loadaddr_kernel};"\
             "if test ${active_slot} = normal; then "\
-                "setenv bootargs ${bootargs} ${fs_type} aml_dt=${aml_dt} recovery_part={recovery_part} recovery_offset={recovery_offset};"\
-            "if imgread dtb recovery ${dtb_mem_addr}; then "\
-                "else echo restore dtb; run common_dtb_load;"\
-            "fi;"\
-            "if imgread kernel ${recovery_part} ${loadaddr} ${recovery_offset}; then bootm ${loadaddr}; fi;"\
+                "setenv bootargs ${bootargs} ${fs_type} aml_dt=${aml_dt} recovery_part=${recovery_part} recovery_offset=${recovery_offset};"\
+                "if test ${upgrade_step} = 3; then "\
+                    "if ext4load mmc 1:2 ${dtb_mem_addr} /recovery/dtb.img; then echo cache dtb.img loaded; fi;"\
+                    "if test ${vendor_boot_mode} = true; then "\
+                        "if imgread kernel ${recovery_part} ${loadaddr} ${recovery_offset}; then bootm ${loadaddr}; fi;"\
+                    "else "\
+                        "if ext4load mmc 1:2 ${loadaddr} /recovery/recovery.img; then echo cache recovery.img loaded; wipeisb; bootm ${loadaddr}; fi;"\
+                    "fi;"\
+                "else "\
+                    "if imgread dtb recovery ${dtb_mem_addr}; then "\
+                        "else echo restore dtb; run common_dtb_load;"\
+                    "fi;"\
+                "fi;"\
+                "if imgread kernel ${recovery_part} ${loadaddr} ${recovery_offset}; then bootm ${loadaddr}; fi;"\
             "else "\
                 "if fdt addr ${dtb_mem_addr}; then else echo retry common dtb; run common_dtb_load; fi;"\
                 "if test ${partiton_mode} = normal; then "\
