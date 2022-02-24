f66b1f9328270a9719e220f3dc3fe4579d7c020b
Shunzhou Jiang <shunzhou.jiang@amlogic.com>
2022-02-24 16:27:25 +0800

    c3: add c3 power domain [1/1]
    PD#SWPL-74251
    
    Problem:
    c3 power status verify
    
    Solution:
    add c3 power status verify
    
    Verify:
    pxp
    
    Change-Id: I323cb1f9257b5a8fbef0e65e3e228ed15e44c99e
    Signed-off-by: Shunzhou Jiang <shunzhou.jiang@amlogic.com>

--- arch/arm/include/asm/arch-c3/pwr_ctrl.h
+++ arch/arm/include/asm/arch-c3/pwr_ctrl.h
@@ -6,21 +6,23 @@
 #ifndef _PWRC_H_
 #define _PWRC_H_
 
-#define PWR_ON    1
-#define PWR_OFF   0
-#define PM_NNA    0
-#define PM_AUDIO  1
-#define PM_SDIOA  2
-#define PM_EMMC   3
-#define PM_USB_COMB     4
-#define PM_ETH          5
-#define PM_RSA          6
-#define PM_AUDIO_PDM    7
-#define PM_DMC          8
-#define PM_SYS_WRAP     9
-#define PM_DSPA         10
+#define PM_NNA		0
+#define PM_AUDIO	1
+#define PM_RESV_SEC	2
+#define PM_SDIOA	3
+#define PM_EMMC		4
+#define PM_USB_COMB	5
+#define PM_SDCARD	6
+#define PM_ETH		7
+#define PM_RESV0	8
+#define PM_GE2D		9
+#define PM_CVE		10
+#define PM_CDG_WRAP	11
+#define PM_ISP_TOP	12
+#define PM_MIPI_ISP_WRAP	13
+#define PM_VCODEC	14
 
-#define PM_MAX		11
+#define PM_MAX		15
 
 //static int pm_max = 8;
 /*
