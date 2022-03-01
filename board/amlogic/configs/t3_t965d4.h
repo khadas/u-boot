5f255af709c181b2e0d4ae4dcee0d02016c2d694
Meng yu <meng.yu@amlogic.com>
2022-03-01 11:51:11 +0000

    bl33_2019: modify the env format of the board [1/1]
    PD#SWPL-73617
    
    Problem:
    Characters are concatenated
    
    Solution:
    add spaces
    
    Verify:
    compile ok
    
    Change-Id: If1f800dba2adc7f6cc574118d3f32eefc62cf1a3
    Signed-off-by: Meng yu <meng.yu@amlogic.com>

--- board/amlogic/configs/t3_t965d4.h
+++ board/amlogic/configs/t3_t965d4.h
@@ -138,7 +138,8 @@
 		"cec_ac_wakeup=1\0" \
 	"disable_ir=0\0"\
         "initargs="\
-            "init=/init " CONFIG_KNL_LOG_LEVEL "console=ttyS0,115200 no_console_suspend earlycon=aml-uart,0xfe07a000"\
+		"init=/init " CONFIG_KNL_LOG_LEVEL "console=ttyS0,115200 "\
+			"no_console_suspend earlycon=aml-uart,0xfe07a000 "\
             "ramoops.pstore_en=1 ramoops.record_size=0x8000 ramoops.console_size=0x4000 loop.max_part=4 "\
             "\0"\
         "upgrade_check="\
