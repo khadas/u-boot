430264cf5bb16f844ef794e9239abb8220f50da2
Yuhui Zhang <yuhui.zhang@amlogic.com>
2022-03-28 18:05:59 +0800

    hdr: hdr_policy should be send to kernel. [1/1]
    PD#SWPL-76314
    
    Problem:
    hdr_policy should be send to kernel.
    
    Solution:
    hdr_policy should be send to kernel.
    
    Verify:
    t7 an400
    
    Change-Id: I71bbc581e7b4b3e4a4e86d264717ff905ce7647d
    Signed-off-by: Yuhui Zhang <yuhui.zhang@amlogic.com>

--- board/amlogic/configs/t7_an400_lpddr4x.h
+++ board/amlogic/configs/t7_an400_lpddr4x.h
@@ -150,6 +150,7 @@
                 "panel_type=${panel_type} lcd_ctrl=${lcd_ctrl} lcd_debug=${lcd_debug} "\
                 "hdmimode=${hdmimode} outputmode=${outputmode} "\
 			"hdmichecksum=${hdmichecksum} dolby_vision_on=${dolby_vision_on} "\
+			"hdr_policy=${hdr_policy} hdr_priority=${hdr_priority} "\
 			"hdmitx=${cecconfig},${colorattribute} "\
 			"frac_rate_policy=${frac_rate_policy} hdmi_read_edid=${hdmi_read_edid} "\
                 "osd_reverse=${osd_reverse} video_reverse=${video_reverse} irq_check_en=${Irq_check_en}  "\
