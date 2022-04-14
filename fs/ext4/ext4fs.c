d552354e7a91bc8eff04f050c431ebdc9e7a749a
Tao Zeng <tao.zeng@amlogic.com>
2022-04-14 17:27:40 +0800

    Squashed commit of the following:
    PD#SWPL-77526
    
    CVE-2019-13104
    
    commit f12324bd71277b2827bd80ca528c1fac8f8f1613
    Author: Stephen Warren <swarren@nvidia.com>
    Date:   Wed Jan 30 12:58:05 2019 -0700
    
        fs: ext4: cache extent data
    
        When a file contains extents, U-Boot currently reads extent-related data
        for each block in the file, even if that data is located in the same
        block each time. This significantly slows down loading of files that use
        extents. Implement a very dumb cache to prevent repeatedly reading the
        same block. Files with extents now load as fast as files without.
    
        Note: There are many cases where read_allocated_block() is called. This
        patch only addresses one of those places; all others still read redundant
        data in any case they did before. This is a minimal patch to fix the
        load command; other cases aren't fixed.
    
        Signed-off-by: Stephen Warren <swarren@nvidia.com>
    
    commit a756eb035995876b0b2a206a4f7ccaceedb68060
    Author: Paul Emge <paulemge@forallsecure.com>
    Date:   Mon Jul 8 16:37:05 2019 -0700
    
        CVE-2019-13104: ext4: check for underflow in ext4fs_read_file
    
        in ext4fs_read_file, it is possible for a broken/malicious file
        system to cause a memcpy of a negative number of bytes, which
        overflows all memory. This patch fixes the issue by checking for
        a negative length.
    
        Signed-off-by: Paul Emge <paulemge@forallsecure.com>
    
    Change-Id: Ib56ad8819e937e8522a0166b11347b6d91017a14

--- fs/ext4/ext4fs.c
+++ fs/ext4/ext4fs.c
@@ -63,13 +63,18 @@ int ext4fs_read_file(struct ext2fs_node *node, loff_t pos,
 	char *delayed_buf = NULL;
 	char *start_buf = buf;
 	short status;
+	struct ext_block_cache cache;
 
-	if (blocksize <= 0)
-		return -1;
+	ext_cache_init(&cache);
 
 	/* Adjust len so it we can't read past the end of the file. */
 	if (len + pos > filesize)
 		len = (filesize - pos);
+
+	if (blocksize <= 0 || len <= 0) {
+		ext_cache_fini(&cache);
+		return -1;
+	}
 
 	blockcnt = lldiv(((len + pos) + blocksize - 1), blocksize);
 
@@ -78,9 +83,11 @@ int ext4fs_read_file(struct ext2fs_node *node, loff_t pos,
 		int blockoff = pos - (blocksize * i);
 		int blockend = blocksize;
 		int skipfirst = 0;
-		blknr = read_allocated_block(&(node->inode), i);
-		if (blknr < 0)
+		blknr = read_allocated_block(&node->inode, i, &cache);
+		if (blknr < 0) {
+			ext_cache_fini(&cache);
 			return -1;
+		}
 
 		blknr = blknr << log2_fs_blocksize;
 
@@ -110,8 +117,10 @@ int ext4fs_read_file(struct ext2fs_node *node, loff_t pos,
 							delayed_skipfirst,
 							delayed_extent,
 							delayed_buf);
-					if (status == 0)
+					if (status == 0) {
+						ext_cache_fini(&cache);
 						return -1;
+					}
 					previous_block_number = blknr;
 					delayed_start = blknr;
 					delayed_extent = blockend;
@@ -138,8 +147,10 @@ int ext4fs_read_file(struct ext2fs_node *node, loff_t pos,
 							delayed_skipfirst,
 							delayed_extent,
 							delayed_buf);
-				if (status == 0)
+				if (status == 0) {
+					ext_cache_fini(&cache);
 					return -1;
+				}
 				previous_block_number = -1;
 			}
 			/* Zero no more than `len' bytes. */
@@ -156,12 +167,15 @@ int ext4fs_read_file(struct ext2fs_node *node, loff_t pos,
 		status = ext4fs_devread(delayed_start,
 					delayed_skipfirst, delayed_extent,
 					delayed_buf);
-		if (status == 0)
+		if (status == 0) {
+			ext_cache_fini(&cache);
 			return -1;
+		}
 		previous_block_number = -1;
 	}
 
 	*actread  = len;
+	ext_cache_fini(&cache);
 	return 0;
 }
 
@@ -254,4 +268,33 @@ int ext4fs_uuid(char *uuid_str)
 #else
 	return -ENOSYS;
 #endif
+}
+
+void ext_cache_init(struct ext_block_cache *cache)
+{
+	memset(cache, 0, sizeof(*cache));
+}
+
+void ext_cache_fini(struct ext_block_cache *cache)
+{
+	free(cache->buf);
+	ext_cache_init(cache);
+}
+
+int ext_cache_read(struct ext_block_cache *cache, lbaint_t block, int size)
+{
+	/* This could be more lenient, but this is simple and enough for now */
+	if (cache->buf && cache->block == block && cache->size == size)
+		return 1;
+	ext_cache_fini(cache);
+	cache->buf = malloc(size);
+	if (!cache->buf)
+		return 0;
+	if (!ext4fs_devread(block, 0, size, cache->buf)) {
+		free(cache->buf);
+		return 0;
+	}
+	cache->block = block;
+	cache->size = size;
+	return 1;
 }
