b84c6d5f141e7ac5dcb4c048f21808708d0a6be4
jason <jason@khadas.com>
2022-04-18 18:31:11 +0000

    wol: vim3l: check the wol state and add the bootargs 'wol_enable'
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: I8d7a15e9c4138f6dd1247abc4a893a4b2d883f40

--- board/khadas/configs/kvim3l.h
+++ board/khadas/configs/kvim3l.h
@@ -144,7 +144,7 @@
             "\0"\
         "storeargs="\
             "get_bootloaderversion;" \
-            "setenv bootargs ${initargs} ${fs_type} otg_device=${otg_device} reboot_mode_android=${reboot_mode_android} logo=${display_layer},loaded,${fb_addr} vout2=${outputmode2}, vout=${outputmode},enable panel_type=${panel_type} lcd_ctrl=${lcd_ctrl} hdmitx=${cecconfig},${colorattribute} hdmimode=${hdmimode} hdmichecksum=${hdmichecksum} dolby_vision_on=${dolby_vision_on} frac_rate_policy=${frac_rate_policy} hdmi_read_edid=${hdmi_read_edid} cvbsmode=${cvbsmode} osd_reverse=${osd_reverse} video_reverse=${video_reverse} irq_check_en=${Irq_check_en}  androidboot.selinux=${EnableSelinux} androidboot.firstboot=${firstboot} jtag=${jtag}; "\
+            "setenv bootargs ${initargs} ${fs_type} otg_device=${otg_device} reboot_mode_android=${reboot_mode_android} logo=${display_layer},loaded,${fb_addr} vout2=${outputmode2}, vout=${outputmode},enable panel_type=${panel_type} lcd_ctrl=${lcd_ctrl} hdmitx=${cecconfig},${colorattribute} hdmimode=${hdmimode} hdmichecksum=${hdmichecksum} dolby_vision_on=${dolby_vision_on} frac_rate_policy=${frac_rate_policy} hdmi_read_edid=${hdmi_read_edid} cvbsmode=${cvbsmode} osd_reverse=${osd_reverse} video_reverse=${video_reverse} irq_check_en=${Irq_check_en}  androidboot.selinux=${EnableSelinux} androidboot.firstboot=${firstboot} jtag=${jtag} wol_enable=${wol_enable}; "\
 	"setenv bootargs ${bootargs} androidboot.hardware=amlogic androidboot.bootloader=${bootloader_version} androidboot.build.expect.baseband=N/A;"\
             "run cmdline_keys;"\
             "\0"\
@@ -283,6 +283,22 @@
                 "hdmitx hpd;hdmitx get_preferred_mode;hdmitx get_parse_edid;setenv dolby_status 0;setenv dolby_vision_on 0;osd open;osd clear;imgread pic logo bootup $loadaddr;bmp display $bootup_offset;bmp scale;vout output ${outputmode};dovi set;dovi pkg;vpp hdrpkt;"\
             "fi;fi;"\
             "\0"\
+        "wol_init="\
+            "if test ${ext_ethernet} = 0; then "\
+               "kbi powerstate;"\
+               "kbi trigger wol r;"\
+               "if test ${wol_enable} = 1; then "\
+               "kbi trigger wol w 1;"\
+               "fi;"\
+               "setenv bootargs ${bootargs} wol_enable=${wol_enable};"\
+               "if test ${power_state} = 1; then "\
+               "kbi trigger wol w 1;"\
+               "kbi poweroff;"\
+               "fi;"\
+            "else "\
+               "setenv bootargs ${bootargs} wol_enable=0;"\
+            "fi;"\
+            "\0"\
         "cmdline_keys="\
             "if keyman init 0x1234; then "\
                 "if keyman read usid ${loadaddr} str; then "\
@@ -328,6 +344,7 @@
             "run factory_reset_poweroff_protect;"\
             "run upgrade_check;"\
             "run init_display;"\
+            "run wol_init;"\
             "run storeargs;"\
             "run upgrade_key;" \
             "forceupdate;" \
