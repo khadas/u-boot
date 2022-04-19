0d9bdb46b89d4668583668182dba8a22a3400486
jason <jason@khadas.com>
2022-04-19 11:48:36 +0000

    wol: vim3l: add forcereset support
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: Iaf3639b7b7a35bf78f8a830b55478b6dc839bbed

--- board/khadas/configs/kvim3l.h
+++ board/khadas/configs/kvim3l.h
@@ -192,6 +192,7 @@
             "fi;fi;fi;fi;fi;fi;"\
             "\0" \
         "storeboot="\
+            "kbi resetflag 0;"\
             "cfgload;"\
             "if test ${reboot_mode} = normal; then "\
             "else if test ${reboot_mode} = cold_boot; then "\
