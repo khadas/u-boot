07c224ab293414eef7e157f85751af59c705411f
Bo Lv <bo.lv@amlogic.com>
2022-04-26 16:57:25 +0800

    BL33: [S805X2-1G] STB VMX cannot bootup [1/1]
    PD#SWPL-79169
    
    Problem:
    If set secmon mermory to 64M, will overlap ramdisk
    and dtb relocate address.
    
    Solution:
    set env of initrd_high and fdt_high to 0xD000000
    
    Verify:
    s805x2 1g
    
    Change-Id: I7b09ae3683d4fb5238055994f0b6f0e37161a936
    Signed-off-by: Bo Lv <bo.lv@amlogic.com>

--- common/bootm.c
+++ common/bootm.c
@@ -179,8 +179,8 @@ static int bootm_find_os(cmd_tbl_t *cmdtp, int flag, int argc,
 #ifdef CONFIG_ANDROID_BOOT_IMAGE
 	case IMAGE_FORMAT_ANDROID:
 		if (image_get_magic((image_header_t *)images.os.image_start) == IH_MAGIC) {
-			env_set("initrd_high", "0A000000");
-			env_set("fdt_high", "0A000000");
+			env_set("initrd_high", "0D000000");
+			env_set("fdt_high", "0D000000");
 			images.os.arch = ((image_header_t *)(images.os.image_start))->ih_arch;
 			images.os.image_start += sizeof(image_header_t);
 		}
