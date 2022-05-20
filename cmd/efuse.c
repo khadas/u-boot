460717cf9ec0f5fa265450c8a58accb20f91bdaf
jason <jason@khadas.com>
2022-05-20 11:43:01 +0000

    efuse: vim1s: add efuse mac/usid cmd
    Signed-off-by: jason <jason@khadas.com>
    Change-Id: Ic12419034a09c24b8ed6919ab30d68c1bec36b2b

--- cmd/efuse.c
+++ cmd/efuse.c
@@ -24,6 +24,9 @@
 
 #define CMD_EFUSE_AMLOGIC_SET      20
 
+#define setenv env_set
+#define getenv env_get
+
 int cmd_efuse(int argc, char * const argv[], char *buf)
 {
 	int i, action = -1;
@@ -35,6 +38,60 @@ int cmd_efuse(int argc, char * const argv[], char *buf)
 	long lAddr1, lAddr2;
 	unsigned long nType;
 
+	if (strncmp(argv[1], "mac", 3) == 0) {
+		char mac[6] = {0};
+		char mac_addr[17] = {0};
+		int size = 6;
+		uint32_t mac_offset = 0;
+		ret = efuse_read_usr(mac, size, (loff_t *)&mac_offset);
+
+		if (ret == -1) {
+			printf("ERROR: efuse read mac information fail!\n");
+			return -1;
+		}
+
+		if (ret != size)
+			printf("ERROR: read %d byte(s) not %d byte(s) data\n",
+			       ret, size);
+
+		/*for(i = 0; i < 6; i++){
+			if(i != 5){
+				printf("%02x:", mac[i]);
+			}else{
+				printf("%02x", mac[i]);
+			}
+		}
+		printf("\n");
+		*/
+		sprintf(mac_addr,"%02x:%02x:%02x:%02x:%02x:%02x", mac[0],mac[1],mac[2],mac[3],mac[4],mac[5]);
+		setenv("eth_mac", mac_addr);
+		return 0;
+	}
+	if (strncmp(argv[1], "usid", 4) == 0) {
+		char usid[11] = {0};
+		char usid_write[11] = {0};
+		int size = 10;
+		uint32_t usid_offset = 18;
+		ret = efuse_read_usr(usid, size, (loff_t *)&usid_offset);
+
+		if (ret == -1) {
+			printf("ERROR: efuse read mac information fail!\n");
+			return -1;
+		}
+
+		if (ret != size)
+			printf("ERROR: read %d byte(s) not %d byte(s) data\n",
+			       ret, size);
+
+		/*for(i = 0; i < 10; i++){
+			printf("wjh=%c", usid[i]);
+		}
+		printf("\n");
+		*/
+		sprintf(usid_write,"%x%x%x%x%x%x%x%x%x%x", usid[0],usid[1],usid[2],usid[3],usid[4],usid[5],usid[6],usid[7],usid[8],usid[9]);
+		setenv("usid", usid_write);
+		return 0;
+	}
 	if (strncmp(argv[1], "read", 4) == 0) {
 		action = CMD_EFUSE_READ;
 	} else if (strncmp(argv[1], "cali_read", 9) == 0) {
