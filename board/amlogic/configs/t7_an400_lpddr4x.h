46cf33d2efacd4fc450de334e2078da3544d609b
Cheng Wang2 <cheng.wang2@amlogic.com>
2022-06-06 19:02:14 +0800

    BDS: remove duplicate osd configuration [1/1]
    PD#SWPL-83229
    
    Problem:
    dual screen boot logo flashes
    
    Solution:
    remove duplicate osd configuration
    
    Verify:
    T7
    
    Signed-off-by: Cheng Wang2 <cheng.wang2@amlogic.com>
    Change-Id: I78e5b301047a07ecc870ff382d83faeb06c4669b

--- board/amlogic/configs/t7_an400_lpddr4x.h
+++ board/amlogic/configs/t7_an400_lpddr4x.h
@@ -647,7 +647,6 @@
             "\0"\
         "init_display="\
 			"hdmitx hpd;hdmitx get_preferred_mode;hdmitx get_parse_edid;dovi process;"\
-            "osd open;osd clear;run load_bmp_logo;bmp scale;vout output ${outputmode};"\
             "setenv outputmode2 ${hdmimode};"\
             "osd dual_logo;"\
 			"dovi set;dovi pkg;vpp hdrpkt;"\
