62f4675f60bc69ee0a70a2c3cef26878ccb5a008
Artem Lapkin <art@khadas.com>
2022-06-28 16:51:29 +0800

    logo: use more simple way to load logo
    Signed-off-by: Artem Lapkin <art@khadas.com>
    Change-Id: I923a3e8aa9c23d40ae31c698468da1aef1156193

--- include/configs/kedge2.h
+++ include/configs/kedge2.h
@@ -13,8 +13,11 @@
 
 #undef ROCKCHIP_DEVICE_SETTINGS
 #define ROCKCHIP_DEVICE_SETTINGS \
-		"logo_addr_c=0x06A00000\0" \
 		"logo_addr_r=0x07000000\0" \
+		"logo_file_dir=/usr/share/fenix/logo\0" \
+		"logocmd_mmc=load $devtype $devnum:2 $logo_addr_r $logo_file_dir/$logo_file || load $devtype $devnum:3 $logo_addr_r $logo_file_dir/$logo_file\0" \
+		"logocmd_usb=run logocmd_mmc\0" \
+		"logocmd=echo Load logo: $logo_file_dir/$logo_file; run logocmd_${devtype}\0" \
 		"stdout=serial,vidconsole\0" \
 		"stderr=serial,vidconsole\0"
 
