15263f2fe6777b0e6c08b433a7ded416dfbcfa45
Cao Wei <wei.cao@amlogic.com>
2022-07-01 14:39:48 +0800

    P1: HIFI5: Run on 2 memory area with remap demo [2/4]
    PD#SWPL-86555
    
    Problem:
    remap register should be set
    
    Solution:
    add cmd_remapset command
    
    Verify:
    P1
    
    Change-Id: Ib5748b9d3957d02209a461eefc4e3b15594460c1
    Signed-off-by: Cao Wei <wei.cao@amlogic.com>

--- board/amlogic/configs/p1_as409.h
+++ board/amlogic/configs/p1_as409.h
@@ -422,6 +422,9 @@
 //use dache command
 #define CONFIG_CMD_CACHE
 
+//use remapset command
+#define CONFIG_CMD_REMAPSET
+
 /* define CONFIG_UPDATE_MMU_TABLE for need update mmu */
 #define	CONFIG_UPDATE_MMU_TABLE
 
