ae0d4cc1ff2882a079f80085eb667a41216ba657
Nick Xie <nick@khadas.com>
2022-08-11 18:19:44 +0800

    config: VIM1S: add tty0 console
    Signed-off-by: Nick Xie <nick@khadas.com>
    Change-Id: I00456b03e8491a086774acad98f60bbd46c43607

--- board/khadas/configs/kvim1s.h
+++ board/khadas/configs/kvim1s.h
@@ -152,7 +152,7 @@
         "fatload_dev=usb\0"\
         "fs_type=""rootfstype=ramfs""\0"\
         "initargs="\
-            "rootflags=data=writeback rw rootfstype=ext4" CONFIG_KNL_LOG_LEVEL "console=ttyS0,921600 no_console_suspend fsck.repair=yes net.ifnames=0 "\
+            "rootflags=data=writeback rw rootfstype=ext4" CONFIG_KNL_LOG_LEVEL "console=ttyS0,921600 console=tty0 no_console_suspend fsck.repair=yes net.ifnames=0 "\
             "ramoops.pstore_en=1 ramoops.record_size=0x8000 ramoops.console_size=0x4000 loop.max_part=4 "\
             "\0"\
         "upgrade_check="\
