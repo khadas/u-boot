0ea5255dbbcaba23fb9abc3bbf4e77ef92e23480
Shunzhou Jiang <shunzhou.jiang@amlogic.com>
2022-10-01 21:57:54 +0800

    T5M: add T5M power domain [2/3]
    PD#SWPL-96298
    
    Problem:
    add t5m power domain
    
    Solution:
    add t5m power domain
    
    Verify:
    t5m_pxp
    
    Change-Id: Id4522ad51b4bb32d31d408011323dc1221804b38
    Signed-off-by: Shunzhou Jiang <shunzhou.jiang@amlogic.com>

--- arch/arm/mach-meson/t5m/pwrc.c
+++ arch/arm/mach-meson/t5m/pwrc.c
@@ -5,30 +5,9 @@
 
 char* domain_name[] =
 {
-	"PM_DSPA",
 	"PM_DOS_HCODEC",
-	"PM_DOS_HEVC",
-	"PM_DOS_VDEC",
+	"PM_DOS_GE2D_WRAP",
 	"PM_VPU_HDMI",
-	"PM_USB_COMB",
-	"PM_PCIE",
-	"PM_GE2D",
-	"PM_SRAMA",
-	"PM_HDMIRX",
-	"PM_VI_CLK1",
-	"PM_VI_CLK2",
-	"PM_ETH",
-	"PM_NNA",
 	"PM_DEMOD",
-	"PM_FRCTOP",
-	"PM_FRCME",
-	"PM_FRCMC",
-	"PM_SDEMMC_B",
-	"PM_SDEMMC_C",
-	"PM_NOC_DEV",
-	"PM_NOC_VPU",
-	"PM_SPICC0",
-	"PM_SPICC1",
-	"PM_SPICC2",
-	"PM_AUDIO",
+	"PM_FRC_TOP"
 };
