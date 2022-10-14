32c0e48d4864e4906c2571166adf2e3c472d4b48
hongyu.chen1 <hongyu.chen1@amlogic.com>
2022-10-14 11:09:58 +0800

    S5: Change the power domain name type [1/1]
    PD#SWPL-97323
    
    Problem:
    The use of enumerated values in HDMI modules is not recognized.
    
    Solution:
    Change the domain-related enumeration definition to a macro definition
    
    Verify:
    S5
    
    Change-Id: I32c189d62db7ee6531005177cb2fb954aef2253b
    Signed-off-by: hongyu.chen1 <hongyu.chen1@amlogic.com>

--- arch/arm/include/asm/arch-s5/pwr_ctrl.h
+++ arch/arm/include/asm/arch-s5/pwr_ctrl.h
@@ -6,42 +6,41 @@
 #define PWR_ON    1
 #define PWR_OFF   0
 
-typedef enum pdid {
-	PM_MALI_0,
-	PM_MALI_1,
-	PM_MALI_2,
-	PM_MALI_TOP,
-	PM_DOS_TOP_WRAP,
-	PM_DOS_VDEC,
-	PM_DOS_HCODEC,
-	PM_DOS_HEVC_TOP,
-	PM_DOS_HEVC_CORE1,
-	PM_USB2,
-	PM_PCIE0,
-	PM_GE2D,
-	PM_VC9000E,
-	PM_VICP,
-	PM_VGE_TOP,
-	PM_VI_CLK1,
-	PM_VI_CLK2,
-	PM_VPU_HDMI,
-	PM_ETH,
-	PM_PCIE1,
-	PM_VPU_DOLBY,
-	PM_USB30,
-	PM_USB31,
-	PM_SDCARD,
-	PM_SDIO,
-	PM_NAND_EMMC,
-	PM_NNA_4T,
-	PM_DMC00,
-	PM_DMC01,
-	PM_NOC_DMC_TOP,
-	PM_DMC10,
-	PM_DMC11,
-	PM_DDRPHY0,
-	PM_DDRPHY1,
-} PM_E;
+#define PM_MALI_0			0
+#define PM_MALI_1			1
+#define PM_MALI_2			2
+#define PM_MALI_TOP			3
+#define PM_DOS_TOP_WRAP		4
+#define PM_DOS_VDEC			5
+#define PM_DOS_HCODEC		6
+#define PM_DOS_HEVC_TOP		7
+#define PM_DOS_HEVC_CORE1	8
+#define PM_USB2				9
+#define PM_PCIE0			10
+#define PM_GE2D				11
+#define PM_VC9000E			12
+#define PM_VICP				13
+#define PM_VGE_TOP			14
+#define PM_VI_CLK1			15
+#define PM_VI_CLK2			16
+#define PM_VPU_HDMI			17
+#define PM_ETH				18
+#define PM_PCIE1			19
+#define PM_VPU_DOLBY		20
+#define PM_USB30			21
+#define PM_USB31			22
+#define PM_SDCARD			23
+#define PM_SDIO				24
+#define PM_NAND_EMMC		25
+#define PM_NNA_4T			26
+#define PM_DMC00			27
+#define PM_DMC01			28
+#define PM_NOC_DMC_TOP		29
+#define PM_DMC10			30
+#define PM_DMC11			31
+#define PM_DDRPHY0			32
+#define PM_DDRPHY1			33
+
 #define PM_MAX (PM_DDRPHY1 + 1)
 
 unsigned long viu_init_psci_smc(void);
