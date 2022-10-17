7696ac976a85dc9f7301712a1a03c8fab10369cf
Jian Hu <jian.hu@amlogic.com>
2022-10-17 22:15:10 +0800

    clk: add clk support [1/1]
    PD#SWPL-104716
    
    Problem:
    t5m clk bringup
    
    Solution:
    add clk support
    
    Verify:
    t5m ptm
    
    Change-Id: I11844aeb87eb9e19fabf382d49f29483421c3546
    Signed-off-by: Jian Hu <jian.hu@amlogic.com>

--- arch/arm/include/asm/arch-t5m/clock.h
+++ arch/arm/include/asm/arch-t5m/clock.h
@@ -4,24 +4,24 @@ License-Identifier: (GPL-2.0+ OR MIT) */
  * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
  */
 
-#ifndef __CLOCK_T3_H
-#define __CLOCK_T3_H
+#ifndef __CLOCK_T5M_H
+#define __CLOCK_T5M_H
 
 // REG_BASE:  REGISTER_BASE_ADDR = 0xfe000000
 // -----------------------------------------------
-#define T3_CLKCTRL_NAND_CLK_CTRL                      (0x005a  << 2)
-#define T3_CLKCTRL_SD_EMMC_CLK_CTRL                   (0x005b  << 2)
-#define T3_CLKCTRL_SPICC_CLK_CTRL                     (0x005d  << 2)
-#define T3_CLKCTRL_SAR_CLK_CTRL0                      (0x005f  << 2)
-#define T3_CLKCTRL_SPICC_CLK_CTRL1                    (0x0070  << 2)
+#define T5M_CLKCTRL_NAND_CLK_CTRL                      (0x005a  << 2)
+#define T5M_CLKCTRL_SD_EMMC_CLK_CTRL                   (0x005b  << 2)
+#define T5M_CLKCTRL_SPICC_CLK_CTRL                     (0x005d  << 2)
+#define T5M_CLKCTRL_SAR_CLK_CTRL0                      (0x005f  << 2)
+#define T5M_CLKCTRL_SPICC_CLK_CTRL1                    (0x0070  << 2)
 
 //========================================================================
 
 // REG_BASE:  REGISTER_BASE_ADDR = 0xfe008000
 // -----------------------------------------------
-#define T3_ANACTRL_SYSPLL_CTRL0                       ((0x00 << 2) + 0x8000)
-#define T3_ANACTRL_FIXPLL_CTRL0                       ((0x10 << 2) + 0x8000)
-#define T3_ANACTRL_GP0PLL_CTRL0                       ((0x20 << 2) + 0x8000)
+#define T5M_ANACTRL_SYS0PLL_CTRL0                      ((0x00 << 2) + 0x8000)
+#define T5M_ANACTRL_FIXPLL_CTRL0                       ((0x10 << 2) + 0x8000)
+#define T5M_ANACTRL_GP0PLL_CTRL0                       ((0x20 << 2) + 0x8000)
 //========================================================================
 
 /*
@@ -29,7 +29,7 @@ License-Identifier: (GPL-2.0+ OR MIT) */
  * APB_BASE:  APB1_BASE_ADDR = 0xfe007400
  */
 
-#include <dt-bindings/clock/t3-clkc.h>
+#include <dt-bindings/clock/t5m-clkc.h>
 #define NR_CLKS				(CLKID_END_BASE)
 
-#endif /* __CLOCK_T3_H */
+#endif /* __CLOCK_T5M_H */
