6a7769d272325663cb6c6a728f5c86c8585f113a
ziyi <ziyi.huang@amlogic.com>
2022-10-28 20:11:49 +0800

    emmc: coverity check and bug [2/2]
    PD#SWPL-97256
    
    Problem:
    bug in gpt partition name transform to char
    
    Solution:
    modify function parameter to avoid access array out of bounds.
    
    Verify:
    local
    
    Change-Id: I8d17cd316cf39a17b5738a2bc3a69c9d63003f3d
    Signed-off-by: ziyi <ziyi.huang@amlogic.com>

--- disk/part_efi.c
+++ disk/part_efi.c
@@ -726,7 +726,7 @@ int gpt_verify_partitions(struct blk_desc *dev_desc,
 
 		/* Check if GPT and ENV partition names match */
 		gpt_convert_efi_name_to_char(efi_str, gpt_e[i].partition_name,
-					     PARTNAME_SZ + 1);
+					     PARTNAME_SZ);
 
 		debug("%s: part: %2d name - GPT: %16s, ENV: %16s ",
 		      __func__, i, efi_str, partitions[i].name);
