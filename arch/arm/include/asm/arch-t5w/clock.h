9d8c8f0dcedf0f2553d80f3ee87f4cbe5f47c070
Huqiang Qin <huqiang.qin@amlogic.com>
2022-11-01 15:32:05 +0800

    saradc: support for t5w saradc in uboot 2019 [1/1]
    PD#TV-65882
    
    Problem:
    Support for t5w saradc in uboot 2019.
    
    Solution:
    Support for t5w saradc in uboot 2019.
    
    Verify:
    T5W
    
    Change-Id: Ie69af967df38dcd03c7b089374c20fe0f32a6547
    Signed-off-by: Huqiang Qin <huqiang.qin@amlogic.com>

--- arch/arm/include/asm/arch-t5w/clock.h
+++ arch/arm/include/asm/arch-t5w/clock.h
@@ -12,7 +12,7 @@
 #define T5W_CLKCTRL_NAND_CLK_CTRL                      (0x0097  << 2)
 #define T5W_CLKCTRL_SD_EMMC_CLK_CTRL                   (0x0099  << 2)
 #define T5W_CLKCTRL_SPICC_CLK_CTRL                     (0x00f7  << 2)
-#define T5W_CLKCTRL_SAR_CLK_CTRL0                      ((0x0090  << 2) + 0x1BA000)//????
+#define T5W_CLKCTRL_SAR_CLK_CTRL0                      ((0x0024  << 2) + 0x1BA000)//????
 #define T5W_CLKCTRL_SPICC_CLK_CTRL1                    (0x00f8  << 2)
 
 #define T5W_ANACTRL_SYSPLL_CTRL0                       (0xe0 << 2)
