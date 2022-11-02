f0a334e258b7a295bd70ddfefb893bc76b76e22c
Liqiang Jin <liqiang.jin@amlogic.com>
2022-11-02 11:58:18 +0800

    S928X Watermark: Enable Watermark init cmd [1/1]
    PD#SWPL-97239
    
    Problem:
    Enable Watermark init cmd
    
    Solution:
    Enable Watermark init cmd
    
    Verify:
    Android R + S5(S928X)
    
    Change-Id: Ifcdc7a1f17083f34f282591b888d7eca56d18675
    Signed-off-by: Liqiang Jin <liqiang.jin@amlogic.com>

--- board/amlogic/configs/s5_ax208.h
+++ board/amlogic/configs/s5_ax208.h
@@ -516,6 +516,8 @@
 #define CONFIG_FAT_WRITE 1
 #define CONFIG_AML_FACTORY_PROVISION 1
 
+#define CONFIG_AML_WATERMARK 1
+
 /* Cache Definitions */
 /* #define CONFIG_SYS_DCACHE_OFF */
 /* #define CONFIG_SYS_ICACHE_OFF */
