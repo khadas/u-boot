d3fc5e7c0a3dd4d225b62a6037df8f2ca2d9e5ee
lizhi.hu <lizhi.hu@amlogic.com>
2022-12-06 21:54:55 +0800

    lcd: typo fix [1/1]
    PD#SWPL-103966
    
    Problem:
    typo check failed
    
    Solution:
    fix wrong word
    
    Verify:
    None
    
    Change-Id: Iaa69a5dfc6444b9ddf5968565dfee53d94f9649b
    Signed-off-by: lizhi.hu <lizhi.hu@amlogic.com>

--- drivers/amlogic/media/vout/lcd/lcd_tablet/edp_tx.c
+++ drivers/amlogic/media/vout/lcd/lcd_tablet/edp_tx.c
@@ -292,7 +292,7 @@ static int dptx_edid_valid_check(unsigned char *edid_buf)
 	return 0;
 }
 
-static void dptx_edid_parase(unsigned char *edid_buf, struct dptx_edid_s *edp_edid)
+static void dptx_edid_parse(unsigned char *edid_buf, struct dptx_edid_s *edp_edid)
 {
 	struct dptx_edid_range_limit_s *range;
 	struct dptx_edid_timing_s *timing;
@@ -491,7 +491,7 @@ dptx_edid_dump_retry:
 		free(str);
 	}
 
-	dptx_edid_parase(edid_buf, &edp_edid);
+	dptx_edid_phrase(edid_buf, &edp_edid);
 	dptx_edid_print(&edp_edid);
 
 	free(edid_buf);
@@ -559,7 +559,7 @@ dptx_edid_timing_probe_retry:
 	if ((edp_cfg->edid_state & EDP_EDID_STATE_APPLY) == 0) {
 		if (edp_cfg->edid_retry_cnt++ > EDP_EDID_RETRY_MAX)
 			return -1;
-		dptx_edid_parase(edid_buf, &edp_edid);
+		dptx_edid_phrase(edid_buf, &edp_edid);
 		if (lcd_debug_print_flag & LCD_DBG_PR_NORMAL)
 			dptx_edid_print(&edp_edid);
 		edp_edid_timing_update(pdrv, &edp_edid);
