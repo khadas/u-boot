54b4bcce0e02bd09011910ef07abfe2c42d9b1a3
Jie Dai <jie.dai@amlogic.com>
2023-01-13 19:08:37 +0800

    lcd: t5m secure tcon verify [1/1]
    PD#SWPL-106778
    
    Problem:
    need veriy secure tcon
    
    Solution:
    add secure relted code for t5m
    
    Verify:
    ay301
    
    Change-Id: I37b2879b0f5b5d0de3aed3284dcc77adb1acd301
    Signed-off-by: Jie Dai <jie.dai@amlogic.com>

--- drivers/amlogic/media/common/vpu/vpu_power.c
+++ drivers/amlogic/media/common/vpu/vpu_power.c
@@ -45,11 +45,16 @@ void vpu_module_init_config(void)
 		vpu_vcbus_write(VPU_RDARB_MODE_L1C1, 0x0); //0x210000
 		vpu_vcbus_write(VPU_RDARB_MODE_L1C2, 0x10000);
 	}
-	vpu_vcbus_write(VPU_RDARB_MODE_L2C1, 0x900000);
+	if (vpu_conf.data->chip_type == VPU_CHIP_T5M)
+		vpu_vcbus_write(VPU_RDARB_MODE_L2C1, 0x400000);
+	else
+		vpu_vcbus_write(VPU_RDARB_MODE_L2C1, 0x900000);
+
 	vpu_vcbus_write(VPU_WRARB_MODE_L2C1, 0x20000);
 #ifdef CONFIG_AMLOGIC_TEE
 	if (vpu_conf.data->chip_type == VPU_CHIP_T3 ||
-	    vpu_conf.data->chip_type == VPU_CHIP_T5W)
+	    vpu_conf.data->chip_type == VPU_CHIP_T5W ||
+		vpu_conf.data->chip_type == VPU_CHIP_T5M)
 		viu_init_psci_smc();
 #endif
 	/* S5 new add registers */
