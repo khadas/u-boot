f1d8a975f4c98363fc70ded75d31d3f60876f4f2
hongyu.chen1 <hongyu.chen1@amlogic.com>
2023-01-16 15:02:35 +0800

    A4: add power domain. [2/3]
    PD#SWPL-108803
    
    Problem:
    need power domain in bl33.
    
    Solution:
    add power domain in bl33.
    
    Verify:
    pxp
    
    Change-Id: I621d4f57e553a4c06af4388ea2017b3088e808be
    Signed-off-by: hongyu.chen1 <hongyu.chen1@amlogic.com>

--- arch/arm/include/asm/arch-a4/pwr_ctrl.h
+++ arch/arm/include/asm/arch-a4/pwr_ctrl.h
@@ -8,34 +8,23 @@
 
 #define PWR_ON    1
 #define PWR_OFF   0
-#define PM_NNA    0
-#define PM_AUDIO  1
-#define PM_SDIOA  2
-#define PM_EMMC   3
-#define PM_USB_COMB     4
-#define PM_ETH          5
-#define PM_RSA          6
-#define PM_AUDIO_PDM    7
-#define PM_DMC          8
-#define PM_SYS_WRAP     9
-#define PM_DSPA         10
 
-#define PM_MAX		11
+#define PM_AUDIO	0
+#define PM_SDIOA	1
+#define PM_EMMC  2
+#define PM_USB_COMB   3
+#define PM_ETH     4
+#define PM_VOUT          5
+#define PM_AUDIO_PDM          6
+#define PM_DMC    7
+#define PM_SYS_WRAP          8
+#define PM_AO_I2C_S     9
+#define PM_AO_UART         10
+#define PM_AO_IR     11
 
-//static int pm_max = 8;
-#if 0
-char* domain_name[] =
-{
-	"PM_DOS_HEVC",
-	"PM_DOS_VDEC",
-	"PM_VPU_HDMI",
-	"PM_USB_COMB",
-	"PM_GE2D",
-	"PM_ETH",
-	"PM_DEMOD",
-	"PM_AUDIO",
-};
-#endif
+#define PM_MAX		12
+
 unsigned long viu_init_psci_smc(void);
 unsigned long pwr_ctrl_psci_smc(unsigned int power_domain, bool power_control);
+unsigned long pwr_ctrl_status_psci_smc(unsigned int power_domain);
 #endif
--- arch/arm/mach-meson/a4/pwrc.c
+++ arch/arm/mach-meson/a4/pwrc.c
@@ -4,15 +4,16 @@
  */
 
 char *domain_name[] = {
-	"PM_NNA",
 	"PM_AUDIO",
 	"PM_SDIOA",
 	"PM_EMMC",
 	"PM_USB_COMB",
 	"PM_ETH",
-	"PM_RSA",
+	"PM_VOUT",
 	"PM_AUDIO_PDM",
 	"PM_DMC",
 	"PM_SYS_WRAP",
-	"PM_DSPA",
+	"PM_AO_I2C_S",
+	"PM_AO_UART",
+	"PM_AO_IR"
 };
