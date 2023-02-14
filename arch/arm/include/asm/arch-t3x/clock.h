9ddac94e17d5f0297cdfc66f74d6efec1be89fb5
Jian Hu <jian.hu@amlogic.com>
2023-02-14 12:07:33 +0000

    clk: add t3x clk support [1/1]
    PD#SWPL-111570
    
    Problem:
    t3x clk bringup
    
    Solution:
    add clk support
    
    Verify:
    t3x mimic
    
    Change-Id: I1caf1c639fdec0da234665f3e3d69afdcca97e1b
    Signed-off-by: Jian Hu <jian.hu@amlogic.com>
    Signed-off-by: benlong.zhou <benlong.zhou@amlogic.com>

--- arch/arm/include/asm/arch-t3x/clock.h
+++ arch/arm/include/asm/arch-t3x/clock.h
@@ -4,24 +4,26 @@ License-Identifier: (GPL-2.0+ OR MIT) */
  * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
  */
 
-#ifndef __CLOCK_T5M_H
-#define __CLOCK_T5M_H
+#ifndef __CLOCK_T3X_H
+#define __CLOCK_T3X_H
 
 // REG_BASE:  REGISTER_BASE_ADDR = 0xfe000000
 // -----------------------------------------------
-#define T5M_CLKCTRL_NAND_CLK_CTRL                      (0x005a  << 2)
-#define T5M_CLKCTRL_SD_EMMC_CLK_CTRL                   (0x005b  << 2)
-#define T5M_CLKCTRL_SPICC_CLK_CTRL                     (0x005d  << 2)
-#define T5M_CLKCTRL_SAR_CLK_CTRL0                      (0x005f  << 2)
-#define T5M_CLKCTRL_SPICC_CLK_CTRL1                    (0x0070  << 2)
+#define T3X_CLKCTRL_NAND_CLK_CTRL                      (0x005a  << 2)
+#define T3X_CLKCTRL_SD_EMMC_CLK_CTRL                   (0x005b  << 2)
+#define T3X_CLKCTRL_SPICC_CLK_CTRL                     (0x005d  << 2)
+#define T3X_CLKCTRL_SAR_CLK_CTRL0                      (0x005f  << 2)
+#define T3X_CLKCTRL_SPICC_CLK_CTRL                     (0x005d  << 2)
+#define T3X_CLKCTRL_SPICC_CLK_CTRL1                    (0x0070  << 2)
+#define T3X_CLKCTRL_SPICC_CLK_CTRL2                    (0x0071  << 2)
 
 //========================================================================
 
-// REG_BASE:  REGISTER_BASE_ADDR = 0xfe008000
+// REG_BASE:  REGISTER_BASE_ADDR = 0xfe000000
 // -----------------------------------------------
-#define T5M_ANACTRL_SYS0PLL_CTRL0                      ((0x00 << 2) + 0x8000)
-#define T5M_ANACTRL_FIXPLL_CTRL0                       ((0x10 << 2) + 0x8000)
-#define T5M_ANACTRL_GP0PLL_CTRL0                       ((0x20 << 2) + 0x8000)
+#define T3X_CLKCTRL_SYS0PLL_CTRL0                      (0x200 << 2)
+#define T3X_CLKCTRL_FIXPLL_CTRL0                       (0x220 << 2)
+#define T3X_CLKCTRL_GP0PLL_CTRL0                       (0x240 << 2)
 //========================================================================
 
 /*
@@ -29,7 +31,7 @@ License-Identifier: (GPL-2.0+ OR MIT) */
  * APB_BASE:  APB1_BASE_ADDR = 0xfe007400
  */
 
-#include <dt-bindings/clock/t5m-clkc.h>
+#include <dt-bindings/clock/t3x-clkc.h>
 #define NR_CLKS				(CLKID_END_BASE)
 
-#endif /* __CLOCK_T5M_H */
+#endif /* __CLOCK_T3X_H */
