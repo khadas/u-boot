ad2d910d64c65522795036eb659e9fcba4433858
Evoke Zhang <evoke.zhang@amlogic.com>
2023-03-02 16:55:23 +0800

    lcd: tcon: support cmd for tool [1/1]
    PD#SWPL-113555
    
    Problem:
    need support uboot cmd for tcon tool
    
    Solution:
    add uboot cmd for tcon tool:
    aml_tcon reg rn <bit_width> <reg_addr> [len]
    aml_tcon reg wm <bit_width> <reg_addr> <mask> <val>
    aml_tcon reg wn <bit_width> <reg_addr> <len> <val0> <val1> ...
    aml_tcon reg ws <bit_width> <reg_addr> <len> <val0> <val1> ...
    
    Verify:
    ar301
    
    Change-Id: I80659250d4519bb3cffea49a7052bedd880b8a25
    Signed-off-by: Evoke Zhang <evoke.zhang@amlogic.com>

--- cmd/amlogic/cmd_lcd.c
+++ cmd/amlogic/cmd_lcd.c
@@ -1128,3 +1128,217 @@ U_BOOT_CMD(
 	"lcd2 ext          - show lcd extern information\n"
 #endif
 );
+
+#ifdef CONFIG_AML_LCD_TCON
+#define DBG_TCON_REG_8_bit             1
+#define DBG_TCON_REG_32_bit            0
+
+static int do_aml_tcon_reg(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	struct aml_lcd_drv_s *pdrv = aml_lcd_get_driver(0);
+	unsigned int addr, mask, val, temp, bit_width, len;
+	int ret = 0, i;
+
+	if (!pdrv) {
+		printf("for_tool:ERROR\n");
+		printf("no lcd driver\n");
+		return 0;
+	}
+	if (!pdrv->tcon_reg_write) {
+		printf("for_tool:ERROR\n");
+		printf("tcon_reg_write is null\n");
+		return 0;
+	}
+	if (!pdrv->tcon_reg_read) {
+		printf("for_tool:ERROR\n");
+		printf("tcon_reg_read is null\n");
+		return 0;
+	}
+	if (strcmp(argv[1], "wn") == 0) {
+		if (argc < 5) {
+			printf("for_tool:ERROR\n");
+			printf("argc is not enough\n");
+			return 0;
+		}
+		if (strcmp(argv[2], "8") == 0) {
+			bit_width = DBG_TCON_REG_8_bit;
+		} else if (strcmp(argv[2], "32") == 0) {
+			bit_width = DBG_TCON_REG_32_bit;
+		} else {
+			printf("for_tool:ERROR\n");
+			printf("aml_tcon reg bit_width %s invalid\n", argv[2]);
+			return 0;
+		}
+		addr = (unsigned int)simple_strtoul(argv[3], NULL, 16);
+		len = (unsigned int)simple_strtoul(argv[4], NULL, 10);
+		if (len == 0 || len > 16) {
+			printf("for_tool:ERROR\n");
+			printf("aml_tcon reg len %d not support\n", len);
+			return 0;
+		}
+		if (argc < (len + 5)) {
+			printf("for_tool:ERROR\n");
+			printf("aml_tcon reg data cnt %d not enough\n", (argc - 5));
+			return 0;
+		}
+		for (i = 0; i < len; i++) {
+			val = (unsigned int)simple_strtoul(argv[5 + i], NULL, 16);
+			pdrv->tcon_reg_write(addr + i, val, bit_width);
+		}
+		printf("for_tool:");
+		if (bit_width == DBG_TCON_REG_8_bit) {
+			for (i = 0; i < len; i++) {
+				temp = pdrv->tcon_reg_read(addr + i, bit_width);
+				printf("0x%04x=0x%02x ", addr + i, temp);
+			}
+		} else {
+			for (i = 0; i < len; i++) {
+				temp = pdrv->tcon_reg_read(addr + i, bit_width);
+				printf("0x%04x=0x%08x ", addr + i, temp);
+			}
+		}
+		printf("\n");
+	} else if (strcmp(argv[1], "wm") == 0) {
+		if (argc < 6) {
+			printf("for_tool:ERROR\n");
+			printf("argc is not enough\n");
+			return 0;
+		}
+		if (strcmp(argv[2], "8") == 0) {
+			bit_width = DBG_TCON_REG_8_bit;
+		} else if (strcmp(argv[2], "32") == 0) {
+			bit_width = DBG_TCON_REG_32_bit;
+		} else {
+			printf("for_tool:ERROR\n");
+			printf("aml_tcon reg bit_width %s invalid\n", argv[2]);
+			return 0;
+		}
+		addr = (unsigned int)simple_strtoul(argv[3], NULL, 16);
+		mask = (unsigned int)simple_strtoul(argv[4], NULL, 16);
+		val = (unsigned int)simple_strtoul(argv[5], NULL, 16);
+
+		temp = pdrv->tcon_reg_read(addr, bit_width);
+		temp &= ~mask;
+		temp |= val & mask;
+		pdrv->tcon_reg_write(addr, temp, bit_width);
+		printf("for_tool:");
+		temp = pdrv->tcon_reg_read(addr, bit_width);
+		if (bit_width == DBG_TCON_REG_8_bit)
+			printf("0x%04x=0x%02x\n", addr, temp);
+		else
+			printf("0x%04x=0x%08x\n", addr, temp);
+	} else if (strcmp(argv[1], "ws") == 0) {
+		if (argc < 5) {
+			printf("for_tool:ERROR\n");
+			printf("argc is not enough\n");
+			return 0;
+		}
+		if (strcmp(argv[2], "8") == 0) {
+			bit_width = DBG_TCON_REG_8_bit;
+		} else if (strcmp(argv[2], "32") == 0) {
+			bit_width = DBG_TCON_REG_32_bit;
+		} else {
+			printf("for_tool:ERROR\n");
+			printf("aml_tcon reg bit_width %s invalid\n", argv[2]);
+			return 0;
+		}
+		addr = (unsigned int)simple_strtoul(argv[3], NULL, 16);
+		len = (unsigned int)simple_strtoul(argv[4], NULL, 10);
+		if (len == 0 || len > 16) {
+			printf("for_tool:ERROR\n");
+			printf("aml_tcon reg len %d not support\n", len);
+			return 0;
+		}
+		if (argc < (len + 5)) {
+			printf("for_tool:ERROR\n");
+			printf("aml_tcon reg data cnt %d not enough\n", (argc - 5));
+			return 0;
+		}
+		for (i = 0; i < len; i++) {
+			val = (unsigned int)simple_strtoul(argv[5 + i], NULL, 16);
+			pdrv->tcon_reg_write(addr, val, bit_width);
+		}
+		printf("for_tool:");
+		if (bit_width == DBG_TCON_REG_8_bit) {
+			for (i = 0; i < len; i++) {
+				temp = pdrv->tcon_reg_read(addr, bit_width);
+				printf("0x%04x=0x%02x ", addr, temp);
+			}
+		} else {
+			for (i = 0; i < len; i++) {
+				temp = pdrv->tcon_reg_read(addr, bit_width);
+				printf("0x%04x=0x%08x ", addr, temp);
+			}
+		}
+		printf("\n");
+	} else if (strcmp(argv[1], "rn") == 0) {
+		if (argc < 4) {
+			printf("for_tool:ERROR\n");
+			printf("argc is not enough\n");
+			return 0;
+		}
+		if (strcmp(argv[2], "8") == 0) {
+			bit_width = DBG_TCON_REG_8_bit;
+		} else if (strcmp(argv[2], "32") == 0) {
+			bit_width = DBG_TCON_REG_32_bit;
+		} else {
+			printf("for_tool:ERROR\n");
+			printf("aml_tcon reg bit_width %s invalid\n", argv[2]);
+			return 0;
+		}
+		addr = (unsigned int)simple_strtoul(argv[3], NULL, 16);
+		if (argc == 4)
+			len = 1;
+		else
+			len = (unsigned int)simple_strtoul(argv[4], NULL, 10);
+		if (len == 0 || len > 16) {
+			printf("for_tool:ERROR\n");
+			printf("aml_tcon reg len %d not support\n", len);
+			return 0;
+		}
+		printf("for_tool:");
+		if (bit_width == DBG_TCON_REG_8_bit) {
+			for (i = 0; i < len; i++) {
+				temp = pdrv->tcon_reg_read(addr + i, bit_width);
+				printf("0x%04x=0x%02x ", addr + i, temp);
+			}
+		} else {
+			for (i = 0; i < len; i++) {
+				temp = pdrv->tcon_reg_read(addr + i, bit_width);
+				printf("0x%04x=0x%08x ", addr + i, temp);
+			}
+		}
+		printf("\n");
+	} else {
+		printf("for_tool:ERROR\n");
+		ret = -1;
+	}
+	return ret;
+}
+
+static cmd_tbl_t cmd_aml_tcon_sub[] = {
+	U_BOOT_CMD_MKENT(reg,   32, 0, do_aml_tcon_reg, "", ""),
+};
+
+static int do_aml_tcon(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	cmd_tbl_t *c;
+
+	/* Strip off leading 'bmp' command argument */
+	argc--;
+	argv++;
+
+	c = find_cmd_tbl(argv[0], &cmd_aml_tcon_sub[0], ARRAY_SIZE(cmd_aml_tcon_sub));
+
+	if (c)
+		return c->cmd(cmdtp, flag, argc, argv);
+
+	cmd_usage(cmdtp);
+	return 1;
+}
+
+U_BOOT_CMD(aml_tcon,	32,	0,	do_aml_tcon,
+	"aml_tcon sub-system",
+	"aml_tcon reg        - aml_tcon reg access\n"
+);
+#endif
