d28e81425d88fc93b23ea1fcf03ad8f74a743bd3
hongyu.chen1 <hongyu.chen1@amlogic.com>
2023-05-10 15:04:38 +0800

    C1: add power domain. [1/2]
    PD#SWPL-121104
    
    Problem:
    need power domain support.
    
    Solution:
    add power domain in bl33.
    
    Verify:
    ae400
    
    Change-Id: Id97a4163160ff1ee07b527fd3dd9a32782b97830
    Signed-off-by: hongyu.chen1 <hongyu.chen1@amlogic.com>

---
+++ arch/arm/mach-meson/c1/pwrc.c
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: (GPL-2.0+ OR MIT)
+/*
+ * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+ */
+
+char *domain_name[] = {
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"PM_DSP_A",
+	"PM_DSP_B",
+	"PM_UART",
+	"PM_DMC",
+	"PM_I2C",
+	"PM_SDEMMC_B",
+	"PM_ACODEC",
+	"PM_AUDIO",
+	"PM_MKL_OTP",
+	"PM_DMA",
+	"PM_SDEMMC_A",
+	"PM_SRAM_A",
+	"PM_SRAM_B",
+	"PM_IR",
+	"PM_SPICC",
+	"PM_SPIFC",
+	"PM_USB",
+	"PM_NIC",
+	"PM_PDM",
+	"PM_RSA",
+	"PM_MIPI_ISP",
+	"PM_HCODEC",
+	"PM_WAVE",
+	"PM_SDEMMC_C",
+	"PM_SRAM_C",
+	"PM_GDC",
+	"PM_GE2D",
+	"PM_NNA",
+	"PM_ETH",
+	"PM_GIC",
+	"PM_DDR",
+	"PM_SPICC_B",
+};
+
