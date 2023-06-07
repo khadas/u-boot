997bf93aa5a0539cc80654569efc59cca9ad2695
Nick Xie <nick@khadas.com>
2023-06-07 23:08:27 +0800

    VIM4: disable lcd when screen not attached
    Signed-off-by: Nick Xie <nick@khadas.com>
    Change-Id: Ib92b6c87920f18aa9ba2854deb00a2b2203104a4

--- cmd/pxe.c
+++ cmd/pxe.c
@@ -1034,7 +1034,7 @@ static int label_boot(cmd_tbl_t *cmdtp, struct pxe_label *label)
 #endif
 			if (!strcmp(env_get("mipi_lcd_exist"), "0")) {
 				printf("MIPI LCD not exist, disable lcd node.\n");
-				run_command("fdt addr ${fdt_addr_r}; fdt resize 65536; fdt set /lcd status disabled;", 0);
+				run_command("fdt addr ${fdt_addr_r}; fdt resize 65536; fdt set /lcd status disabled;fdt set /lcd1 status disabled;fdt set /lcd2 status disabled", 0);
 			} else if (!strcmp(env_get("mipi_lcd_exist"), "1")) {
 				// Set fbdev size to TS050 MIPI LCD resolution 1080x1920
 				run_command("fdt addr ${fdt_addr_r}; fdt resize 65536; fdt set /drm-subsystem fbdev_sizes <1080 1920 1080 3840 32>;", 0);
