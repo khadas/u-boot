848fcba2c598bc231c8dbe823f507423eac26ca9
huijie.huang <huijie.huang@amlogic.com>
2023-06-19 19:33:29 +0800

    lcd: fix mlvds pi_clk_sel [1/3]
    PD#SWPL-128661
    
    Problem:
    Need to update mlvds pi_clk_sel calculation
    
    Solution:
    Fix pi_clk_sel calculation
    
    Verify:
    TV platform
    
    Change-Id: I7c83657cb34b2feb2687a33ec58ed8399d268a61
    Signed-off-by: huijie.huang <huijie.huang@amlogic.com>

--- drivers/amlogic/media/vout/lcd/lcd_common.c
+++ drivers/amlogic/media/vout/lcd/lcd_common.c
@@ -2185,31 +2185,8 @@ void lcd_mlvds_config_set(struct aml_lcd_drv_s *pdrv)
 	channel_sel0 = pconf->control.mlvds_cfg.channel_sel0;
 	channel_sel1 = pconf->control.mlvds_cfg.channel_sel1;
 	switch (pdrv->data->chip_type) {
-	case LCD_CHIP_TXHD2:
-		/* mlvds channel:    //tx 10 channels
-		 *    0: d0_a
-		 *    1: d1_a
-		 *    2: d2_a
-		 *    3: clk_a
-		 *    4: d3_a
-		 *    5: d0_b
-		 *    6: d1_b
-		 *    7: clk_b
-		 *    8: d2_b
-		 *    9: d3_b
-		 */
-		for (i = 0; i < 8; i++) {
-			temp = (channel_sel0 >> (i * 4)) & 0xf;
-			if (temp == 3 || temp == 7)
-				pi_clk_sel |= (1 << i);
-		}
-		for (i = 0; i < 2; i++) {
-			temp = (channel_sel1 >> (i * 4)) & 0xf;
-			if (temp == 3 || temp == 7)
-				pi_clk_sel |= (1 << (i + 8));
-		}
-		break;
-	default:
+	case LCD_CHIP_TL1:
+	case LCD_CHIP_TM2:
 		/* mlvds channel:    //tx 12 channels
 		 *    0: clk_a
 		 *    1: d0_a
@@ -2234,6 +2211,35 @@ void lcd_mlvds_config_set(struct aml_lcd_drv_s *pdrv)
 			if (temp == 0 || temp == 6)
 				pi_clk_sel |= (1 << (i + 8));
 		}
+		break;
+	case LCD_CHIP_T5:
+	case LCD_CHIP_T5D:
+	case LCD_CHIP_T3:
+	case LCD_CHIP_T5W:
+	case LCD_CHIP_T5M:
+	case LCD_CHIP_TXHD2:
+		/* mlvds channel:    //tx 8 channels
+		 *    0: d0_a
+		 *    1: d1_a
+		 *    2: d2_a
+		 *    3: clk_a
+		 *    4: d0_b
+		 *    5: d1_b
+		 *    6: d2_b
+		 *    7: clk_b
+		 */
+		for (i = 0; i < 8; i++) {
+			temp = (channel_sel0 >> (i * 4)) & 0xf;
+			if (temp == 3 || temp == 7)
+				pi_clk_sel |= (1 << i);
+		}
+		for (i = 0; i < 4; i++) {
+			temp = (channel_sel1 >> (i * 4)) & 0xf;
+			if (temp == 3 || temp == 7)
+				pi_clk_sel |= (1 << (i + 8));
+		}
+		break;
+	default:
 		break;
 	}
 
