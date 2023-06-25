ee83ca2058cbcb390d3070f8a7da55e4630a9c30
bangzheng.liu <bangzheng.liu@amlogic.com>
2023-06-25 11:34:27 +0800

    bl33: uboot dsp cmd: modify dsp clk for all hifi5 [1/1]
    PD#SWPL-129082
    
    Problem:
    modify dsp clk for all hifi5.
    
    Solution:
    modify dsp clk for all hifi5.
    modify code styles.
    
    Verify:
    t7_an400_lpddr4x, t3x_bc302
    
    Change-Id: Ic6639062ad0926b4322ac1643f28495c82c02de0
    Signed-off-by: bangzheng.liu <bangzheng.liu@amlogic.com>

--- cmd/cmd_startdsp.c
+++ cmd/cmd_startdsp.c
@@ -23,85 +23,136 @@
 
 /*
  * clk_util_set_dsp_clk
- * freq_sel: 0:800MHz  fclk_7
- *           1:400MHz  fclk_5
- *           2:500MHz  fclk_2/2
+ * freq_sel: 0:800MHz  fclk_2p5
+ *           1:667MHz  fclk_3
+ *           2:500MHz  fclk_4
+ *           3:400MHz  fclk_2p5/2
  *           4:333MHz  fclk_3/2
- *           5:250Mhz  fclk_2/4
- *           6:200Mhz  fclk_5/2
- *           7:100Mhz  fclk_5/4
- *           8:24Mhz   oscin
- *           others:286MHz fclk/7
+ *           5:250MHz  fclk_4/2
+ *           6:200MHz  fclk_2p5/2
+ *           7:100MHz  fclk_2p5/8
+ *           8:24MHz   oscin
+ *           9:3MHz    oscin/3
+ *     default:800MHz  fclk_2p5
+ *      others:286MHz  fclk_7
  */
 
-void set_dsp_clk(uint32_t id, uint32_t freq_sel)
+int set_dsp_clk(uint32_t id, uint32_t freq_sel)
 {
-	int			control;
-	uint32_t	clk_sel;
-	uint32_t	clk_div;
-	uint32_t	addr = 0;
+	uint32_t control;
+	uint32_t clk_sel;
+	uint32_t clk_div;
+	uint32_t addr = 0;
+	char *dsp_clk_tag = NULL;
+	int ret = 0;
 
-	switch ( id )
-	{
-		case 0  :  addr = CLKCTRL_DSPA_CLK_CTRL0; break;
-#ifndef CONFIG_CLK_A5_TYPE
-		case 1  :  addr = CLKCTRL_DSPB_CLK_CTRL0; break;
+	switch (id) {
+	case 0:
+		addr = CLKCTRL_DSPA_CLK_CTRL0;
+		break;
+#if defined(CONFIG_CLK_MESON_T7) || defined(CONFIG_CLK_MESON_P1)
+	case 1:
+		addr = CLKCTRL_DSPB_CLK_CTRL0;
+		break;
 #endif
-		default :  break;
+	default:
+		ret = -1;
+		break;
 	}
+
+	if (ret) {
+		printf("dsp clk set failed: invalid id\n");
+		return ret;
+	}
+
 	// Make sure not busy from last setting and we currently match the last setting
 
 	control = readl(addr);
-	printf("control = 0x%x \n", control);
-	printf ("CLKCTRL_DSP_CLK_CTRL0 =%x", readl(addr));
-	printf ("\n");
+	printf("control = 0x%x\n", control);
+	printf("CLKCTRL_DSP_CLK_CTRL0 = %x\n", readl(addr));
 
-#if defined(CONFIG_CLK_P1_TYPE) || defined(CONFIG_CLK_A5_TYPE)
-	switch (freq_sel)
-	{
-		case 0  : clk_sel = 1; clk_div =0; printf ("CLK_UTIL:dsp[]:fclk2p5:800MHz\n")    ; break;
-		case 1  : clk_sel = 1; clk_div =1; printf ("CLK_UTIL:dsp[]:fclk2p5/2:400MHz\n")  ; break;
-		case 2  : clk_sel = 5; clk_div =0; printf ("CLK_UTIL:dsp[]:fclk4:500MHz\n")      ; break;
-		case 3  : clk_sel = 2; clk_div =0; printf ("CLK_UTIL:dsp[]:fclk/3:667MHz\n")     ; break;
-		case 4  : clk_sel = 2; clk_div =1; printf ("CLK_UTIL:dsp[]:fclk3/2:333MHz\n")    ; break;
-		case 5  : clk_sel = 5; clk_div =1; printf ("CLK_UTIL:dsp[]:fclk4/2:250MHz\n")    ; break;
-		case 6  : clk_sel = 1; clk_div =3; printf ("CLK_UTIL:dsp[]:fclk2p5/4:200MHz\n")  ; break;
-		case 7  : clk_sel = 1; clk_div =7; printf ("CLK_UTIL:dsp[]:fclk5/4:100MHz\n")    ; break;
-		case 8  : clk_sel = 0; clk_div =0; printf ("CLK_UTIL:dsp[]:oscin:24MHz\n")       ; break;
-		case 10 : clk_sel = 0; clk_div =7; printf ("CLK_UTIL:dsp[]:oscin/8:3MHz\n")      ; break;
-		default : clk_sel = 1; clk_div =0; printf ("CLK_UTIL:dsp[]:fclk2p5:800MHz\n")    ; break;
-		}
-#else
-	switch (freq_sel)
-	{
-		case 0  : clk_sel = 1; clk_div =0; printf ("CLK_UTIL:dsp[]:fclk2p5:800MHz\n")    ; break;
-		case 1  : clk_sel = 3; clk_div =0; printf ("CLK_UTIL:dsp[]:fclk5:400MHz\n")      ; break;
-		case 2  : clk_sel = 5; clk_div =0; printf ("CLK_UTIL:dsp[]:fclk4:500MHz\n")      ; break;
-		case 3  : clk_sel = 2; clk_div =0; printf ("CLK_UTIL:dsp[]:fclk/3:667MHz\n")     ; break;
-		case 4  : clk_sel = 2; clk_div =1; printf ("CLK_UTIL:dsp[]:fclk3/2:333MHz\n")    ; break;
-		case 5  : clk_sel = 5; clk_div =1; printf ("CLK_UTIL:dsp[]:fclk4/2:250MHz\n")    ; break;
-		case 6  : clk_sel = 3; clk_div =1; printf ("CLK_UTIL:dsp[]:fclk5/2:200MHz\n")    ; break;
-		case 7  : clk_sel = 3; clk_div =3; printf ("CLK_UTIL:dsp[]:fclk5/4:100MHz\n")    ; break;
-		case 8  : clk_sel = 0; clk_div =0; printf ("CLK_UTIL:dsp[]:oscin:24MHz\n")       ; break;
-		case 10 : clk_sel = 0; clk_div =7; printf ("CLK_UTIL:dsp[]:oscin/8:3MHz\n")      ; break;
-		default : clk_sel = 3; clk_div =0; printf ("CLK_UTIL:dsp[]:fclk5:400MHz\n")      ; break;
+	switch (freq_sel) {
+	case 0:
+		clk_sel = 1;
+		clk_div = 0;
+		dsp_clk_tag = "fclk2p5:800MHz";
+		break;
+	case 1:
+		clk_sel = 2;
+		clk_div = 0;
+		dsp_clk_tag = "fclk/3:667MHz";
+		break;
+	case 2:
+		clk_sel = 5;
+		clk_div = 0;
+		dsp_clk_tag = "fclk4:500MHz";
+		break;
+	case 3:
+		clk_sel = 1;
+		clk_div = 1;
+		dsp_clk_tag = "fclk2p5/2:400MHz";
+		break;
+	case 4:
+		clk_sel = 2;
+		clk_div = 1;
+		dsp_clk_tag = "fclk3/2:333MHz";
+		break;
+	case 5:
+		clk_sel = 5;
+		clk_div = 1;
+		dsp_clk_tag = "fclk4/2:250MHz";
+		break;
+	case 6:
+		clk_sel = 1;
+		clk_div = 3;
+		dsp_clk_tag = "fclk2p5/4:200MHz";
+		break;
+	case 7:
+		clk_sel = 1;
+		clk_div = 7;
+		dsp_clk_tag = "fclk2p5/8:100MHz";
+		break;
+	case 8:
+		clk_sel = 0;
+		clk_div = 0;
+		dsp_clk_tag = "oscin:24MHz";
+		break;
+	case 9:
+		clk_sel = 0;
+		clk_div = 7;
+		dsp_clk_tag = "oscin/8:3MHz";
+		break;
+	default:
+		clk_sel = 1;
+		clk_div = 0;
+		dsp_clk_tag = "fclk2p5:800MHz (default)";
+		break;
 	}
-#endif
+
+	printf("CLK_UTIL:dsp[%d]:%s\n", id, dsp_clk_tag);
 
 	if (control & (1 << 15)) {   //if sync_mux ==1, sel mux 0
-		control = (control & ~( ( 1<<15) | (0x3ff<<0) | (0x7 <<10) ) ) | (1<<13)| (1<<29) | (clk_div<<0) | (clk_sel<<10);
+		control = (control & ~((1 << 15) | (0x3ff << 0) | (0x7 << 10))) |
+			(1 << 13) | (1 << 29) | (clk_div << 0) | (clk_sel << 10);
 	} else {
-		control = (control & ~( ( 1<<15) | (0x3ff<<16) | (0x7 <<26) ) ) | (1<<13)| (1<<29) | (clk_div<<16) | (clk_sel<<26) | (1<<15);
+		control = (control & ~((1 << 15) | (0x3ff << 16) | (0x7 << 26))) |
+			(1 << 13) | (1 << 29) | (clk_div << 16) | (clk_sel << 26) | (1 << 15);
 	}
 
 	writel(control, addr);
 
-	switch ( id )
-	{
-		case 0  :  printf ("CLKCTRL_DSPA_CLK_CTRL0 =%x\n", readl(addr)); break;
-		case 1  :  printf ("CLKCTRL_DSPB_CLK_CTRL0 =%x\n", readl(addr)); break;
-		default :  break;
+	switch (id) {
+	case 0:
+		printf("CLKCTRL_DSPA_CLK_CTRL0 = %x\n", readl(addr));
+		break;
+	case 1:
+		printf("CLKCTRL_DSPB_CLK_CTRL0 = %x\n", readl(addr));
+		break;
+	default:
+		break;
 	}
+
+	return ret;
 }
 
 static int do_startdsp(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
@@ -113,42 +164,44 @@ static int do_startdsp(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]
 	uint32_t StatVectorSel;
 	uint32_t strobe = 1;
 
-	int ret=0;
-
 	if (argc <= 1) {
-		printf("plese input dsp boot args:id, addrss, clk!\n");
+		printf("please input dsp boot args: id, address, clk!\n");
 		return CMD_RET_USAGE;
 	}
+
 	dspid = simple_strtoul(argv[1], NULL, 16);
+	if (dspid > 1) {
+		printf("start dsp failed: invalid id\n");
+		return -1;
+	}
+
 	addr = simple_strtoul(argv[2], NULL, 16);
 	freq_sel = simple_strtoul(argv[3], NULL, 16);
 
-	printf("dsp%d boot \n",dspid);
-	printf("dspboot start address:0x%x\n",addr);
-	printf("dsp clk num:%d\n",freq_sel);
+	printf("dsp%d boot\n", dspid);
+	printf("dspboot start address: 0x%x\n", addr);
+	printf("dsp clk num:%d\n", freq_sel);
 
 	StatVectorSel = (addr != 0xfffa0000);
-	cfg0 = 0x1 |  StatVectorSel << 1 | strobe << 2;
+	cfg0 = 0x1 | StatVectorSel << 1 | strobe << 2;
 
 	power_set_dsp((dspid == 0) ? PM_DSPA : PM_DSPB, PWR_ON);
 	udelay(100);
-	set_dsp_clk(dspid,freq_sel);
+	if (set_dsp_clk(dspid, freq_sel))
+		return -1;
 	udelay(100);
 	init_dsp(dspid, addr, cfg0);
-	printf("dsp init over! \n");
-	return ret;
-}
+	printf("dsp init over!\n");
 
+	return 0;
+}
 
 U_BOOT_CMD(
 	startdsp, 4, 1, do_startdsp,
 	"load dspboot.bin from address",
 	"\narg[0]: cmd\n"
-	"arg[1]: dspid \n"
+	"arg[1]: dspid\n"
 	"arg[2]: dspboot.bin load address!\n"
-	"arg[3]: dsp clk set 0:800M 1:400M 2:500M 4:333M 5:250M 6:200M 7:100M 8 :24M"
+	"arg[3]: dsp clk set 0:800M 1:667M 2:500M 3:400M 4:333M 5:250M 6:200M 7:100M 8:24M 9:3M"
 );
-
-
-
 
