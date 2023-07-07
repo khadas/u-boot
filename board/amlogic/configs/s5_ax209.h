80506e9fd4209112525efe6c7e59efe6ac720d32
chao.zhang <chao.zhang@amlogic.com>
2023-07-07 15:49:29 +0800

    pwm:bl33: set default voltage of s5 [1/1]
    PD#SWPL-91114
    
    Problem:
    default voltage not march
    
    Solution:
    1. change ee and gpu default voltage
    
    Verify:
    s5-ax201
    
    Change-Id: Id66db17c2e2345518e04415811b380d1f91ceb9d
    Signed-off-by: chao.zhang <chao.zhang@amlogic.com>

--- board/amlogic/configs/s5_ax209.h
+++ board/amlogic/configs/s5_ax209.h
@@ -26,10 +26,11 @@
 /*
  * platform power init config
  */
-#define AML_VDDEE_INIT_VOLTAGE			840		// VDDEE power up voltage
+#define AML_VDDEE_INIT_VOLTAGE			870		// VDDEE power up voltage
 #define AML_VDD_CPUA_INIT_VOLTAGE		1009	// VCCK A power up voltage
 #define AML_VDD_CPUB_INIT_VOLTAGE		1009	// VCCK B power up voltage
-#define AML_VDDGPU_INIT_VOLTAGE			840		// VDDGPU power up voltage
+#define AML_VDDGPU_INIT_VOLTAGE			900		// VDDGPU power up voltage
+#define AML_VDDNPU_INIT_VOLTAGE			840		// VDDNPU power up voltage
 
 /* SMP Definitions */
 #define CPU_RELEASE_ADDR		secondary_boot_func
