ff21870eacc516521e5c8a6315ebfd894b34fe88
Xiong Zhang <xiong.zhang@wesion.com>
2023-07-25 15:15:03 +0800

    LED: Repair VIM3L boot white LED light is not bright
    Signed-off-by: Xiong Zhang <xiong.zhang@wesion.com>

--- board/khadas/kvim3l/kvim3l.c
+++ board/khadas/kvim3l/kvim3l.c
@@ -681,6 +681,7 @@ static int check_forcebootsd(void)
 }
 int board_init(void)
 {
+	printf("board init\n");
 	sys_led_init();
     //Please keep CONFIG_AML_V2_FACTORY_BURN at first place of board_init
     //As NOT NEED other board init If USB BOOT MODE
@@ -771,6 +772,7 @@ void aml_config_dtb(void)
 #ifdef CONFIG_BOARD_LATE_INIT
 int board_late_init(void)
 {
+	printf("board late init\n");
 	TE(__func__);
 		//update env before anyone using it
 		run_command("get_rebootmode; echo reboot_mode=${reboot_mode}; "\
