177fb3f2c5acf92d8b6f905c3127e486643a742b
Bichao Zheng <bichao.zheng@amlogic.com>
2023-08-06 10:07:05 +0800

    spinand: add FORESEE F35SQA002G support [1/1]
    PD#SWPL-128559
    
    Problem:
    1. need add F35SQA002G support.
    
    Solution:
    1. add FORESEE driver
    
    Verify:
    A1
    
    Change-Id: I1138f6e3eb736356a0ed41ed911514ae9a87bb3f
    Signed-off-by: Bichao Zheng <bichao.zheng@amlogic.com>

--- include/linux/mtd/spinand.h
+++ include/linux/mtd/spinand.h
@@ -228,6 +228,7 @@ extern const struct spinand_manufacturer micron_spinand_manufacturer;
 extern const struct spinand_manufacturer winbond_spinand_manufacturer;
 extern const struct spinand_manufacturer zetta_spinand_manufacturer;
 extern const struct spinand_manufacturer xtx_spinand_manufacturer;
+extern const struct spinand_manufacturer foresee_spinand_manufacturer;
 
 /**
  * struct spinand_op_variants - SPI NAND operation variants
