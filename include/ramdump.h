967e84addc373c69d94fcf7565ba3ae4b41f9102
dongqing.li <dongqing.li@amlogic.com>
2023-09-01 18:05:54 +0800

    ramdump: bl33/v2019 overwrite dts rsvmem info [1/1]
    PD#SWPL-132538
    
    Problem:
    The core-dump file cannot be saved to /data in Android.
    
    Solution:
    1) When a kernel panic occurs, flush cache all.
    2) Restart to uboot stage and compress the full DDR.
    3) Set kernel dts rsv mem based on compressed infor.
    4) Crop reserved memory sub nodes to reduce mem usage.
    5) Kernel stage parsing dts to configure rsv mem.
    6) Android init.rc read sysnode, and generate crash file.
    
    Verify:
    sc2_ah212
    
    Change-Id: I73c083d854f4fe9ed9d2cf17eb8192258139817e
    Signed-off-by: dongqing.li <dongqing.li@amlogic.com>

--- include/ramdump.h
+++ include/ramdump.h
@@ -6,6 +6,20 @@
 #ifndef __RAMDUMP_H__
 #define __RAMDUMP_H__
 
+/* PAGE_SHIFT determines the page size */
+#define PAGE_SHIFT              12
+#define PAGE_SIZE               ((1) << PAGE_SHIFT)
+
+/* align addr on a size boundary - adjust address up/down if needed */
+#define _ALIGN_UP(addr, size)	(((addr) + ((size) - 1)) & (~((typeof(addr))(size) - 1)))
+#define _ALIGN_DOWN(addr, size)	((addr) & (~((typeof(addr))(size) - 1)))
+
+/* align addr on a size boundary - adjust address up if needed */
+#define _ALIGN(addr, size)      _ALIGN_UP(addr, size)
+
+/* to align the pointer to the (next) page boundary */
+#define PAGE_ALIGN(addr)        _ALIGN(addr, PAGE_SIZE)
+
 #define RAMDUMP_STICKY_DATA_MASK		(0xFFFF)
 
 extern void check_ramdump(void);
