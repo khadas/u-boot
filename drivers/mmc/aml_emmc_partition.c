c3f53601acbd28fe8825591ea044561c995aed03
Nick Xie <nick@khadas.com>
2023-09-12 11:25:06 +0800

    partition: remove cache partition
    Signed-off-by: Nick Xie <nick@khadas.com>
    Change-Id: I29daf7f2ef8e738cd5c230731ecc7e57b0839772

--- drivers/mmc/aml_emmc_partition.c
+++ drivers/mmc/aml_emmc_partition.c
@@ -102,7 +102,7 @@ struct partitions emmc_partition_table[] = {
 	PARTITION_ELEMENT(MMC_RESERVED_NAME, MMC_RESERVED_SIZE, 0),
 	/* prior partitions, same partition name with dts*/
 	/* partition size will be override by dts*/
-	PARTITION_ELEMENT(MMC_CACHE_NAME, 0, 0),
+	/*PARTITION_ELEMENT(MMC_CACHE_NAME, 0, 0),*/
 	PARTITION_ELEMENT(MMC_ENV_NAME, MMC_ENV_SIZE, 0),
 #ifdef CONFIG_AB_UPDATE
 	PARTITION_ELEMENT(FIP_A_NAME, FIP_SIZE, 0),
