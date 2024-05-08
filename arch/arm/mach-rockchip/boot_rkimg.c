7861fab51182d590777d7049c47a193bc0cb146d
Gray Huang <gray.huang@wesion.com>
2024-05-08 10:58:23 +0800

    rk3588: Edge2: dtbo: load dtbo from rootfs
    Change-Id: I356f287debb3eff96090b398cf565ef470d929f0
    Signed-off-by: Gray Huang <gray.huang@wesion.com>

--- arch/arm/mach-rockchip/boot_rkimg.c
+++ arch/arm/mach-rockchip/boot_rkimg.c
@@ -522,10 +522,44 @@ static int dtb_scan(void *fdt, int where)
 	return -EINVAL;
 }
 
+#define DEFAULT_LOAD_ENV_ADDR   (0x0a200000)
+#define DEFAULT_ENV_FILE_SIZE   (0x107b4a)
+#define DEFAULT_LOAD_DTBO_ADDR  (0x5000000)
 int rockchip_read_dtb_file(void *fdt)
 {
 	int locate, ret;
 	int size;
+	char env_buf[128] = {'\0'};
+	char * env_addr = env_get("ramdisk_addr_r");
+	char * file_size = env_get("filesize_s");
+	unsigned long load_env_addr = 0;
+	unsigned long env_size = 0;
+
+	if (strict_strtoul(env_addr, 16, &load_env_addr) < 0) {
+		printf("Get ramdisk_addr_r failed, set default.\n");
+		load_env_addr = DEFAULT_LOAD_ENV_ADDR;
+	}
+
+	if (strict_strtoul(file_size, 16, &env_size) < 0) {
+		printf("Get filesize_s failed, set default.\n");
+		env_size = DEFAULT_ENV_FILE_SIZE;
+	}
+
+	/* load uEnv.txt */
+	snprintf(env_buf, sizeof(env_buf), "load mmc 0:6 0x%lx  /boot/dtb/rockchip/rk3588s-khadas-edge2.dtb.overlay.env", load_env_addr);
+	printf("cmd:%s\n", env_buf);
+	ret = run_command(env_buf, 0);
+	if (!ret) {
+		memset(env_buf, 0, sizeof(env_buf));
+		snprintf(env_buf, sizeof(env_buf), "env import -t 0x%lx 0x%lx", load_env_addr, env_size);
+		printf("cmd:%s\n", env_buf);
+		ret = run_command(env_buf, 0);
+		if (ret) {
+			printf("env import failed\n");
+		}
+	} else {
+		printf("load /boot/dtb/rockchip/rk3588s-khadas-edge2.dtb.overlay.env failed\n");
+	}
 
 	for (locate = 0; locate < LOCATE_END; locate++) {
 		ret = dtb_scan(fdt, locate);
@@ -543,11 +577,46 @@ int rockchip_read_dtb_file(void *fdt)
 		ALIGN(size, RK_BLK_SIZE) + CONFIG_SYS_FDT_PAD))
 		return -ENOMEM;
 
-	/* fixup/overlay */
-	rk_board_early_fdt_fixup(fdt);
-#if defined(CONFIG_ANDROID_BOOT_IMAGE) && defined(CONFIG_OF_LIBFDT_OVERLAY)
-	android_fdt_overlay_apply((void *)fdt);
-#endif
+	char * overlays = env_get("fdt_overlays");
+	char * setoverlays = NULL;
+	char * ptr = NULL;
+	char cmd_buf[128] = {'\0'};
+	char * dtbo_env_addr = env_get("fdtbo_addr_r");
+	unsigned long load_dtbo_addr = 0;
+
+	if (strict_strtoul(dtbo_env_addr, 16, &load_dtbo_addr) < 0) {
+		printf("Get dtbo_env_addr failed, set default.\n");
+		load_dtbo_addr = DEFAULT_LOAD_DTBO_ADDR;
+	}
+
+	if (NULL != overlays) {
+		setoverlays = (char *)malloc(strlen(overlays)+1);
+		memset(setoverlays, 0, strlen(overlays)+1);
+		memcpy(setoverlays, overlays, strlen(overlays));
+		if (NULL != setoverlays) {
+			ptr = strtok(setoverlays, " ");
+			while(ptr != NULL){
+				memset(cmd_buf, 0, sizeof(cmd_buf));
+				snprintf(cmd_buf, sizeof(cmd_buf), "load mmc 0:6 0x%lx  /boot/dtb/rockchip/rk3588s-khadas-edge2.dtb.overlays/%s.dtbo", load_dtbo_addr, ptr);
+				printf("cmd:%s\n", cmd_buf);
+				ret = run_command(cmd_buf, 0);
+				if (!ret) {
+					ret = fdt_increase_size(fdt, fdt_totalsize((void *)load_dtbo_addr));
+					if (!ret) {
+						ret = fdt_overlay_apply(fdt, (void *)load_dtbo_addr);
+						if (!ret) {
+							printf("Linux: fdt overlay OK\n");
+						} else {
+							printf("Linux: fdt overlay failed, ret=%d\n", ret);
+						}
+					}
+				}
+				ptr = strtok(NULL, " ");
+			}
+		}
+		free(setoverlays);
+		setoverlays = NULL;
+	}
 
 	return 0;
 }
