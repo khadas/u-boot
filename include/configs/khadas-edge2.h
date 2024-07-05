4b1207a17b8c6bfc2cb3aa99a8f1dd999180e7ed
Jacobe Zang <jacobe.zang@wesion.com>
2024-07-05 09:30:26 +0800

    Kconfig: set SYS_BOARD=khadas-edge2 sync from evb_rk3588
    Signed-off-by: Jacobe Zang <jacobe.zang@wesion.com>
    Change-Id: I32ca9a6bb26a5bdbcf27e46cdb88333b1c812aaa

---
+++ include/configs/khadas-edge2.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2023 Rockchip Electronics Co., Ltd.
+ */
+
+#ifndef __KHADAS_EDGE2_H
+#define __KHADAS_EDGE2_H
+
+#include <configs/rk3588_common.h>
+
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0"
+
+#endif
