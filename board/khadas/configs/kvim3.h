c036474388d8130de72ecbe098b09371e63bd211
Reeve Huang <reeve.huang@wesion.com>
2025-01-21 17:03:46 +0800

    VIM3: fix hdmi logo display
    Signed-off-by: Reeve Huang <reeve.huang@wesion.com>
    Change-Id: I11f2623abe5d114a4ee1d7e22533ec84861537e3

--- board/khadas/configs/kvim3.h
+++ board/khadas/configs/kvim3.h
@@ -202,11 +202,16 @@
             "\0"\
         "storeargs="\
             "get_bootloaderversion;" \
+        "if test ${lcd_exist} != 1; then "\
+			"setenv vout2_args ;"\
+		"else "\
+			"setenv vout2_args vout2=${outputmode2},enable;"\
+		"fi;"\
 	"setenv bootargs ${initargs} hdr_policy=${hdr_policy}  hdr_priority=${hdr_priority} "\
 		"otg_device=${otg_device} reboot_mode_android=${reboot_mode_android} "\
 		"logo=${display_layer},loaded,${fb_addr} "\
 		"fb_width=${fb_width} fb_height=${fb_height} display_bpp=${display_bpp} "\
-		"outputmode=${outputmode} vout2=${outputmode2},enable "\
+		"outputmode=${outputmode} ${vout2_args} "\
 		"vout=${outputmode},${vout_init} "\
 		"panel_type=${panel_type} lcd_ctrl=${lcd_ctrl} lcd_debug=${lcd_debug} "\
 		"hdmitx=${cecconfig},${colorattribute} hdmimode=${hdmimode} "\
@@ -498,9 +503,10 @@
 
 /* buffer rotate for portrait screen */
 #define CONFIG_SINGLE_LOGO \
-	"setenv outputmode panel;setenv display_layer osd0;"\
-	"setenv fb_height 1920; setenv fb_width 1080;"\
-	"vout output panel;osd open;osd clear;imgread pic logo bootup_rotate $loadaddr;bmp display $bootup_rotate_offset;bmp scale;"\
+	"setenv outputmode 1080p60hz;setenv display_layer osd0;"\
+	"setenv fb_height 1080; setenv fb_width 1920;"\
+	"vout output $outputmode;osd open;osd clear;imgread pic logo bootup $loadaddr;"\
+	"bmp display $bootup_offset;bmp scale;"\
 	"\0"\
 
 //#define CONFIG_ENV_IS_NOWHERE  1
