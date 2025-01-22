5b024082c3af0e70d706c2e468867edb32bef71f
Reeve Huang <reeve.huang@wesion.com>
2025-01-22 10:59:30 +0800

    VIM3: fix mipi panel failure after using the reboot command upon system boot.
    Signed-off-by: Reeve Huang <reeve.huang@wesion.com>
    Change-Id: I778a8565120fced4a3c86bc1333c5cca5dcce041

--- board/khadas/configs/kvim3.h
+++ board/khadas/configs/kvim3.h
@@ -497,6 +497,7 @@
 	"fi; "\
 	"vout output ${outputmode};osd open;osd clear;imgread pic logo bootup $loadaddr;"\
 	"bmp display $bootup_offset;bmp scale;"\
+    "lcd disable; lcd enable;"\
 	"setenv outputmode2 1080p60hz;setenv display_layer viu2_osd0;"\
 	"vout2 prepare ${outputmode2};vout2 output ${outputmode2};osd open;osd clear;imgread pic logo bootup $loadaddr;"\
 	"bmp display $bootup_offset;bmp scale;"\
@@ -533,6 +534,7 @@
     "fi; "\
 	"vout output $outputmode;osd open;osd clear;imgread pic logo bootup $loadaddr;"\
 	"bmp display $bootup_offset;bmp scale;"\
+    "lcd disable; lcd enable;"\
 	"\0"\
 
 //#define CONFIG_ENV_IS_NOWHERE  1
