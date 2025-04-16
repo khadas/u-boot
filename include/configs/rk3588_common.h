8aecdf6e0f649f168413e9253e6601e730f23f05
Xiong Zhang <xiong.zhang@wesion.com>
2025-04-16 18:30:12 +0800

    Support device tree overlay
    Signed-off-by: Xiong Zhang <xiong.zhang@wesion.com>

--- include/configs/rk3588_common.h
+++ include/configs/rk3588_common.h
@@ -87,9 +87,11 @@
 	"scriptaddr=0x00500000\0" \
 	"pxefile_addr_r=0x00600000\0" \
 	"fdt_addr_r=0x08300000\0" \
+	"fdtbo_addr_r=0x05000000\0" \
 	"kernel_addr_r=0x00400000\0" \
 	"kernel_addr_c=0x05480000\0" \
-	"ramdisk_addr_r=0x0a200000\0"
+	"ramdisk_addr_r=0x0a200000\0" \
+	"filesize_s=0x107b4a\0"
 
 #include <config_distro_bootcmd.h>
 
