48d34356c520d3749edc50fa25874de1b1a366e9
Gray Huang <gray.huang@wesion.com>
2025-10-15 14:29:06 +0800

    Edge 2L: add Khadas Edge 2L support
    rk3576-khadas-edge-2l.dts from rk3576-evb.dts
    khadas-edge-2l-rk3576_defconfig from rk3576_defconfig
    kedge-2l.c from evb_rk3576.c
    
    Signed-off-by: Gray Huang <gray.huang@wesion.com>
    Change-Id: I924fe7fc0c9c8381501996da9e8efeff536a78f6

---
+++ include/configs/kedge-2l.h
@@ -0,0 +1,34 @@
+/*
+ * SPDX-License-Identifier:     GPL-2.0+
+ *
+ * Copyright (c) 2022 Wesion Technology Co., Ltd
+ */
+
+#ifndef __CONFIGS_KEDGE_2L_H
+#define __CONFIGS_KEDGE_2L_H
+
+#include <configs/rk3576_common.h>
+
+#ifndef CONFIG_SPL_BUILD
+
+#undef ROCKCHIP_DEVICE_SETTINGS
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"logo_addr_r=0x07000000\0" \
+		"logo_file_dir=/usr/share/fenix/logo\0" \
+		"logocmd_mmc=load $devtype $devnum:2 $logo_addr_r $logo_file_dir/$logo_file || load $devtype $devnum:3 $logo_addr_r $logo_file_dir/$logo_file; setenv logo_filesize $filesize\0" \
+		"logocmd_usb=run logocmd_mmc\0" \
+		"logocmd=echo Load logo: $logo_file_dir/$logo_file; run logocmd_${devtype}\0" \
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0"
+
+#define CONFIG_SYS_MMC_ENV_DEV		0
+
+#undef CONFIG_BOOTCOMMAND
+#define CONFIG_BOOTCOMMAND  "run distro_bootcmd"
+
+#undef BOOTENV_BOOT_TARGETS
+#define BOOTENV_BOOT_TARGETS \
+    "boot_targets=usb0 mmc1 mmc0 pxe dhcp\0"
+
+#endif
+#endif
