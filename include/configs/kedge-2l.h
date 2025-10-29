672773ec87bbd3154b2ee0653f595ca47d80a179
Reeve Huang <reeve.huang@wesion.com>
2025-10-29 09:47:13 +0800

    Edge 2L: logo: fix bmp logo display issue
    Signed-off-by: Reeve Huang <reeve.huang@wesion.com>
    Change-Id: I814a4b3b3918454432da05d16ebda51aeb9eff32

--- include/configs/kedge-2l.h
+++ include/configs/kedge-2l.h
@@ -13,9 +13,11 @@
 
 #undef ROCKCHIP_DEVICE_SETTINGS
 #define ROCKCHIP_DEVICE_SETTINGS \
-		"logo_addr_r=0x07000000\0" \
+		"logo_addr_r=0x49400000\0" \
 		"logo_file_dir=/usr/share/fenix/logo\0" \
 		"logocmd_mmc=load $devtype $devnum:2 $logo_addr_r $logo_file_dir/$logo_file || load $devtype $devnum:3 $logo_addr_r $logo_file_dir/$logo_file; setenv logo_filesize $filesize\0" \
+		"logocmd_usb=run logocmd_mmc\0" \
+		"logocmd=echo Load logo: $logo_file_dir/$logo_file; run logocmd_${devtype}\0" \
 		"stdout=serial,vidconsole\0" \
 		"stderr=serial,vidconsole\0"
 
